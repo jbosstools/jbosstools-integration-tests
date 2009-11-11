65b0c2a453466a66e464c3356ea93e1063c2716e
Vlado Pakan <vpakan@redhat.com>
2009-11-11 11:45:59 +0000

    Added some common constants.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18611 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/ui/bot/test/WidgetVariables.java
+++ tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/ui/bot/test/WidgetVariables.java
@@ -8,10 +8,17 @@ package org.jboss.tools.ui.bot.test;
 
 
 public class WidgetVariables {
-	
 	public static final String ERROR_LOG = "Error Log";
 	public static final String PACKAGE_EXPLORER = "Package Explorer";
 	public static final String WELCOME = "Welcome";
 	public static final String PROGRESS_STATUS = "Progress";
 	public static final String PALETTE = "JBoss Tools Palette";
+	public static final String WEB_PROJECTS = "Web Projects";
+	public static final String SERVERS = "Servers";
+	public static final String OK_BUTTON = "OK";
+	public static final String NEXT_BUTTON = "Next >";
+	public static final String BACK_BUTTON = "< Back";
+	public static final String FINISH_BUTTON = "Finish";
+	public static final String CONTINUE_BUTTON = "Continue";
+	public static final String NEW_BUTTON = "New...";	
 }
