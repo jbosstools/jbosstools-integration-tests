5b12368ad89c5438aadd00bd10144baece4c12a0
Denis Maliarevich <dmaliarevich@exadel.com>
2010-02-22 15:42:55 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2795, updating SWTBot Test, adding icons to the wizards.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20416 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/wizard/ImportUnknownTagsWizardTest.java
+++ tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/wizard/ImportUnknownTagsWizardTest.java
@@ -9,10 +9,17 @@
  ******************************************************************************/
 package org.jboss.tools.vpe.ui.bot.test.wizard;
 
-import org.eclipse.swtbot.swt.finder.SWTBot;
+import java.io.File;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.jboss.tools.ui.bot.test.WidgetVariables;
 import org.jboss.tools.vpe.ui.bot.test.VPEAutoTestCase;
+import org.osgi.framework.Bundle;
 
 public class ImportUnknownTagsWizardTest extends VPEAutoTestCase {
 
@@ -32,19 +39,56 @@ public class ImportUnknownTagsWizardTest extends VPEAutoTestCase {
 	}
 
 	public void _testImportWizard() throws Throwable {
+		/*
+		 * Open wizard page
+		 */
 		bot.menu("File").menu("Import...").click(); //$NON-NLS-1$ //$NON-NLS-2$
 		bot.shell("Import").activate(); //$NON-NLS-1$
 		SWTBotTree importTree = bot.tree();
 		importTree.expandNode("Other").select("Unknown tags templates"); //$NON-NLS-1$ //$NON-NLS-2$
 		bot.button(WidgetVariables.NEXT_BUTTON).click();
-		bot.text().setText(STORED_TAGS_PATH);
-	}
-	
-	protected void openPage() {
-		SWTBot innerBot = bot.viewByTitle(WidgetVariables.PACKAGE_EXPLORER).bot();
-		SWTBotTree tree = innerBot.tree();
-		tree.expandNode(JBT_TEST_PROJECT_NAME)
-		.expandNode("WebContent").expandNode("pages").getNode(TEST_PAGE).doubleClick(); //$NON-NLS-1$ //$NON-NLS-2$
+		/*
+		 * Load stored tags
+		 */
+		Bundle bundle = Platform.getBundle("org.jboss.tools.vpe.ui.bot.test"); //$NON-NLS-1$
+		URL url = bundle.getEntry("/resources"); //$NON-NLS-1$
+		url = FileLocator.resolve(url);
+		IPath path = new Path(url.getPath());
+		File file = path.append(STORED_TAGS_PATH).toFile();
+        assertTrue("File '" + file.getAbsolutePath() +"' does not exist.", file.exists()); //$NON-NLS-1$ //$NON-NLS-2$
+        bot.text().setText(file.getAbsolutePath());
+        /*
+         * Check table values 
+         */
+        String taglib = bot.table().cell(0, 0);
+        assertEquals("Wrong table value.", "lib:tag", taglib); //$NON-NLS-1$ //$NON-NLS-2$
+        taglib = bot.table().cell(1, 0);
+        assertEquals("Wrong table value.", "taglibName:tagName", taglib); //$NON-NLS-1$  //$NON-NLS-2$
+        /*
+         * Check that finish button is enabled
+         */
+        assertTrue("Finish button should be enabled.", //$NON-NLS-1$ 
+        		bot.button(WidgetVariables.FINISH_BUTTON).isEnabled());
+        bot.sleep(5000);
+        bot.button(WidgetVariables.FINISH_BUTTON).click();
+        /*
+         * Check that templates have been added to the preference page 
+         */
+        bot.menu("Window").menu("Preferences").click(); //$NON-NLS-1$ //$NON-NLS-2$
+		bot.shell("Preferences").activate(); //$NON-NLS-1$
+		importTree = bot.tree();
+		importTree.expandNode("JBoss Tools") //$NON-NLS-1$d
+				.expandNode("Web") //$NON-NLS-1$
+				.expandNode("Editors") //$NON-NLS-1$
+				.select("Visual Page Editor"); //$NON-NLS-1$
+		bot.tabItem("Templates").activate(); //$NON-NLS-1$
+		/*
+         * Check table values on the preferences page
+         */
+        taglib = bot.table().cell(0, 0);
+        assertEquals("Wrong table value.", "taglibName:tagName", taglib); //$NON-NLS-1$  //$NON-NLS-2$
+        taglib = bot.table().cell(1, 0);
+        assertEquals("Wrong table value.", "lib:tag", taglib); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 	
 }
