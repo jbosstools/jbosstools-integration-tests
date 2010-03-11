e18506839b1d6caefee95848a0dac4f81bed9f17
Yura Zhishko <yzhishko@exadel.com>
2010-03-11 10:09:17 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20755 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/ui/bot/test/WidgetVariables.java
+++ tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/ui/bot/test/WidgetVariables.java
@@ -31,5 +31,6 @@ public class WidgetVariables {
 	public static final String EDIT_BUTTON = "Edit";	 //$NON-NLS-1$
 	public static final String REMOVE_BUTTON = "Remove";	 //$NON-NLS-1$
 	public static final String CSS_STYLE_DIALOG_TITLE = "CSS Style";	 //$NON-NLS-1$
+	public static final String CSS_SELECTOR_DIALOG_TITLE = "CSS Selector"; //$NON-NLS-1$
 	
 }
