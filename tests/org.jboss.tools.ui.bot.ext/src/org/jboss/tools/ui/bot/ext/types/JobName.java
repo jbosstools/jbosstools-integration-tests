88262c9c81a88be901183d60f7f380fd03814db8
Vlado Pakan <vpakan@redhat.com>
2010-03-29 11:17:43 +0000

    Added new jobs
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@21101 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/types/JobName.java
+++ tests/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/types/JobName.java
@@ -20,4 +20,12 @@ public class JobName {
   public static final String VISUAL_UPDATE = "Visual Editor View Update";
   public static final String VISUAL_REFRESH = "Visual Editor Refresh";
   public static final String UPDATING_INDEXES = "Updating indexes";
+  public static final String USAGE_DATA_EVENT_CONSUMER = "Usage Data Event consumer";
+  public static final String ANIMATION_START = "Animation start";
+  public static final String UPDATE_PROCESS = "Update Progress";
+  public static final String PERIODIC_WORKSPACE_SAVE = "Periodic workspace save.";
+  public static final String JBOSS_TOOLS_MODEL_UPDATE = "JBoss Tools Model Update";
+  public static final String INITIALIZING_QUICK_DIFF = "Initializing Quick Diff";
+  public static final String PROCESSING_DIRTY_REGIONS = "Processing Dirty Regions";
+  public static final String UPDATING_WORKSPACE = "Updating workspace";
 }
