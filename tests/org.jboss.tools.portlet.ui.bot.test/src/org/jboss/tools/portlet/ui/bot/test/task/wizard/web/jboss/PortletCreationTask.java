29e070e1016ba0723dfc1759165532a1cb94b6ac
Lucia Jelinkova <ljelinko@redhat.com>
2011-11-21 13:07:04 +0000

    created
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36482 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/task/wizard/web/jboss/PortletCreationTask.java
@@ -0,0 +1,47 @@
+package org.jboss.tools.portlet.ui.bot.test.task.wizard.web.jboss;
+
+import org.jboss.tools.portlet.ui.bot.test.task.wizard.WizardFillingTask;
+import org.jboss.tools.portlet.ui.bot.test.task.wizard.WizardOpeningTask;
+
+/**
+ * Creates a new portlet using a JBoss wizard. 
+ * 
+ * @author Lucia Jelinkova
+ *
+ */
+public class PortletCreationTask extends WizardFillingTask {
+
+	private PortletWizardPageFillingTask firstPage;
+	
+	public PortletCreationTask() {
+		super();
+		firstPage = new PortletWizardPageFillingTask();
+		addWizardPage(firstPage);
+	}
+	
+	@Override
+	public void perform() {
+		performInnerTask(new WizardOpeningTask("Java Portlet", "JBoss Tools Web/Portlet"));
+		super.perform();
+	}
+
+	public void setProject(String project) {
+		firstPage.setProject(project);
+	}
+
+	public void setSourceFolder(String sourceFolder) {
+		firstPage.setSourceFolder(sourceFolder);
+	}
+
+	public void setPackageName(String packageName) {
+		firstPage.setPackageName(packageName);
+	}
+
+	public void setClassName(String className) {
+		firstPage.setClassName(className);
+	}
+
+	public void setSuperclass(String superclass) {
+		firstPage.setSuperclass(superclass);
+	}
+}
