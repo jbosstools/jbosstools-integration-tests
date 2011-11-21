5a1f815a017a11d8d0cc3efcb0436177489d3a26
Lucia Jelinkova <ljelinko@redhat.com>
2011-11-21 13:08:26 +0000

    changed code to use SWT bot ext
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36485 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/task/wizard/WizardOpeningTask.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/task/wizard/WizardOpeningTask.java
@@ -1,8 +1,12 @@
 package org.jboss.tools.portlet.ui.bot.test.task.wizard;
 
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.jboss.tools.portlet.ui.bot.test.task.AbstractSWTTask;
+import org.jboss.tools.ui.bot.ext.SWTBotFactory;
+import org.jboss.tools.ui.bot.ext.gen.INewObject;
 
 /**
  * Opens a specified wizard. 
@@ -11,37 +15,44 @@ import org.jboss.tools.portlet.ui.bot.test.task.AbstractSWTTask;
  *
  */
 public class WizardOpeningTask extends AbstractSWTTask {
-	
+
 	private String category;
-	
+
 	private String name;
-	
+
 	public WizardOpeningTask(String name) {
 		super();
 		this.name = name;
 	}
-	
-	public WizardOpeningTask(String name, String category) {
+
+	/**
+	 * 
+	 * @param name Name of the wizard to open
+	 * @param categoryPath path to the wizard (categories separated by '/')
+	 */
+	public WizardOpeningTask(String name, String categoryPath) {
 		this(name);
-		this.category = category;
+		this.category = categoryPath;
 	}
 
 	@Override
 	public void perform() {
-		getBot().menu("File").menu("New").menu("Other...").click();
-
-		SWTBotTree tree  = getBot().tree();
-		getTreeItem(tree).select();
-		 
-		getBot().button("Next >").click();
-	}
-	
-	private SWTBotTreeItem getTreeItem(SWTBotTree tree){
-		if (category == null){
-			return tree.getTreeItem(name);
-		} else {
-			tree.expandNode(category);
-			return tree.getTreeItem(category).getNode(name);			
-		}
+		INewObject wizardPath = new INewObject() {
+			
+			@Override
+			public String getName() {
+				return name;
+			}
+			
+			@Override
+			public List<String> getGroupPath() {
+				if (category == null){
+					return Collections.emptyList();
+				}
+				
+				return Arrays.asList(category.split("/"));
+			}
+		};
+		SWTBotFactory.getOpen().newObject(wizardPath);
 	}
 }
