50e520cec864e8d1bca30e56a3a611353ac0366b
Lucia Jelinkova <ljelinko@redhat.com>
2011-11-22 16:43:24 +0000

    created new test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36534 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/AllTestsSuite.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/AllTestsSuite.java
@@ -2,13 +2,15 @@ package org.jboss.tools.portlet.ui.bot.test;
 
 import org.jboss.tools.portlet.ui.bot.test.create.CreateJavaPortlet;
 import org.jboss.tools.portlet.ui.bot.test.create.CreateJavaPortletProject;
+import org.jboss.tools.portlet.ui.bot.test.create.RunJavaPortletOnServer;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(RequirementAwareUsageWindowClosingSuite.class)
 @Suite.SuiteClasses({
 	CreateJavaPortletProject.class,
-	CreateJavaPortlet.class
+	CreateJavaPortlet.class, 
+	RunJavaPortletOnServer.class
 	})
 public class AllTestsSuite {
 
---
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/create/RunJavaPortletOnServer.java
@@ -0,0 +1,35 @@
+package org.jboss.tools.portlet.ui.bot.test.create;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.jboss.tools.portlet.ui.bot.test.create.CreateJavaPortletProject.PROJECT_NAME;
+import static org.hamcrest.Matchers.not;
+import static org.jboss.tools.portlet.ui.bot.test.matcher.factory.DefaultMatchersFactory.inConsoleOutput;
+
+import org.jboss.tools.portlet.ui.bot.test.task.console.ConsoleClearingTask;
+import org.jboss.tools.portlet.ui.bot.test.task.server.RunninngProjectOnServerTask;
+import org.jboss.tools.portlet.ui.bot.test.testcase.SWTTaskBasedTestCase;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Server;
+import org.jboss.tools.ui.bot.ext.config.Annotations.ServerState;
+import org.jboss.tools.ui.bot.ext.config.Annotations.ServerType;
+import org.junit.Test;
+
+/**
+ * Performs Run on Server on the java portlet project and checks if there is no exception in the
+ * console.  
+ * 
+ * @author Lucia Jelinkova
+ *
+ */
+@Require(clearWorkspace=false, clearProjects=false, server=@Server(required=true, state=ServerState.Running, type=ServerType.EPP))
+public class RunJavaPortletOnServer extends SWTTaskBasedTestCase {
+
+	@Test
+	public void testRunOnServer(){
+		doPerform(new ConsoleClearingTask());
+		doPerform(new RunninngProjectOnServerTask(PROJECT_NAME));
+		
+		assertThat("Exception", not(inConsoleOutput()));
+		System.out.println();
+	}
+}
