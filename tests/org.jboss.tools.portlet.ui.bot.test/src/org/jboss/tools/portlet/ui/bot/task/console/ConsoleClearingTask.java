bfd841149c1877d3dc6506e2bedf3cd2b3ef98f6
Lucia Jelinkova <ljelinko@redhat.com>
2011-12-07 08:32:40 +0000

    removed sysouts
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37035 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/console/ConsoleClearingTask.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/console/ConsoleClearingTask.java
@@ -20,11 +20,8 @@ public class ConsoleClearingTask extends AbstractSWTTask {
 		SWTBotView view = SWTBotFactory.getConsole().show();
 		
 		for (SWTBotToolbarButton button : view.getToolbarButtons()){
-			System.out.println(button.getToolTipText());
 			if (IDELabel.ConsoleView.BUTTON_CLEAR_CONSOLE_TOOLTIP.equals(button.getToolTipText())){
-				System.out.println("Before click");
 				button.click();
-				System.out.println("After click");
 			}
 		}
 	}
