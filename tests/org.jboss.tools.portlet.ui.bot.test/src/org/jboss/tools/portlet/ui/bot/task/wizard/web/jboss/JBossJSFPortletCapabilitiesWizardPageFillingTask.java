a71c99ffeca8197a69678470b4561b4d2a4998a4
Lucia Jelinkova <ljelinko@redhat.com>
2011-12-08 13:15:14 +0000

    Added constructor to support selection of portlet bridge location
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37110 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/wizard/web/jboss/JBossJSFPortletCapabilitiesWizardPageFillingTask.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/wizard/web/jboss/JBossJSFPortletCapabilitiesWizardPageFillingTask.java
@@ -1,11 +1,16 @@
 package org.jboss.tools.portlet.ui.bot.task.wizard.web.jboss;
 
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.jboss.tools.portlet.ui.bot.task.AbstractSWTTask;
 import org.jboss.tools.portlet.ui.bot.task.wizard.WizardPageFillingTask;
 
 /**
  * Fills the JBoss JSF portlet specific wizard page (for selection of 
- * how the portlet libraries should be configured)
+ * how the portlet libraries should be configured). 
+ * 
+ * This task tries to fill the value of portlet bridge runtime only if the field is visible, 
+ * otherwise it is skipped (the wizard page shows field for portlet bridge selection only if 
+ * it cannot determine it automatically from the server) 
  * 
  * @author Lucia Jelinkova
  *
@@ -32,13 +37,27 @@ public class JBossJSFPortletCapabilitiesWizardPageFillingTask extends
 
 	private Type type;
 	
+	private String portletBridge;
+	
 	public JBossJSFPortletCapabilitiesWizardPageFillingTask(Type type) {
 		super();
 		this.type = type;
+	}
+	
+	public JBossJSFPortletCapabilitiesWizardPageFillingTask(Type type, String portletBridge) {
+		super();
+		this.type = type;
+		this.portletBridge = portletBridge;
 	}
 
 	@Override
 	public void perform() {
 		getBot().comboBoxWithLabel("Type:").setSelection(type.toString());
+		
+		try {
+			getBot().textInGroup("Portletbridge Runtime").setText(portletBridge);
+		} catch (WidgetNotFoundException e) {
+			// ok, the portlet bridge is recognized in the server location
+		}
 	}
 }
