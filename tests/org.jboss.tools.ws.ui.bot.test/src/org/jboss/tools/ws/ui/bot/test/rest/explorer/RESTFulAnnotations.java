932c50ae6c22c43a5067cf2872665f3fad29e1a3
Jaroslav Jankovič <jjankovi@redhat.com>
2012-01-04 13:55:20 +0000

    Annotations in RESTful services environment added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37639 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/rest/explorer/RESTFulAnnotations.java
@@ -0,0 +1,25 @@
+package org.jboss.tools.ws.ui.bot.test.rest.explorer;
+
+public enum RESTFulAnnotations {
+	
+	CONFIGURE_MENU_LABEL("Configure"),
+	REST_SUPPORT_MENU_LABEL_ADD("Add JAX-RS 1.1 support..."),
+	REST_SUPPORT_MENU_LABEL_REMOVE("Remove JAX-RS 1.1 support..."),
+	REST_EXPLORER_LABEL("JAX-RS REST Web Services"),
+	REST_EXPLORER_LABEL_BUILD("Building RESTful Web Services..."),
+	GET("GET"),
+	POST("POST"),
+	PUT("PUT"),
+	DELETE("DELETE");
+	
+	private String label; 
+	
+	private RESTFulAnnotations(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+
+}
