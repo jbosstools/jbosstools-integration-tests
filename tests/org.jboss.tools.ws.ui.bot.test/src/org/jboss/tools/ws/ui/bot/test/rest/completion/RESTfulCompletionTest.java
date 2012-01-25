c96b73b82288106e0d9638b1e05d4dc86a693e3f
Jaroslav Jankovič <jjankovi@redhat.com>
2012-01-25 16:46:54 +0000

    RESTfulCompletionTest completed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38175 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/rest/completion/RESTfulCompletionTest.java
+++ tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/rest/completion/RESTfulCompletionTest.java
@@ -11,7 +11,12 @@
 
 package org.jboss.tools.ws.ui.bot.test.rest.completion;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.jboss.tools.ui.bot.ext.helper.ContentAssistHelper;
 import org.jboss.tools.ws.ui.bot.test.rest.RESTfulTestBase;
+import org.jboss.tools.ws.ui.bot.test.rest.explorer.RESTfulExplorerTest;
 import org.junit.Test;
 
 /**
@@ -21,6 +26,22 @@ import org.junit.Test;
  */
 public class RESTfulCompletionTest extends RESTfulTestBase{
 
+	private final String SIMPLE_REST_WS_RESOURCE = "/resources/restful/SimpleRestWS.java.ws";
+	
+	private final String GET_METHOD_PATH = "/{userId}";
+	
+	private final String EMPTY_PATH_PARAM = "";
+	
+	private final String CORRECT_PATH_PARAM = "userId";
+	
+	private final String INCORRECT_PATH_PARAM = "someId";
+	
+	private final String PATH_PARAM_NAVIGATION = "@PathParam(";
+	
+	private final List<String> EXP_NON_EMPTY_COMPLETION_RESULT = Arrays.asList("userId - JAX-RS Mapping");
+	
+	private final List<String> EXP_EMPTY_COMPLETION_RESULT = Arrays.asList("No Default Proposals");
+	
 	protected String getWsProjectName() {
 		return "RestServicesCompletion";
 	}
@@ -48,30 +69,80 @@ public class RESTfulCompletionTest extends RESTfulTestBase{
 	@Test
 	public void testWithEmptyPrefix() {
 		
+		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
+				   RESTfulExplorerTest.class.getResourceAsStream(SIMPLE_REST_WS_RESOURCE), 
+				   false, getWsPackage(), getWsName(), GET_METHOD_PATH, EMPTY_PATH_PARAM);
+		
+		ContentAssistHelper.checkContentAssistContent(bot, 
+				getWsName() + ".java", PATH_PARAM_NAVIGATION, PATH_PARAM_NAVIGATION.length() + 1, 
+				0, EXP_NON_EMPTY_COMPLETION_RESULT);
+		
 	}
 	
 	@Test
 	public void testWithValidPrefixAtTheEnd() {
 		
+		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
+				   RESTfulExplorerTest.class.getResourceAsStream(SIMPLE_REST_WS_RESOURCE), 
+				   false, getWsPackage(), getWsName(), GET_METHOD_PATH, CORRECT_PATH_PARAM);
+		
+		ContentAssistHelper.checkContentAssistContent(bot, 
+				getWsName() + ".java", PATH_PARAM_NAVIGATION, PATH_PARAM_NAVIGATION.length() + 
+				CORRECT_PATH_PARAM.length() + 1, 
+				0, EXP_NON_EMPTY_COMPLETION_RESULT);
+		
 	}
 	
 	@Test
 	public void testWithValidPrefixInTheBeginning() {
+	
+		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
+				   RESTfulExplorerTest.class.getResourceAsStream(SIMPLE_REST_WS_RESOURCE), 
+				   false, getWsPackage(), getWsName(), GET_METHOD_PATH, CORRECT_PATH_PARAM);
+		
+		ContentAssistHelper.checkContentAssistContent(bot, 
+				getWsName() + ".java", PATH_PARAM_NAVIGATION, PATH_PARAM_NAVIGATION.length() + 1, 
+				0, EXP_NON_EMPTY_COMPLETION_RESULT);
 		
 	}
 	
 	@Test
 	public void testWithInvalidPrefixAtTheEnd() {
 		
+		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
+				   RESTfulExplorerTest.class.getResourceAsStream(SIMPLE_REST_WS_RESOURCE), 
+				   false, getWsPackage(), getWsName(), GET_METHOD_PATH, INCORRECT_PATH_PARAM);
+		
+		ContentAssistHelper.checkContentAssistContent(bot, 
+				getWsName() + ".java", PATH_PARAM_NAVIGATION, PATH_PARAM_NAVIGATION.length() + 
+				INCORRECT_PATH_PARAM.length() + 1, 
+				0, EXP_EMPTY_COMPLETION_RESULT);
+		
 	}
-	
+
 	@Test
 	public void testWithInvalidPrefixInTheBeginning() {
+	
+		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
+				   RESTfulExplorerTest.class.getResourceAsStream(SIMPLE_REST_WS_RESOURCE), 
+				   false, getWsPackage(), getWsName(), GET_METHOD_PATH, INCORRECT_PATH_PARAM);
+		
+		ContentAssistHelper.checkContentAssistContent(bot, 
+				getWsName() + ".java", PATH_PARAM_NAVIGATION, PATH_PARAM_NAVIGATION.length() + 1, 
+				0, EXP_NON_EMPTY_COMPLETION_RESULT);
 		
 	}
 	
 	@Test
 	public void testWithAllInvalidParamSelection() {
+		
+		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
+				   RESTfulExplorerTest.class.getResourceAsStream(SIMPLE_REST_WS_RESOURCE), 
+				   false, getWsPackage(), getWsName(), GET_METHOD_PATH, INCORRECT_PATH_PARAM);
+		
+		ContentAssistHelper.checkContentAssistContent(bot, 
+				getWsName() + ".java", PATH_PARAM_NAVIGATION, PATH_PARAM_NAVIGATION.length() + 1, 
+				INCORRECT_PATH_PARAM.length(), EXP_NON_EMPTY_COMPLETION_RESULT);
 		
 	}
 	
