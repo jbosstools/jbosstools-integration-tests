d7261df21ad4ac0ca231d367d04c9860b1dc57f1
Jaroslav Jankovič <jjankovi@redhat.com>
2012-02-07 15:42:22 +0000

    AnnotationPropertiesTest disabled from Test Suite because it is not completed yet
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38469 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/WSAllBotTests.java
+++ tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/WSAllBotTests.java
@@ -11,7 +11,6 @@
 package org.jboss.tools.ws.ui.bot.test;
 
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
-import org.jboss.tools.ws.ui.bot.test.annotation.AnnotationPropertiesTest;
 import org.jboss.tools.ws.ui.bot.test.rest.completion.RESTfulCompletionTest;
 import org.jboss.tools.ws.ui.bot.test.rest.explorer.RESTfulExplorerTest;
 import org.jboss.tools.ws.ui.bot.test.rest.explorer.RESTfulSupportTest;
@@ -54,7 +53,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	SampleRESTWebServiceTest.class,
 	SimpleSoapWebServiceTest.class,
 	SimpleRESTWebServiceTest.class,
-	AnnotationPropertiesTest.class,
+//	AnnotationPropertiesTest.class,
 	RESTfulSupportTest.class, 
 	RESTfulExplorerTest.class,
 	RESTfulValidationTest.class,
