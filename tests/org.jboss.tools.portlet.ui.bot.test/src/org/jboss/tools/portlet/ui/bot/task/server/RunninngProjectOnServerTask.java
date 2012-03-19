e820cdac77ef4c3a60520639fc0499df55558038
Lucia Jelinkova <ljelinko@redhat.com>
2012-03-19 14:14:59 +0000

    Added wait till the jobs are finished
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39613 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/server/RunninngProjectOnServerTask.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/server/RunninngProjectOnServerTask.java
@@ -2,6 +2,8 @@ package org.jboss.tools.portlet.ui.bot.task.server;
 
 import org.jboss.tools.portlet.ui.bot.task.AbstractSWTTask;
 import org.jboss.tools.ui.bot.ext.SWTBotFactory;
+import org.jboss.tools.ui.bot.ext.condition.NonSystemJobRunsCondition;
+import org.jboss.tools.ui.bot.ext.condition.TaskDuration;
 
 /**
  * Performs Run on Server on the specified project. 
@@ -21,5 +23,6 @@ public class RunninngProjectOnServerTask extends AbstractSWTTask {
 	public void perform() {
 		SWTBotFactory.getPackageexplorer().show();
 		SWTBotFactory.getPackageexplorer().runOnServer(project);
+		getBot().waitWhile(new NonSystemJobRunsCondition(), TaskDuration.NORMAL.getTimeout());
 	}
 }
