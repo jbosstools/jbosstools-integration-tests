73718e5a790703ac46bf71ca93cbda3bbdc55333
Jaroslav Jankovič <jjankovi@redhat.com>
2012-03-20 15:48:32 +0000

    initial commit for archives bot test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39667 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.archives.ui.bot.test/src/org/jboss/tools/archives/ui/bot/test/ArchivesAllBotTests.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.archives.ui.bot.test;
+
+import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ * @author Jaroslav Jankovic
+ */
+@RunWith(RequirementAwareSuite.class)
+@SuiteClasses({	
+
+})
+public class ArchivesAllBotTests {
+		
+}
