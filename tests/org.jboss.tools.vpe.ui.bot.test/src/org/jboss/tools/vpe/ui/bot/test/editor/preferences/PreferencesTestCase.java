4184552989185ab6290ecdef8e225d4f4b22e656
Yahor Radtsevich <yradtsevich@exadel.com>
2012-04-16 15:09:26 +0000

    https://issues.jboss.org/browse/JBIDE-11302 -- adding junit.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40213 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/preferences/PreferencesTestCase.java
+++ tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/preferences/PreferencesTestCase.java
@@ -26,8 +26,8 @@ import org.jboss.tools.vpe.ui.bot.test.VPEAutoTestCase;
 
 public abstract class PreferencesTestCase extends VPEAutoTestCase{
 
-	protected static final String TOGGLE_SELECTION_BAR_TOOLTIP = "Toggle Selection Bar (" + 
-	  (SWTJBTExt.isRunningOnMacOs() ? "⌘6" : "Ctrl+6") + ")"; //$NON-NLS-1$
+	protected static final String TOGGLE_SELECTION_BAR_TOOLTIP = "Toggle Selection Bar (" +  //$NON-NLS-1$
+	  (SWTJBTExt.isRunningOnMacOs() ? "⌘6" : "Ctrl+6") + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	protected static final String SHOW_NON_VISUAL_TAGS = "Show non-visual tags"; //$NON-NLS-1$
 	protected static final String SHOW_BORDER_FOR_UNKNOWN_TAGS = "Show border for unknown tags"; //$NON-NLS-1$
 	protected static final String SHOW_RESOURCE_BUNDLES = "Show resource bundles usage as EL expressions"; //$NON-NLS-1$
@@ -37,7 +37,10 @@ public abstract class PreferencesTestCase extends VPEAutoTestCase{
 	protected static final String SHOW_TEXY_FORMAT = "Show text formatting bar"; //$NON-NLS-1$
 	protected static final String PREF_TOOLTIP = "Preferences"; //$NON-NLS-1$
 	protected static final String PREF_FILTER_SHELL_TITLE = "Preferences (Filtered)"; //$NON-NLS-1$
-	
+	protected static final String SCROLL_LOCK_TOOLTIP = "Synchronize scrolling between source and visual panes"; //$NON-NLS-1$
+	protected static final String I18N_BUTTON_TOOLTIP = "Externalize selected string...(" + //$NON-NLS-1$
+			(SWTJBTExt.isRunningOnMacOs() ? "⌘6" : "Ctrl+7") + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	protected static final String SHOW_VISUAL_TOOLBAR_PREFS_CHECKBOX_NAME = "Show toolbar within the editor (otherwise in Eclipse's toolbar)"; //$NON-NLS-1$
 	
 	@Override
 	protected void closeUnuseDialogs() {
