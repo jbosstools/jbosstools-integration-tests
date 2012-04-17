2f2911de26dc5435288611a1ad7ca23cbc4e6d40
Jaroslav Jankovič <jjankovi@redhat.com>
2012-04-17 13:43:05 +0000

    template of new test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40241 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/annotation/AnnotationPropertiesTest.java
+++ tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/annotation/AnnotationPropertiesTest.java
@@ -11,10 +11,7 @@
 
 package org.jboss.tools.ws.ui.bot.test.annotation;
 
-import org.jboss.tools.ui.bot.ext.SWTJBTExt;
 import org.jboss.tools.ws.ui.bot.test.rest.RESTfulTestBase;
-import org.jboss.tools.ws.ui.bot.test.rest.explorer.RESTfulExplorerTest;
-import org.jboss.tools.ws.ui.bot.test.uiutils.views.AnnotationProperties;
 import org.junit.Test;
 
 /**
@@ -46,17 +43,6 @@ public class AnnotationPropertiesTest extends RESTfulTestBase {
 	 */
 	@Test
 	public void testAbsenceOfAnnotation() {
-		
-		/*
-		resourceHelper.copyResourceToClass(bot.editorByTitle(getWsName() + ".java"), 
-										   RESTfulExplorerTest.class.
-										   getResourceAsStream(BASIC_WS_RESOURCE), 
-										   false, getWsPackage(), getWsName());
-		
-		SWTJBTExt.selectTextInSourcePane(bot, getWsName() + ".java", getWsName(), 0, 0);
-		
-		AnnotationProperties annotProperties = new AnnotationProperties();
-		*/
 		
 	}
 	
