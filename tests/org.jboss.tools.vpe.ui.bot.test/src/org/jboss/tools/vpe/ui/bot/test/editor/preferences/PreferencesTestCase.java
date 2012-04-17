3c136a72197b6adb7fa733b141a33e3611315ede
Yahor Radtsevich <yradtsevich@exadel.com>
2012-04-17 09:22:35 +0000

    https://issues.jboss.org/browse/JBIDE-11302 -- adding junit.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40230 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/preferences/PreferencesTestCase.java
+++ tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/preferences/PreferencesTestCase.java
@@ -38,7 +38,8 @@ public abstract class PreferencesTestCase extends VPEAutoTestCase{
 	protected static final String PREF_TOOLTIP = "Preferences"; //$NON-NLS-1$
 	protected static final String PREF_FILTER_SHELL_TITLE = "Preferences (Filtered)"; //$NON-NLS-1$
 	protected static final String SCROLL_LOCK_TOOLTIP = "Synchronize scrolling between source and visual panes"; //$NON-NLS-1$
-	protected static final String I18N_BUTTON_TOOLTIP = "Externalize selected string...(" + //$NON-NLS-1$
+	protected static final String I18N_BUTTON_TOOLTIP1 = "Externalize selected string..."; //$NON-NLS-1$
+	protected static final String I18N_BUTTON_TOOLTIP = "Externalize selected string... (" + //$NON-NLS-1$
 			(SWTJBTExt.isRunningOnMacOs() ? "⌘7" : "Ctrl+7") + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	protected static final String SHOW_VISUAL_TOOLBAR_PREFS_CHECKBOX_NAME = "Show toolbar within the editor (otherwise in Eclipse's toolbar)"; //$NON-NLS-1$
 	
