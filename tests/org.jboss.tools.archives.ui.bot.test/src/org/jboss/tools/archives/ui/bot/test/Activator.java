361da02949a6b21cba43dca144f2f1badec84f58
Jaroslav Jankovič <jjankovi@redhat.com>
2012-05-16 14:23:21 +0000

    archives bot test is now included as module in tests' pom.xml
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41069 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.archives.ui.bot.test/src/org/jboss/tools/archives/ui/bot/test/Activator.java
+++ tests/org.jboss.tools.archives.ui.bot.test/src/org/jboss/tools/archives/ui/bot/test/Activator.java
@@ -1,12 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.archives.ui.bot.test;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+/**
+ * 
+ * @author jjankovi
+ *
+ */
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
 
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.jboss.tools.archives.ui.bot.test";
+	
 	static BundleContext getContext() {
 		return context;
 	}
