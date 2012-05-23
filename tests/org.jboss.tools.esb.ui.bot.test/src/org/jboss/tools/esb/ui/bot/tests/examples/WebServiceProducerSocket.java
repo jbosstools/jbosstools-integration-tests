00b5b363cb88dded8563672b216edc3e4fa9223f
Len DiMaggio <ldimaggi@redhat.com>
2012-05-23 11:26:12 +0000

    Divided examples/WebServiceProducer.java into 3 tests - to avoid server intermittent logging issues
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41282 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.esb.ui.bot.test/src/org/jboss/tools/esb/ui/bot/tests/examples/WebServiceProducerSocket.java
@@ -0,0 +1,28 @@
+package org.jboss.tools.esb.ui.bot.tests.examples;
+
+import org.jboss.tools.ui.bot.ext.SWTTestExt;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Server;
+import org.jboss.tools.ui.bot.ext.config.Annotations.ServerState;
+import org.jboss.tools.ui.bot.ext.config.Annotations.ServerType;
+
+@Require(server=@Server(type=ServerType.SOA,state=ServerState.Running),perspective="Java")
+public class WebServiceProducerSocket extends ESBExampleTest {
+	@Override
+	public String getExampleName() {
+		return "JBoss ESB Web Service producer Example";
+	}
+	public String[] getProjectNames() {
+		return new String[] {"webservice_producer","webservice_producer_client"};
+	}
+	@Override
+	protected void executeExample() {
+		super.executeExample();	
+		
+		String text = executeClientGetServerOutput("org.jboss.soa.esb.samples.quickstart.webserviceproducer.test.SendMessage","socket 8888");
+		assertNotNull("Calling Send message failed, nothing appended (socket) to server log",text);	
+		assertTrue("Calling Send message failed, unexpected server output :"+text,text.contains("Ah Goodbye then!!!!"));
+		
+		SWTTestExt.servers.removeAllProjectsFromServer();
+	}
+}
