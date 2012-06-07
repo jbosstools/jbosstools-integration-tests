2803ecab7e74ab063115b441a9939197227609cc
Jaroslav Jankovič <jjankovi@redhat.com>
2012-06-07 15:38:28 +0000

    new resource projects for ApplicationAnnotationTest added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41774 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.ws.ui.bot.test/resources/projects/app1/src/test/App.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/path")
+public class App extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(RestService.class);
+		return super.getClasses();
+	}
+	
+}
---
+++ tests/org.jboss.tools.ws.ui.bot.test/resources/projects/app2/src/test/RestService.java
@@ -0,0 +1,14 @@
+package test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/rest")
+public class RestService {
+
+	@GET
+	public void method() {
+		
+	}
+	
+}
