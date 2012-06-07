5ca35299ed059a06f06b23900968929dd87ac4bd
Lucia Jelinkova <ljelinko@redhat.com>
2012-06-07 13:45:32 +0000

    Added method getToolbarButtons to fix Juno issues with SWTBot (fix taken from Eclipse Bug 375598 proposed patch)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41770 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/console/ConsoleClearingTask.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/console/ConsoleClearingTask.java
@@ -1,6 +1,5 @@
 package org.jboss.tools.portlet.ui.bot.task.console;
 
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarButton;
 import org.jboss.tools.portlet.ui.bot.task.AbstractSWTTask;
 import org.jboss.tools.ui.bot.ext.SWTBotFactory;
@@ -17,9 +16,8 @@ public class ConsoleClearingTask extends AbstractSWTTask {
 	@Override
 	public void perform() {
 		// does not use the ConsoleView.clear() funtionality because it didn't work for me 
-		SWTBotView view = SWTBotFactory.getConsole().show();
-		
-		for (SWTBotToolbarButton button : view.getToolbarButtons()){
+
+		for (SWTBotToolbarButton button : SWTBotFactory.getConsole().getToolbarButtons()){
 			if (button.isEnabled() && IDELabel.ConsoleView.BUTTON_CLEAR_CONSOLE_TOOLTIP.equals(button.getToolTipText())){
 				button.click();
 			}
