f9b284eb995719288212aa7111d2fb1c50b4898a
Lucia Jelinkova <ljelinko@redhat.com>
2012-06-08 14:14:36 +0000

    Added first dummy test.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41816 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/AllTestsSuite.java
@@ -0,0 +1,13 @@
+package org.jboss.ide.eclipse.as.ui.bot.test;
+
+import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(RequirementAwareSuite.class)
+@Suite.SuiteClasses({
+		CreateServerTest.class
+})
+public class AllTestsSuite {
+
+}
---
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/CreateServerTest.java
@@ -0,0 +1,12 @@
+package org.jboss.ide.eclipse.as.ui.bot.test;
+
+import org.jboss.tools.ui.bot.ext.SWTTestExt;
+import org.junit.Test;
+
+public class CreateServerTest extends SWTTestExt {
+
+	@Test
+	public void test(){
+		System.out.println("TEST");
+	}
+}
