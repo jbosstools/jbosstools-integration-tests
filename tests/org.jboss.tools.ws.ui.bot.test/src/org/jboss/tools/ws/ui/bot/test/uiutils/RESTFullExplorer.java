adcd4f3531251738408ab970d9064fd2d02574da
Jiri Peterka <peterka.jiri@gmail.com>
2012-06-21 09:04:16 +0000

    Dummy classes added to avoid compilation problem, real classes to be added  later
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42137 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/uiutils/RESTFullExplorer.java
@@ -0,0 +1,46 @@
+package org.jboss.tools.ws.ui.bot.test.uiutils;
+
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+
+public class RESTFullExplorer {
+
+	public RESTFullExplorer(String projectName) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getRestServiceName(SWTBotTreeItem restService) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPathForRestFulService(SWTBotTreeItem restService) {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public Object getConsumesInfo(SWTBotTreeItem restService) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getProducesInfo(SWTBotTreeItem restService) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SWTBotTreeItem[] getAllRestServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object restService(String webService) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RunOnServerDialog runOnServer(Object restService) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ tests/org.jboss.tools.ws.ui.bot.test/src/org/jboss/tools/ws/ui/bot/test/uiutils/RunOnServerDialog.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.ws.ui.bot.test.uiutils;
+
+public class RunOnServerDialog {
+
+	public class Server1 {
+		public class Server2 {
+			public void finish() {
+			}
+		}
+
+		public Server2 selectServer(String str) {
+			return new Server2();
+
+		}
+
+	}
+
+	public Server1 chooseExistingServer() {
+		return new Server1();
+	}
+
+}
