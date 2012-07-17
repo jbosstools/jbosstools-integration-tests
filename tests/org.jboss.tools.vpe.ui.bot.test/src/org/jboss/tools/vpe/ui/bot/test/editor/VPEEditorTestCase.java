92a1f951774d671716135c2e9f91f67d96467ab2
Vlado Pakan <vpakan@redhat.com>
2012-07-17 15:02:01 +0000

    Remove changes from revision 42175. All tests from /trunk/vpe/tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/ were broken.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42620 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/VPEEditorTestCase.java
+++ tests/org.jboss.tools.vpe.ui.bot.test/src/org/jboss/tools/vpe/ui/bot/test/editor/VPEEditorTestCase.java
@@ -10,11 +10,17 @@
  ******************************************************************************/
 package org.jboss.tools.vpe.ui.bot.test.editor;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.jboss.tools.ui.bot.ext.Timing;
 import org.jboss.tools.ui.bot.test.WidgetVariables;
+import org.jboss.tools.vpe.ui.bot.test.Activator;
 import org.jboss.tools.vpe.ui.bot.test.VPEAutoTestCase;
 import org.jboss.tools.vpe.ui.bot.test.tools.SWTBotWebBrowser;
 
@@ -48,26 +54,15 @@ public abstract class VPEEditorTestCase extends VPEAutoTestCase{
 	protected void closeUnuseDialogs() {
 	}
 	
-//	@Override
-//	/**
-//	 * Path "trunk\vpe\tests\org.jboss.tools.vpe.ui.bot.test\editor\"
-//	 * does not exist. The code is obsolete.
-//	 * So there is no need in the overridden method.
-//	 */
-//	protected String getPathToResources(String pagePathNextToPluginBundlePath) throws IOException {
-//		String pluginBundlePath = FileLocator.toFileURL(Platform.getBundle(Activator.PLUGIN_ID).getEntry("/")).getFile(); 
-//		String filePath = pluginBundlePath +"resources/editor/"+pagePathNextToPluginBundlePath; //$NON-NLS-1$
-//		File file = new File(filePath);
-//		/*
-//		 * Path "trunk\vpe\tests\org.jboss.tools.vpe.ui.bot.test\editor\"
-//		 * does not exist. The code is obsolete.
-//		 */
-//		if (!file.isFile()) {
-//			filePath = pluginBundlePath +"editor/"+testPage;  //$NON-NLS-1$
-//			file = new File(filePath);
-//		}
-//		return filePath;
-//	}
+	@Override
+	protected String getPathToResources(String testPage) throws IOException {
+		String filePath = FileLocator.toFileURL(Platform.getBundle(Activator.PLUGIN_ID).getEntry("/")).getFile()+"resources/editor/"+testPage; //$NON-NLS-1$ //$NON-NLS-2$
+		File file = new File(filePath);
+		if (!file.isFile()) {
+			filePath = FileLocator.toFileURL(Platform.getBundle(Activator.PLUGIN_ID).getEntry("/")).getFile()+"editor/"+testPage;  //$NON-NLS-1$//$NON-NLS-2$
+		}
+		return filePath;
+	}
 	
 	protected void openPage(){
 		SWTBot innerBot = bot.viewByTitle(WidgetVariables.PACKAGE_EXPLORER).bot();
