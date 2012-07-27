24cf8a1ad4010264c63ac16bef5527ebc24a7783
Lucia Jelinkova <ljelinko@redhat.com>
2012-07-27 11:15:58 +0000

    Created
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42753 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/EAPCompatibilitySuite.java
@@ -0,0 +1,24 @@
+package org.jboss.ide.eclipse.as.ui.bot.test;
+
+import org.jboss.ide.eclipse.as.ui.bot.test.as7.CreateAS7Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as7.DeleteAS7Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as7.DeployJSPProject;
+import org.jboss.ide.eclipse.as.ui.bot.test.as7.HotDeployJSPFile;
+import org.jboss.ide.eclipse.as.ui.bot.test.as7.OperateAS7Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as7.UndeployJSPProject;
+import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(RequirementAwareSuite.class)
+@Suite.SuiteClasses({
+		CreateAS7Server.class, 
+		OperateAS7Server.class,
+		DeployJSPProject.class,
+		HotDeployJSPFile.class,
+		UndeployJSPProject.class,
+		DeleteAS7Server.class
+})
+public class EAPCompatibilitySuite {
+
+}
