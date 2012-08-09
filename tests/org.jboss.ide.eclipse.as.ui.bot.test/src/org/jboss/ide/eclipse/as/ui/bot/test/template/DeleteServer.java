7566ce211da9abb4e1010b64894b252d7667809c
Lucia Jelinkova <ljelinko@redhat.com>
2012-08-09 14:19:57 +0000

    Added requirement annotation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@42953 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/template/DeleteServer.java
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/template/DeleteServer.java
@@ -1,6 +1,8 @@
 package org.jboss.ide.eclipse.as.ui.bot.test.template;
 
 import org.jboss.tools.ui.bot.ext.SWTTestExt;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Server;
 import org.jboss.tools.ui.bot.ext.view.ServersView;
 import org.junit.Test;
 
@@ -10,6 +12,7 @@ import org.junit.Test;
  * @author Lucia Jelinkova
  *
  */
+@Require(server=@Server)
 public class DeleteServer extends SWTTestExt {
 
 	private ServersView serversView = new ServersView();
