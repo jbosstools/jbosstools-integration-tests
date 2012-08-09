fa64afee72dca7e96bf9e5ad6252228ef714a49a
Lucia Jelinkova <ljelinko@redhat.com>
2012-08-09 08:40:20 +0000

    Changed PageSourceMatcher so that it actually waits for the text to appear.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@42943 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/web/PageSourceMatcher.java
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/web/PageSourceMatcher.java
@@ -1,8 +1,11 @@
 package org.jboss.ide.eclipse.as.ui.bot.test.web;
 
+import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
+import org.eclipse.swtbot.swt.finder.widgets.TimeoutException;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 import org.jboss.tools.ui.bot.ext.SWTBotFactory;
+import org.jboss.tools.ui.bot.ext.condition.TaskDuration;
 import org.jboss.tools.ui.bot.ext.parts.SWTBotBrowserExt;
 
 public class PageSourceMatcher extends TypeSafeMatcher<String> {
@@ -11,13 +14,27 @@ public class PageSourceMatcher extends TypeSafeMatcher<String> {
 
 	private String url;
 	
+	private long timeout;
+	
 	public PageSourceMatcher() {
 		super();
+		timeout = 0;
 	}
 
+	public PageSourceMatcher(long timeout) {
+		super();
+		this.timeout = timeout;
+	}
+	
+	public PageSourceMatcher(TaskDuration timeout) {
+		super();
+		this.timeout = timeout.getTimeout();
+	}
+	
 	public PageSourceMatcher(String url) {
 		this();
 		this.url = url;
+		this.timeout = 0;
 	}
 	
 	@Override
@@ -27,16 +44,43 @@ public class PageSourceMatcher extends TypeSafeMatcher<String> {
 			browser.loadUrlToBrowser(url, SWTBotFactory.getBot());
 		}
 		
-		pageText = browser.getText();
-		if ("".equals(item)){
-			return pageText.equals(item);
+		try {
+			SWTBotFactory.getBot().waitUntil(new PageContainsTextCondition(browser, item), timeout);
+			return true;
+		} catch (TimeoutException e){
+			return false;
 		}
-		return pageText.contains(item);
 	}
 
 	@Override
 	public void describeTo(Description description) {
 		description.appendText("is on the page, but there was instead: ");
 		description.appendValue(pageText);
+	}
+	
+	private class PageContainsTextCondition extends DefaultCondition {
+
+		private SWTBotBrowserExt browser;
+		
+		private String expectedText;
+
+		public PageContainsTextCondition(SWTBotBrowserExt browser, String item) {
+			this.browser = browser;
+			this.expectedText = item;
+		}
+
+		@Override
+		public boolean test() throws Exception {
+			pageText = browser.getText();
+			if ("".equals(expectedText)){
+				return pageText.equals(expectedText);
+			}
+			return pageText.contains(expectedText);
+		}
+
+		@Override
+		public String getFailureMessage() {
+			return null;
+		}
 	}
 }
