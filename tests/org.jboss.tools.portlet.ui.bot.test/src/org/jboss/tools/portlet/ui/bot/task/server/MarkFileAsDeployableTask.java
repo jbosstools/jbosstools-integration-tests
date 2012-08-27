2604b8cba828e571149cbe1d16ba4547ddcece28
Lucia Jelinkova <ljelinko@redhat.com>
2012-08-27 07:19:48 +0000

    Using bot for closing the dialog.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@43240 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/server/MarkFileAsDeployableTask.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/task/server/MarkFileAsDeployableTask.java
@@ -2,13 +2,7 @@ package org.jboss.tools.portlet.ui.bot.task.server;
 
 import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellIsActive;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-
 import org.apache.log4j.Logger;
-import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
-import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.jboss.tools.portlet.ui.bot.entity.WorkspaceFile;
 import org.jboss.tools.portlet.ui.bot.task.AbstractSWTTask;
 import org.jboss.tools.portlet.ui.bot.task.workspace.FileContextMenuSelectingTask;
@@ -38,30 +32,9 @@ public class MarkFileAsDeployableTask extends AbstractSWTTask {
 		log.info("Waiting for confirmation shell to appear");
 		SWTBotFactory.getBot().waitUntil(shellIsActive("Really mark these resources as deployable?"));
 
-		// for the confirmation dialog select OK (the dialog is native and normal swtbot functions do now work)
-		try {
-			Robot robot = new Robot();
-			if (!isWindowsOS()){
-				log.info("Non Windows OS");
-				KeyboardFactory.getAWTKeyboard().pressShortcut(Keystrokes.RIGHT, Keystrokes.CR, Keystrokes.LF);
-			} else {
-				log.info("Windows OS");
-				robot.keyPress(KeyEvent.VK_RIGHT);
-				robot.keyRelease(KeyEvent.VK_RIGHT);
-				robot.keyPress(KeyEvent.VK_LEFT);
-				robot.keyRelease(KeyEvent.VK_LEFT);
-				robot.keyPress(KeyEvent.VK_ENTER);
-				robot.keyRelease(KeyEvent.VK_ENTER);
-			}
-		} catch (AWTException e) {
-			throw new IllegalStateException("Cannot create instance of " + Robot.class + " in order to close native dialog", e);
-		}
+		getBot().button("OK").click();
 		
 		log.info("Waiting for confirmation shell to disappear");
 		SWTBotFactory.getBot().waitWhile(shellIsActive("Really mark these resources as deployable?"));
-	}
-
-	private boolean isWindowsOS(){
-		return System.getProperty("os.name").toLowerCase().contains("win");
 	}
 }
