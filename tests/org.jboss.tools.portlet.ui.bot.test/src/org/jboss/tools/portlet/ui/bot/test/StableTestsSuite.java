52e74af6112a879585021848f20a61c066f45d81
Lucia Jelinkova <ljelinko@redhat.com>
2012-09-25 15:04:01 +0000

    Reverted back, tests are not ready
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44003 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/StableTestsSuite.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/StableTestsSuite.java
@@ -6,12 +6,6 @@ import org.jboss.tools.portlet.ui.bot.test.core.CreateJavaPortletProject;
 import org.jboss.tools.portlet.ui.bot.test.core.HotJavaPortletDeploymentGatein;
 import org.jboss.tools.portlet.ui.bot.test.core.LoadJavaPortletInBrowserJBPortal;
 import org.jboss.tools.portlet.ui.bot.test.core.RunJavaPortletOnServer;
-import org.jboss.tools.portlet.ui.bot.test.jsf.CreateJSFPortletGatein;
-import org.jboss.tools.portlet.ui.bot.test.jsf.CreateJSFPortletJBPortal;
-import org.jboss.tools.portlet.ui.bot.test.jsf.CreateJSFPortletProject;
-import org.jboss.tools.portlet.ui.bot.test.jsf.HotJSFPortletDeploymentGatein;
-import org.jboss.tools.portlet.ui.bot.test.jsf.LoadJSFPortletInBrowserJBPortal;
-import org.jboss.tools.portlet.ui.bot.test.jsf.RunJSFPortletOnServer;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -24,13 +18,7 @@ import org.junit.runners.Suite;
 	RunJavaPortletOnServer.class,
 	LoadJavaPortletInBrowserJBPortal.class,
 	HotJavaPortletDeploymentGatein.class,
-	CreateJSFPortletProject.class,
-	CreateJSFPortletJBPortal.class, 
-	CreateJSFPortletGatein.class, 
-	RunJSFPortletOnServer.class,
-	LoadJSFPortletInBrowserJBPortal.class,
-	HotJSFPortletDeploymentGatein.class
-	})
+})
 public class StableTestsSuite {
 
 }
