71fe03468f8a9df9212555b318ce779e5ac969a3
Lucia Jelinkova <ljelinko@redhat.com>
2012-09-25 13:51:52 +0000

    Added additional tests to stable suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@43994 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/StableTestsSuite.java
+++ tests/org.jboss.tools.portlet.ui.bot.test/src/org/jboss/tools/portlet/ui/bot/test/StableTestsSuite.java
@@ -6,6 +6,12 @@ import org.jboss.tools.portlet.ui.bot.test.core.CreateJavaPortletProject;
 import org.jboss.tools.portlet.ui.bot.test.core.HotJavaPortletDeploymentGatein;
 import org.jboss.tools.portlet.ui.bot.test.core.LoadJavaPortletInBrowserJBPortal;
 import org.jboss.tools.portlet.ui.bot.test.core.RunJavaPortletOnServer;
+import org.jboss.tools.portlet.ui.bot.test.jsf.CreateJSFPortletGatein;
+import org.jboss.tools.portlet.ui.bot.test.jsf.CreateJSFPortletJBPortal;
+import org.jboss.tools.portlet.ui.bot.test.jsf.CreateJSFPortletProject;
+import org.jboss.tools.portlet.ui.bot.test.jsf.HotJSFPortletDeploymentGatein;
+import org.jboss.tools.portlet.ui.bot.test.jsf.LoadJSFPortletInBrowserJBPortal;
+import org.jboss.tools.portlet.ui.bot.test.jsf.RunJSFPortletOnServer;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -17,7 +23,13 @@ import org.junit.runners.Suite;
 	CreateJavaPortletGatein.class,
 	RunJavaPortletOnServer.class,
 	LoadJavaPortletInBrowserJBPortal.class,
-	HotJavaPortletDeploymentGatein.class
+	HotJavaPortletDeploymentGatein.class,
+	CreateJSFPortletProject.class,
+	CreateJSFPortletJBPortal.class, 
+	CreateJSFPortletGatein.class, 
+	RunJSFPortletOnServer.class,
+	LoadJSFPortletInBrowserJBPortal.class,
+	HotJSFPortletDeploymentGatein.class
 	})
 public class StableTestsSuite {
 
