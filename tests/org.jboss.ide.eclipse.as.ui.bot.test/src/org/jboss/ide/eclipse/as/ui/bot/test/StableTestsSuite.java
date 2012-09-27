a586ae3a59b1015e2ca5f69a769b87c7dda44b3f
Lucia Jelinkova <ljelinko@redhat.com>
2012-09-27 10:38:53 +0000

    Added AS 7.0 and 6 to stable suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44087 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/StableTestsSuite.java
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/StableTestsSuite.java
@@ -1,5 +1,17 @@
 package org.jboss.ide.eclipse.as.ui.bot.test;
 
+import org.jboss.ide.eclipse.as.ui.bot.test.as6.CreateAS6Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as6.DeleteServerAS6Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as6.DeployJSPProjectAS6Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as6.HotDeployJSPFileAS6Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as6.OperateAS6Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as6.UndeployJSPProjectAS6Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as70.CreateAS70Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as70.DeleteServerAS70Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as70.DeployJSPProjectAS70Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as70.HotDeployJSPFileAS70Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as70.OperateAS70Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as70.UndeployJSPProjectAS70Server;
 import org.jboss.ide.eclipse.as.ui.bot.test.as71.CreateAS71Server;
 import org.jboss.ide.eclipse.as.ui.bot.test.as71.DeleteServerAS71Server;
 import org.jboss.ide.eclipse.as.ui.bot.test.as71.DeployJSPProjectAS71Server;
@@ -18,6 +30,20 @@ import org.junit.runners.Suite;
 		HotDeployJSPFileAS71Server.class,
 		UndeployJSPProjectAS71Server.class,
 		DeleteServerAS71Server.class,
+		
+		CreateAS70Server.class,
+		OperateAS70Server.class,
+		DeployJSPProjectAS70Server.class,
+		HotDeployJSPFileAS70Server.class,
+		UndeployJSPProjectAS70Server.class,
+		DeleteServerAS70Server.class,
+		
+		CreateAS6Server.class,
+		OperateAS6Server.class,
+		DeployJSPProjectAS6Server.class,
+		HotDeployJSPFileAS6Server.class,
+		UndeployJSPProjectAS6Server.class,
+		DeleteServerAS6Server.class,
 })
 public class StableTestsSuite {
 
