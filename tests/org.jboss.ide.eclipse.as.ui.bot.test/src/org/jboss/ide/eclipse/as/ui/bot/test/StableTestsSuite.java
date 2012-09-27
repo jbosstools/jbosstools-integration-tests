c3a20ed75f40937e0d105ae23e69ae4f2d0353e4
Lucia Jelinkova <ljelinko@redhat.com>
2012-09-27 09:02:45 +0000

    Created stable suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44083 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/StableTestsSuite.java
@@ -0,0 +1,24 @@
+package org.jboss.ide.eclipse.as.ui.bot.test;
+
+import org.jboss.ide.eclipse.as.ui.bot.test.as71.CreateAS71Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as71.DeleteServerAS71Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as71.DeployJSPProjectAS71Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as71.HotDeployJSPFileAS71Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as71.OperateAS71Server;
+import org.jboss.ide.eclipse.as.ui.bot.test.as71.UndeployJSPProjectAS71Server;
+import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(RequirementAwareSuite.class)
+@Suite.SuiteClasses({
+		CreateAS71Server.class,
+		OperateAS71Server.class,
+		DeployJSPProjectAS71Server.class,
+		HotDeployJSPFileAS71Server.class,
+		UndeployJSPProjectAS71Server.class,
+		DeleteServerAS71Server.class,
+})
+public class StableTestsSuite {
+
+}
