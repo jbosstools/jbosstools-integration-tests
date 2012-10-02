acfcca6eb72ffe1a9dc211822fb4c92d5f0d7c14
Jaroslav Jankovič <jjankovi@redhat.com>
2012-10-02 12:19:50 +0000

    new test project added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44211 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.ws.ui.bot.test/resources/projects/app4/src/test/App.java
@@ -0,0 +1,9 @@
+package test;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest")
+public class App {
+
+	
+}
--- tests/org.jboss.tools.ws.ui.bot.test/resources/projects/app5/src/test/App.java
+++ tests/org.jboss.tools.ws.ui.bot.test/resources/projects/app5/src/test/App.java
@@ -3,5 +3,6 @@ package test;
 import javax.ws.rs.core.Application;
 
 public class App extends Application {
+
 	
 }
