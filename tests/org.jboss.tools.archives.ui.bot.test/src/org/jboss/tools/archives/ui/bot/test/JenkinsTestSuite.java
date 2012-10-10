fd8de86fafecf7894e80906d38588b15ce1bbdf8
Jaroslav Jankovič <jjankovi@redhat.com>
2012-10-10 07:01:51 +0000

    adding tests to stable jenkins suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44396 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.archives.ui.bot.test/src/org/jboss/tools/archives/ui/bot/test/JenkinsTestSuite.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.archives.ui.bot.test;
+
+import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Suite of tests executed on jenkins slave
+ * @author Jaroslav Jankovic
+ */
+@RunWith(RequirementAwareSuite.class)
+@SuiteClasses({
+	ViewIsPresentTest.class,
+	VariousProjectsArchiving.class,
+	ArchiveViewReSwitchingTest.class,
+	BuildingArchiveNode.class,
+	BuildingProjectTest.class,
+	CreatingArchiveTest.class,
+	
+	/** Not stable yet
+	
+	FolderTest.class,
+	FilesetTest.class,
+	UserLibrariesFilesetTest.class,
+	ArchivesSupportTest.class,
+	
+	DeletingArchiveTest.class, 
+	ModifyingArchiveTest.class,
+	DeployingArchiveTest.class,
+	
+	**/
+})
+public class JenkinsTestSuite {
+		
+}
