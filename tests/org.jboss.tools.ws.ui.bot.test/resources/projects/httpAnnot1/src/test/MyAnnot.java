44099a864c261d51f1da489fac03cbc2a9fbbc0c
Jaroslav Jankovič <jjankovi@redhat.com>
2012-10-16 15:08:33 +0000

    new test projects added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44527 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.ws.ui.bot.test/resources/projects/httpAnnot1/src/test/MyAnnot.java
@@ -0,0 +1,14 @@
+package test;
+
+import javax.ws.rs.HttpMethod;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod
+public @interface MyAnnot {
+
+}
