e2f051eb06b04955515453184850e51493abc7d8
Jiri Peterka <peterka.jiri@gmail.com>
2012-11-20 19:27:17 +0100

    JBIDE-13120 Codegeneration test fixed and updated for multiple hibernate versions

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/test/generation/CreateCodeGenerationConfiguration40.java
@@ -0,0 +1,30 @@
+package org.jboss.tools.hb.ui.bot.test.generation;
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotCheckBox;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.hamcrest.Matcher;
+import org.jboss.tools.hb.ui.bot.test.HibernateBaseTest;
+import org.jboss.tools.ui.bot.ext.config.Annotations.DB;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
+import org.junit.Test;
+
+/**
+ * Hibernate code generation configuration ui bot test
+ * - Code generation configuration can be created
+ * - Source code can be generated via hibernate generation configuration
+ * @author jpeterka
+ * 
+ */
+@Require(db = @DB, clearProjects = true, perspective = "Hibernate")
+public class CreateCodeGenerationConfiguration40 extends CreateCodeGenerationConfiguration {
+
+	@Test
+	public void hibernateCodeGeneration40() {
+		setPrjName("hibernate40");
+		hibernateCodeGeneration();
+	}
+}
