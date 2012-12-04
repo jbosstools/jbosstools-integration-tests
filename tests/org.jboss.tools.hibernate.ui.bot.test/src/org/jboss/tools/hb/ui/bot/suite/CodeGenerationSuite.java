f70e7d03b5a01f5574d74b82d36c89beec3082ce
Jiri Peterka <peterka.jiri@gmail.com>
2012-12-04 20:30:36 +0100

    JBIDE-13236 - Hibernate/JPA annotation generation test added

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/CodeGenerationSuite.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hb/ui/bot/suite/CodeGenerationSuite.java
@@ -1,14 +1,15 @@
 package org.jboss.tools.hb.ui.bot.suite;
 
-import org.jboss.tools.hb.ui.bot.test.generation.CreateCodeGenerationConfiguration35;
-import org.jboss.tools.hb.ui.bot.test.generation.CreateCodeGenerationConfiguration36;
-import org.jboss.tools.hb.ui.bot.test.generation.CreateCodeGenerationConfiguration40;
+import org.jboss.tools.hb.ui.bot.test.generation.GenerateJPAHibernateAnnotationsContextMenu;
+import org.jboss.tools.hb.ui.bot.test.generation.GenerateJPAHibernateAnnotationsMenuBar;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(RequirementAwareSuite.class)
-@SuiteClasses({CreateCodeGenerationConfiguration35.class,CreateCodeGenerationConfiguration36.class,CreateCodeGenerationConfiguration40.class})
+//@SuiteClasses({CreateCodeGenerationConfiguration35.class,CreateCodeGenerationConfiguration36.class,CreateCodeGenerationConfiguration40.class})
+@SuiteClasses({GenerateJPAHibernateAnnotationsContextMenu.class, GenerateJPAHibernateAnnotationsMenuBar.class})
+//@SuiteClasses({GenerateJPAHibernateAnnotationsMenuBar.class})
 //@SuiteClasses({CreateCodeGenerationConfiguration40.class})
 //@SuiteClasses({JPADDLGenerationTest.class})
 //@SuiteClasses({JPAEntitiesGenerationTest.class})
