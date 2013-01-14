a6b250a1ee1b6c7f02db809892a5965ecbc34a41
Andrej Podhradsky <apodhrad@redhat.com>
2013-01-14 20:34:01 +0100

    JBIDE-13373: Allow to specify modeshape repository from commad line

--- tests/org.jboss.tools.modeshape.rest.ui.bot.test/src/org/jboss/tools/modeshape/rest/ui/bot/ext/ModeshapeBot.java
+++ tests/org.jboss.tools.modeshape.rest.ui.bot.test/src/org/jboss/tools/modeshape/rest/ui/bot/ext/ModeshapeBot.java
@@ -1,17 +1,21 @@
 package org.jboss.tools.modeshape.rest.ui.bot.ext;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
+import org.jboss.tools.modeshape.rest.ui.bot.ext.dialog.ModeshapeLocationDialog;
 import org.jboss.tools.modeshape.rest.ui.bot.ext.dialog.ModeshapePublishDialog;
 import org.jboss.tools.modeshape.rest.ui.bot.ext.dialog.ModeshapeServerDialog;
 import org.jboss.tools.modeshape.rest.ui.bot.ext.util.ModeshapeWebdav;
 import org.jboss.tools.modeshape.rest.ui.bot.ext.view.ModeshapeExplorer;
 import org.jboss.tools.modeshape.rest.ui.bot.ext.view.ModeshapeView;
 import org.jboss.tools.ui.bot.ext.SWTBotExt;
+import org.jboss.tools.ui.bot.ext.config.TestConfigurator;
 import org.jboss.tools.ui.bot.ext.gen.ActionItem.View.GeneralProjectExplorer;
 import org.jboss.tools.ui.bot.ext.gen.IView;
 
@@ -19,7 +23,7 @@ import org.jboss.tools.ui.bot.ext.gen.IView;
  * This class represents a bot for modeshape operations.
  * 
  * @author apodhrad
- *
+ * 
  */
 public class ModeshapeBot extends SWTBotExt {
 
@@ -29,6 +33,8 @@ public class ModeshapeBot extends SWTBotExt {
 	public static final String DEFAULT_PUBLISH_AREA = "/files";
 	public static final String DEFAULT_USER = "admin";
 	public static final String DEFAULT_PASSWORD = "admin";
+
+	private static final Logger log = Logger.getLogger(ModeshapeBot.class);
 
 	private String user;
 	private String password;
@@ -42,75 +48,73 @@ public class ModeshapeBot extends SWTBotExt {
 	protected ModeshapeView modeshapeView;
 
 	public ModeshapeBot() {
+		this(getDefaultRepository());
+	}
+
+	public ModeshapeBot(String repository) {
 		super();
 
-		setAuthorization(DEFAULT_USER, DEFAULT_PASSWORD);
-		setServerUrl(DEFAULT_SERVER_URL);
-		setRepository(DEFAULT_REPOSITORY);
-		setWorkspace(DEFAULT_WORKSPACE);
-		setPublishArea(DEFAULT_PUBLISH_AREA);
+		this.user = DEFAULT_USER;
+		this.password = DEFAULT_PASSWORD;
+		this.serverUrl = DEFAULT_SERVER_URL;
+		this.workspace = DEFAULT_WORKSPACE;
+		this.publishArea = DEFAULT_PUBLISH_AREA;
+		this.repository = repository;
 
 		modeshapeView = new ModeshapeView();
 		modeshapeWebdav = new ModeshapeWebdav();
-		
+
 		setModeshapeExplorer(GeneralProjectExplorer.LABEL);
+	}
+
+	private static String getDefaultRepository() {
+		String repository = TestConfigurator.currentConfig.getProperty("MODESHAPE");
+		if (repository != null) {
+			return repository.split(",")[0];
+		}
+		return DEFAULT_REPOSITORY;
 	}
 
 	public void setModeshapeExplorer(IView view) {
 		modeshapeExplorer = new ModeshapeExplorer(view);
 	}
 
-	public void setAuthorization(String user, String password) {
-		this.user = user;
-		this.password = password;
-	}
-
 	public String getServerUrl() {
 		return serverUrl;
-	}
-
-	public void setServerUrl(String serverUrl) {
-		this.serverUrl = serverUrl;
 	}
 
 	public String getRepository() {
 		return repository;
 	}
 
-	public void setRepository(String repository) {
-		this.repository = repository;
-	}
-
 	public String getWorkspace() {
 		return workspace;
-	}
-
-	public void setWorkspace(String workspace) {
-		this.workspace = workspace;
 	}
 
 	public String getPublishArea() {
 		return publishArea;
 	}
 
-	public void setPublishArea(String publishArea) {
-		this.publishArea = publishArea;
-	}
-
 	public void createModeShapeServer() {
+		log.info("Create Modeshape server with url " + serverUrl);
+
 		modeshapeView.show();
-		ModeshapeServerDialog dialog = modeshapeView.newServer();
-		dialog.setUrl(serverUrl);
-		dialog.setUser(user);
-		dialog.setPassword(password);
-		assertTrue("Couldn't connect to the server", dialog.testServerConnection());
-		dialog.finishWithWait();
-		assertTrue("Created server not visible in ModeShape view.",
-				modeshapeView.containsServer(serverUrl));
+		// Don't create new server if it already exists
+		if (!modeshapeView.containsServer(serverUrl)) {
+			ModeshapeServerDialog dialog = modeshapeView.newServer();
+			dialog.setUrl(serverUrl);
+			dialog.setUser(user);
+			dialog.setPassword(password);
+			assertTrue("Couldn't connect to the server", dialog.testServerConnection());
+			dialog.finishWithWait();
+			assertTrue("Created server not visible in ModeShape view.",
+					modeshapeView.containsServer(serverUrl));
+		}
 		modeshapeView.reconnectServer(serverUrl);
 	}
 
 	public void deleteModeShapeServer() {
+		log.info("Delete Modeshape server with url " + serverUrl);
 		modeshapeView.show();
 		modeshapeView.deleteServer(serverUrl);
 		shell("Confirm Delete Server").bot().button("OK").click();
@@ -119,17 +123,23 @@ public class ModeshapeBot extends SWTBotExt {
 	}
 
 	public void publishFile(String... path) {
+		log.info("Publish " + toString(path));
 		modeshapeExplorer.publish(path);
 		fillPublishDialog("Publish to ModeShape");
 	}
 
 	public void unpublishFile(String... path) {
+		log.info("Unpublish " + toString(path));
 		modeshapeExplorer.unpublish(path);
 		fillPublishDialog("Unpublish from ModeShape");
 	}
 
 	public boolean isFilePublished(String path) throws IOException {
 		URL url = modeshapeWebdav.computeUrl(repository, workspace, publishArea, path);
+		return isFilePublished(url);
+	}
+
+	public boolean isFilePublished(URL url) throws IOException {
 		return modeshapeWebdav.isFileAvailable(url, user, password);
 	}
 
@@ -140,5 +150,31 @@ public class ModeshapeBot extends SWTBotExt {
 		publishDialog.setJcrWorkspace(workspace);
 		publishDialog.setPublishArea(publishArea);
 		publishDialog.finishWithWait();
+	}
+
+	public String getPublishLocation(String... path) {
+		String publishLocation = null;
+
+		log.info("Show publish location for " + toString(path));
+		modeshapeExplorer.showPublishedLocations(path);
+
+		ModeshapeLocationDialog locationDialog = new ModeshapeLocationDialog(
+				shell("Published Locations"));
+		assertEquals("Server URL doesn't match", serverUrl, locationDialog.getServerUrl());
+		assertEquals("User doesn't match", user, locationDialog.getUser());
+		assertEquals("Repository doesn't match", repository, locationDialog.getRepository());
+		assertEquals("Workspace doesn't match", workspace, locationDialog.getWorkspace());
+		publishLocation = locationDialog.getPublishedUrl();
+		locationDialog.clickOK();
+
+		return publishLocation;
+	}
+
+	private String toString(String... path) {
+		StringBuffer result = new StringBuffer();
+		for (int i = 0; i < path.length; i++) {
+			result.append(path[i]).append("/");
+		}
+		return result.toString();
 	}
 }
