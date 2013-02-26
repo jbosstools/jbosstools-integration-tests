f40ed2a2de3f22af689c6209d26d4bba13704a67
jjankovi <jjankovi@redhat.com>
2013-02-26 15:33:52 +0100

    WeldExtensionTest implemented

--- tests/org.jboss.tools.cdi.bot.test/src/org/jboss/tools/cdi/bot/test/editor/BeansEditor.java
+++ tests/org.jboss.tools.cdi.bot.test/src/org/jboss/tools/cdi/bot/test/editor/BeansEditor.java
@@ -21,7 +21,22 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.eclipse.ui.IEditorReference;
+import org.jboss.reddeer.swt.api.Table;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.table.DefaultTable;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.swt.impl.tree.ShellTreeItem;
+import org.jboss.reddeer.swt.impl.tree.ViewTreeItem;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.tools.cdi.reddeer.common.model.ui.AddIfClassAvailableDialog;
+import org.jboss.tools.cdi.reddeer.common.model.ui.AddIfSystemPropertyDialog;
+import org.jboss.tools.cdi.reddeer.common.model.ui.AddIncludeExcludeDialog;
 import org.jboss.tools.ui.bot.ext.SWTBotExt;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
 import org.jboss.tools.ui.bot.ext.widgets.SWTBotMultiPageEditor;
 
 /**
@@ -70,6 +85,101 @@ public class BeansEditor extends SWTBotMultiPageEditor {
 	
 	public String getSelectedItem() {
 		return bot().tree().selection().get(0, 0);
+	}
+	
+	public void newWeldScan() {
+		new ViewTreeItem(IDELabel.WebProjectsTree.BEANS_XML).select();
+		new ContextMenu("New", "Weld", "Scan...").select();
+	}
+	
+	public Table getIncludeExcludeTable() {
+		activateScanRoot();
+		return new DefaultTable();
+	}
+	
+	public AddIncludeExcludeDialog invokeAddIncludeExcludeDialog() {
+		activateScanRoot();
+		new ContextMenu("Add Include/Exclude").select();
+		new WaitUntil(new ShellWithTextIsActive("Add Include/Exclude"));
+		return new AddIncludeExcludeDialog();
+	}
+	
+	public void removeIncludeExclude(int index) {
+		getIncludeExcludeTable().select(index);
+		removeIncludeExcludeWithHandler();
+	}
+	
+	public void removeIncludeExclude(String name) {
+		Table table = getIncludeExcludeTable();
+		for (int i = 0; i < table.rowCount(); i++) {
+			if (table.cell(i, 1).equals(name)) {
+				table.select(i);
+				break;
+			}
+		}
+		removeIncludeExcludeWithHandler();
+	}
+	
+	public void editIncludeExclude(String oldName, String newName, boolean isRegular) {
+		Table table = getIncludeExcludeTable();
+		for (int i = 0; i < table.rowCount(); i++) {
+			if (table.cell(i, 1).equals(oldName)) {
+				table.select(i);
+				break;
+			}
+		};
+		editIncludeExcludeValues(newName, isRegular);
+	}
+	
+	public void editIncludeExclude(int index, String newName, boolean isRegular) {
+		getIncludeExcludeTable().select(index);
+		editIncludeExcludeValues(newName, isRegular);
+	}
+	
+	public AddIfClassAvailableDialog invokeAddClassAvailableDialog(String property) {
+		new ViewTreeItem("beans.xml", "Scan", property).select();
+		new ContextMenu("Add Class Available").select();
+		new WaitUntil(new ShellWithTextIsActive("Add If Class Available"));
+		return new AddIfClassAvailableDialog();
+	}
+	
+	public AddIfSystemPropertyDialog invokeAddIfSystemPropertyDialog(String property) {
+		new ViewTreeItem("beans.xml", "Scan", property).select();
+		new ContextMenu("Add System Property").select();
+		new WaitUntil(new ShellWithTextIsActive("Add If System Property"));
+		return new AddIfSystemPropertyDialog();
+	}
+	
+	public boolean isObjectInEditor(String... path) {
+		try {
+			new ViewTreeItem(path);
+			return true;
+		} catch (SWTLayerException exc) {
+			return false;
+		}
+	}
+	
+	private void editIncludeExcludeValues(String newName, boolean isRegular) {
+		new PushButton("Edit...").click();
+		new LabeledText("Name:").setText(newName);
+		new CheckBox().toggle(isRegular);
+	}
+	
+	private void activateScanRoot() {
+		try {
+			new ViewTreeItem(IDELabel.WebProjectsTree.BEANS_XML, "Scan").select();
+		} catch (SWTLayerException exc) {
+			// problem when finding beans.xml tree
+			// try to shell tree item with index 1
+			new ShellTreeItem(1, IDELabel.WebProjectsTree.BEANS_XML, "Scan").select();
+		}
+		
+	}
+	
+	private void removeIncludeExcludeWithHandler() {
+		new PushButton("Remove...").click();
+		new WaitUntil(new ShellWithTextIsActive("Confirmation"));
+		new PushButton("OK").click();
 	}
 	
 	private BeansEditor modify(Item item, String name, String actionLabel, DialogHandler h) {
