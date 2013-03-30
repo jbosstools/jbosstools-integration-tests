f00f90a1227ad989606584d6aa4fac873b2d0aa5
Marek Baluch <mbaluch@redhat.com>
2013-03-30 22:17:22 +0100

    Added initial commit for JBQA-7844
    Resolved JBQA-7844
    
    Removed zip file

---
+++ tests/org.jboss.tools.bpmn2.ui.bot.test/src/org/jboss/tools/bpmn2/ui/bot/test/wizard/BPMN2ModelWizardTest.java
@@ -0,0 +1,51 @@
+package org.jboss.tools.bpmn2.ui.bot.test.wizard;
+
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
+import org.jboss.reddeer.eclipse.jface.exception.JFaceLayerException;
+import org.jboss.tools.bpmn2.ui.bot.ext.wizard.BPMN2ModelWizard;
+import org.jboss.tools.bpmn2.ui.bot.ext.wizard.JBPMProjectLegacyWizard;
+import org.jboss.tools.ui.bot.ext.config.Annotations.JBPM;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Verify functionality of the bpmn2 model wizard.
+ * 
+ * @author Marek Baluch <mbaluch@redhat.com>
+ */
+@Require(jbpm = @JBPM(), runOnce = true)
+public class BPMN2ModelWizardTest {
+
+	@BeforeClass
+	public static void createProject() {
+		new JBPMProjectLegacyWizard().execute("TestProject");
+	}
+	
+	@AfterClass
+	public static void deleteProject() throws Exception {
+		new PackageExplorer().getProject("TestProject").delete(true);
+	}
+	
+	@Test
+	public void newModelTest() throws Exception {
+		new BPMN2ModelWizard().execute("SampleProcess.bpmn2", new String[] {"TestProject"});
+		Assert.assertTrue(new PackageExplorer().selectProject("TestProject").containsItem("SampleProcess.bpmn2"));
+	}
+	
+	@Test
+	public void newModelFormValidationTest() throws Exception {
+		BPMN2ModelWizard wizard = new BPMN2ModelWizard();
+		try {
+			wizard.execute("");
+		} catch (JFaceLayerException e) {
+			Assert.assertEquals("Button '&Finish' is not enabled", e.getMessage());
+		} finally {
+			wizard.cancel();
+		}
+	}
+	
+}
