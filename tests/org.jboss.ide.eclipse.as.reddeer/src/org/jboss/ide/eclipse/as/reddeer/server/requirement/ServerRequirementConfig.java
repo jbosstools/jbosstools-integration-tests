987be43d26af4aad0b3a48b80f47db7a6586b668
psrna <psrna@redhat.com>
2013-04-05 16:58:54 +0200

    JBIDE-13938 Add server types.

--- tests/org.jboss.ide.eclipse.as.reddeer/src/org/jboss/ide/eclipse/as/reddeer/server/requirement/ServerRequirementConfig.java
+++ tests/org.jboss.ide.eclipse.as.reddeer/src/org/jboss/ide/eclipse/as/reddeer/server/requirement/ServerRequirementConfig.java
@@ -6,18 +6,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name="server-requirement", namespace="http://www.jboss.org/NS/ServerReq")
 public class ServerRequirementConfig {
 	
-	private String version;
-	
 	private String runtime;
-
-	public String getVersion() {
-		return version;
-	}
-
-	@XmlElement(namespace="http://www.jboss.org/NS/ServerReq")
-	public void setVersion(String version) {
-		this.version = version;
-	}
 
 	public String getRuntime() {
 		return runtime;
@@ -27,4 +16,6 @@ public class ServerRequirementConfig {
 	public void setRuntime(String runtime) {
 		this.runtime = runtime;
 	}
+	
+	
 }
