595e6a6f5463ea13325cc75c060bbdfca4e4a992
psrna <psrna@redhat.com>
2013-04-10 12:28:43 +0200

    Removes unused imports.

--- tests/org.jboss.ide.eclipse.as.reddeer/src/org/jboss/ide/eclipse/as/reddeer/server/requirement/ServerRequirementConfig.java
+++ tests/org.jboss.ide.eclipse.as.reddeer/src/org/jboss/ide/eclipse/as/reddeer/server/requirement/ServerRequirementConfig.java
@@ -2,13 +2,11 @@ package org.jboss.ide.eclipse.as.reddeer.server.requirement;
 
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
 /**
  * 
