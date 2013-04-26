abad17ce36797ff070b0bb9b26c648a7b63fbc73
Marek Baluch <mbaluch@redhat.com>
2013-04-26 22:21:51 +0200

    Fixed isAvailable(int x, int y) method

--- tests/org.jboss.tools.bpmn2.itests/src/org/jboss/tools/bpmn2/itests/editor/constructs/other/SubProcess.java
+++ tests/org.jboss.tools.bpmn2.itests/src/org/jboss/tools/bpmn2/itests/editor/constructs/other/SubProcess.java
@@ -1,14 +1,14 @@
 package org.jboss.tools.bpmn2.itests.editor.constructs.other;
 
 import org.jboss.tools.bpmn2.itests.editor.ConstructType;
-import org.jboss.tools.bpmn2.itests.editor.constructs.Construct;
+import org.jboss.tools.bpmn2.itests.editor.constructs.ContainerConstruct;
 
 /**
  * TBD
  * 
  * @author Marek Baluch <mbaluch@redhat.com>
  */
-public class SubProcess extends Construct {
+public class SubProcess extends ContainerConstruct {
 
 	/**
 	 * 
