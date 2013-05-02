28f7320228bd4dd73d1f106fae64a670f29e7728
Marek Baluch <mbaluch@redhat.com>
2013-05-02 19:23:56 +0200

    Updated validators

--- tests/org.jboss.tools.bpmn2.itests/src/org/jboss/tools/bpmn2/itests/validator/JBPM5Validator.java
+++ tests/org.jboss.tools.bpmn2.itests/src/org/jboss/tools/bpmn2/itests/validator/JBPM5Validator.java
@@ -1,56 +1,27 @@
 package org.jboss.tools.bpmn2.itests.validator;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
+import javax.xml.transform.stream.StreamSource;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-/*
-import org.drools.compiler.BPMN2ProcessFactory;
-import org.drools.compiler.DroolsError;
-import org.drools.compiler.PackageBuilder;
-import org.drools.io.impl.ReaderResource;
-
-import org.jbpm.compiler.ProcessBuilderImpl;
-*/
+import org.jboss.tools.bpmn2.itests.test.Activator;
+import org.jboss.tools.ui.bot.ext.helper.ResourceHelper;
 
 /**
  * 
- * @author mbaluch
+ * @author Marek Baluch <mbaluch@redhat.com>
  */
-public class JBPM5Validator {
+public class JBPM5Validator extends BPMN2Validator {
 
-	/*
-	private Logger log = Logger.getLogger(JBPM5Validator.class);
+	@SuppressWarnings("unused")
+	private Logger log = Logger.getLogger(JBPM5Validator.class); 
 	
-	private List<DroolsError> errorList = new ArrayList<DroolsError>();
-	
-	public boolean validate(String xmlContent) {
-		errorList.clear();
+	public JBPM5Validator() {
+		super();
 		
-		Reader reader = new InputStreamReader(new ByteArrayInputStream(xmlContent.getBytes()));
-		PackageBuilder pb = new PackageBuilder();
-		BPMN2ProcessFactory.configurePackageBuilder(pb);
-		
-		ProcessBuilderImpl pbi = new ProcessBuilderImpl(pb);
-		errorList = pbi.addProcessFromXml(new ReaderResource(reader));
-
-		return errorList.isEmpty();
+		schemaList.add(new StreamSource(
+				ResourceHelper.getResourceAbsolutePath(
+						Activator.PLUGIN_ID, "resources/bpmn2/drools.xsd")));
 	}
 	
-	public List<String> getErrorList() {
-		return errorList;
-	}
-	
-	private void logIssues(Level level, String title, List<String> stringList) {
-		log.log(level, title);
-		for (String s : stringList) {
-			log.log(level, "\t" + s);
-		}
-	}
-	*/
 }
