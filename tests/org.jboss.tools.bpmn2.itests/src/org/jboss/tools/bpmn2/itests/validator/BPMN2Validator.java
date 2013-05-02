078d89e38c2a2959eeba65fe61f83eae5bd32a68
Marek Baluch <mbaluch@redhat.com>
2013-05-02 13:43:32 +0200

    Added modeling smoke tests and deleted test commited by mistake

--- tests/org.jboss.tools.bpmn2.itests/src/org/jboss/tools/bpmn2/itests/validator/BPMN2Validator.java
+++ tests/org.jboss.tools.bpmn2.itests/src/org/jboss/tools/bpmn2/itests/validator/BPMN2Validator.java
@@ -11,6 +11,8 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.jboss.tools.bpmn2.itests.test.Activator;
 import org.jboss.tools.ui.bot.ext.helper.ResourceHelper;
 import org.xml.sax.SAXException;
@@ -20,8 +22,10 @@ import org.xml.sax.SAXParseException;
  * 
  * @author Marek Baluch <mbaluch@redhat.com>
  */
-public class SchemaValidator {
+public class BPMN2Validator {
 
+	private static final Logger log = Logger.getLogger(BPMN2Validator.class);
+	
 	private final File xsd;
 	
 	private final List<String> warningList;
@@ -31,7 +35,7 @@ public class SchemaValidator {
 	/**
 	 * 
 	 */
-	public SchemaValidator() {
+	public BPMN2Validator() {
 		xsd = new File(ResourceHelper.getResourceAbsolutePath(
 				Activator.PLUGIN_ID, "resources/bpmn2/BPMN20.xsd"));
 		
@@ -48,14 +52,16 @@ public class SchemaValidator {
 	public boolean validate(String xmlContent) {
 		try {
 			SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-			
-			ErrorHandler eh = new ErrorHandler();
-			
 			Schema s = sf.newSchema(new StreamSource(xsd));
+
 			Validator v = s.newValidator();
-			v.setErrorHandler(eh);
+			v.setErrorHandler(new ErrorHandler());
 			v.validate(new StreamSource(new ByteArrayInputStream(xmlContent.getBytes())));
 
+			if (!fatalErrorList.isEmpty()) logIssues(Level.FATAL, "Fatal Errors:", fatalErrorList);
+			if (!errorList.isEmpty()) logIssues(Level.ERROR, "Errors:", errorList);
+			if (!warningList.isEmpty()) logIssues(Level.WARN, "Warnings:", warningList);
+			
 			return errorList.isEmpty() && fatalErrorList.isEmpty();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -85,6 +91,17 @@ public class SchemaValidator {
 	 */
 	public List<String> getFatalErrorList() {
 		return fatalErrorList;
+	}
+	
+	/**
+	 * 
+	 * @param stringList
+	 */
+	private void logIssues(Level level, String title, List<String> stringList) {
+		log.log(level, title);
+		for (String s : stringList) {
+			log.log(level, "\t" + s);
+		}
 	}
 	
 	/**
