9cb14387c204709d78b8968885b9bffb52ba51cc
Andrej Podhradsky <apodhrad@redhat.com>
2013-07-08 09:30:55 +0200

    JBIDE-15077: Adapt modeshape bot tests for Kepler

---
+++ tests/org.jboss.tools.modeshape.reddeer/src/org/jboss/tools/modeshape/reddeer/shell/PublishedLocations.java
@@ -0,0 +1,57 @@
+package org.jboss.tools.modeshape.reddeer.shell;
+
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.util.Bot;
+
+/**
+ * Published Locations Shell
+ * 
+ * @author apodhrad
+ *
+ */
+public class PublishedLocations extends DefaultShell {
+
+	public static final String DIALOG_TITLE = "";
+
+	public static final String LABEL_SERVER_URL = "Server URL";
+	public static final String LABEL_USER = "User";
+	public static final String LABEL_REPOSITORY = "Repository";
+	public static final String LABEL_WORKSPACE = "Workspace";
+	public static final String LABEL_PUBLISHED_URL = "Published URL";
+
+	public String getServerUrl() {
+		return getTableCell(LABEL_SERVER_URL);
+	}
+
+	public String getUser() {
+		return getTableCell(LABEL_USER);
+	}
+
+	public String getRepository() {
+		return getTableCell(LABEL_REPOSITORY);
+	}
+
+	public String getWorkspace() {
+		return getTableCell(LABEL_WORKSPACE);
+	}
+
+	public String getPublishedUrl() {
+		return getTableCell(LABEL_PUBLISHED_URL);
+	}
+
+	public void copyURL() {
+		new PushButton("Copy URL").click();
+	}
+
+	private String getTableCell(String label) {
+		SWTBotTable table = Bot.get().table();
+		int columnIndex = table.indexOfColumn(label);
+		return table.cell(0, columnIndex);
+	}
+
+	public void ok() {
+		new PushButton("OK").click();
+	}
+}
---
+++ tests/org.jboss.tools.modeshape.reddeer/src/org/jboss/tools/modeshape/reddeer/wizard/ServerWizard.java
@@ -0,0 +1,38 @@
+package org.jboss.tools.modeshape.reddeer.wizard;
+
+import org.jboss.reddeer.eclipse.jface.wizard.NewWizardDialog;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
+
+/**
+ * Wizard for creating a new general server.
+ * 
+ * @author apodhrad
+ * 
+ */
+public class ServerWizard extends NewWizardDialog {
+
+	private String[] type;
+	private String name;
+
+	public ServerWizard() {
+		super("Server", "Server");
+	}
+
+	public void setType(String[] type) {
+		this.type = type;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void execute() {
+		open();
+
+		new DefaultTreeItem(type).select();
+		new LabeledText("Server name:").setText(name);
+
+		finish();
+	}
+}
