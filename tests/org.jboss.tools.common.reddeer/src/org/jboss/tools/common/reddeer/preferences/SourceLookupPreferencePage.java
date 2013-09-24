4e3b31a354573b2286b42086634662a358bcd459
jniederm <jniederm@redhat.com>
2013-09-24 10:33:06 +0200

    common.reddeer project created

---
+++ tests/org.jboss.tools.common.reddeer/src/org/jboss/tools/common/reddeer/preferences/SourceLookupPreferencePage.java
@@ -0,0 +1,42 @@
+package org.jboss.tools.common.reddeer.preferences;
+
+import org.jboss.reddeer.eclipse.jface.preference.PreferencePage;
+import org.jboss.reddeer.swt.impl.button.RadioButton;
+
+/**
+ * Reddeer model of JBoss Tools > Source Lookup preference page
+ * 
+ * @author jniederm
+ */
+public class SourceLookupPreferencePage extends PreferencePage  {
+	
+	public SourceLookupPreferencePage() {
+		super("JBoss Tools", "Source Lookup");
+	}
+
+	@Override
+	public void open() {
+		super.open();
+	}
+	
+	public void setSourceAttachment(SourceLookupPreferencePage.SourceAttachmentEnum option) {
+		new RadioButton(option.getText()).click();
+		log.info("Attach sources '" + option.getText() + "' selected.");
+	}
+	
+	public enum SourceAttachmentEnum {
+		ALWAYS("Always"),
+		NEVER("Never"),
+		PROMPT("Prompt");
+		
+		private String text;
+		
+		SourceAttachmentEnum(String text) {
+			this.text = text;
+		}
+		
+		public String getText() {
+			return this.text;
+		}
+	}
+}
