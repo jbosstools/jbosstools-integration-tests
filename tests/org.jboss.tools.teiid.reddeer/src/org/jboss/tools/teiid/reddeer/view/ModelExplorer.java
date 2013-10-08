7b932574ceaeef41cf5286bd146bb50a0d5993ae
Lucie Fabrikova <lfabriko@redhat.com>
2013-10-08 12:56:00 +0200

    Fixed connection profile in ServerMgmtTest

--- tests/org.jboss.tools.teiid.reddeer/src/org/jboss/tools/teiid/reddeer/view/ModelExplorer.java
+++ tests/org.jboss.tools.teiid.reddeer/src/org/jboss/tools/teiid/reddeer/view/ModelExplorer.java
@@ -1,6 +1,9 @@
 package org.jboss.tools.teiid.reddeer.view;
 
 import org.jboss.reddeer.eclipse.jdt.ui.AbstractExplorer;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.tools.teiid.reddeer.ModelProject;
 import org.jboss.tools.teiid.reddeer.wizard.ModelProjectWizard;
 
@@ -38,6 +41,13 @@ public class ModelExplorer extends AbstractExplorer {
 	 */
 	public ModelProject getModelProject(String modelName) {
 		return new ModelProject(getProject(modelName));
+	}
+	
+	public void changeConnectionProfile(String connectionProfile, String projectName, String... projectItem){
+		new ModelExplorer().getProject(projectName).getProjectItem(projectItem).select();
+		new ContextMenu("Modeling", "Set Connection Profile").select();
+		new DefaultTreeItem("Database Connections", connectionProfile).select();
+		new PushButton("OK").click();
 	}
 
 }
