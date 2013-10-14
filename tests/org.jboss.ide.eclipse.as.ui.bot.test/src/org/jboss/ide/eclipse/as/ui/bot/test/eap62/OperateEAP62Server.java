ea5d5481a36aec0e6fc12d7603c0933f0e9fb8cd
Radim Hopp <rhopp@redhat.com>
2013-10-14 14:25:33 +0200

    Support for EAP 6.2 server adapter

---
+++ tests/org.jboss.ide.eclipse.as.ui.bot.test/src/org/jboss/ide/eclipse/as/ui/bot/test/eap62/OperateEAP62Server.java
@@ -0,0 +1,17 @@
+package org.jboss.ide.eclipse.as.ui.bot.test.eap62;
+
+import org.jboss.ide.eclipse.as.ui.bot.test.template.OperateServerTemplate;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Require;
+import org.jboss.tools.ui.bot.ext.config.Annotations.Server;
+import org.jboss.tools.ui.bot.ext.config.Annotations.ServerState;
+import org.jboss.tools.ui.bot.ext.config.Annotations.ServerType;
+
+@Require(server=@Server(type=ServerType.EAP, version="6.1", state=ServerState.NotRunning))
+public class OperateEAP62Server extends OperateServerTemplate {
+
+	@Override
+	public String getWelcomePageText() {
+		return "Welcome to EAP 6";
+	}
+
+}
