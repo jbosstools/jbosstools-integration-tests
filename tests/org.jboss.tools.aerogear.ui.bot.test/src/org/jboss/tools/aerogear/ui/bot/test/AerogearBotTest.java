df7a4c9cc6583438c669793771d348bc80db43d7
vpakan <vpakan@redhat.com>
2013-11-29 10:42:48 +0100

    Adds test for running Cordova app on android emulator
    Adds disclaimer

--- tests/org.jboss.tools.aerogear.ui.bot.test/src/org/jboss/tools/aerogear/ui/bot/test/AerogearBotTest.java
+++ tests/org.jboss.tools.aerogear.ui.bot.test/src/org/jboss/tools/aerogear/ui/bot/test/AerogearBotTest.java
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Copyright (c) 2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.aerogear.ui.bot.test;
 
 import java.util.List;
@@ -7,6 +17,7 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.jboss.tools.ui.bot.ext.SWTTestExt;
 import org.jboss.tools.ui.bot.ext.condition.NonSystemJobRunsCondition;
 import org.jboss.tools.ui.bot.ext.gen.INewObject;
+import org.jboss.tools.ui.bot.ext.types.PerspectiveType;
 import org.junit.After;
 import org.junit.Before;
 
@@ -98,6 +109,7 @@ public class AerogearBotTest extends SWTTestExt {
 
   @Before
   public void setUp() {
+    eclipse.openPerspective(PerspectiveType.JBOSS);
     createHTMLHybridMobileApplication(AerogearBotTest.CORDOVA_PROJECT_NAME,
         AerogearBotTest.CORDOVA_APP_NAME, "org.jboss.example.cordova");
 
