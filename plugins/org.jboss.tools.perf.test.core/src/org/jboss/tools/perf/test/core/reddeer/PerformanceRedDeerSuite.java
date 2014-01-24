11136ea284c134ae50db5acd64df2abb42b24287
vpakan <vpakan@redhat.com>
2014-01-24 10:11:54 +0100

    Adds Performance tests support

---
+++ plugins/org.jboss.tools.perf.test.core/src/org/jboss/tools/perf/test/core/reddeer/PerformanceRedDeerSuite.java
@@ -0,0 +1,22 @@
+package org.jboss.tools.perf.test.core.reddeer;
+
+import org.jboss.perf.test.client.listener.junit.PerfListener;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.junit.runners.model.RunnerBuilder;
+
+/**
+ * RedDeer testsuite runner measuring performance.
+ * 
+ * @author Vlado Pakan
+ */
+public class PerformanceRedDeerSuite extends RedDeerSuite {
+  final static PerfListener[] performanceListeners = new PerfListener[] {new PerfListener()};
+  
+  static{
+    runListeners = performanceListeners;
+  }
+  
+	public PerformanceRedDeerSuite(Class<?> klass , RunnerBuilder builder) throws Throwable {
+    super(klass,builder);
+  }
+}
