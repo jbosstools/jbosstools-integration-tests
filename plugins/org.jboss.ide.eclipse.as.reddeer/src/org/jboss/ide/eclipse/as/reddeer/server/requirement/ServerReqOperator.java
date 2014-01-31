d31b83360386cd52f264ee525f6e921acd42b5ab
rrabara <rrabara@redhat.com>
2014-01-31 09:45:35 +0100

    JBIDE-13938 Server requirements can define required type and version

---
+++ plugins/org.jboss.ide.eclipse.as.reddeer/src/org/jboss/ide/eclipse/as/reddeer/server/requirement/ServerReqOperator.java
@@ -0,0 +1,39 @@
+package org.jboss.ide.eclipse.as.reddeer.server.requirement;
+
+/**
+ * Enumeration of operators used to match server's version
+ * 
+ * @author Radoslav Rabara
+ *
+ */
+public enum ServerReqOperator {
+	/**
+	 * =
+	 */
+	EQUALS,
+	
+	/**
+	 * !=
+	 */
+	NOT_EQUAL,
+	
+	/**
+	 * >
+	 */
+	GREATER,
+	
+	/**
+	 * <
+	 */
+	LESS,
+	
+	/**
+	 * >=
+	 */
+	GREATER_OR_EQUAL,
+	
+	/**
+	 * <=
+	 */
+	LESS_OR_EQUAL
+}
