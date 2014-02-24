0934c73045968ac5f0909362a1dee8ea782a2ea1
rawagner <rawagner@redhat.com>
2014-02-24 10:11:51 +0100

    added JSF reddeer plugin

---
+++ plugins/org.jboss.tools.jsf.reddeer/src/org/jboss/tools/jsf/reddeer/ui/JSFNewProjectFirstPage.java
@@ -0,0 +1,21 @@
+package org.jboss.tools.jsf.reddeer.ui;
+
+import org.jboss.reddeer.eclipse.jface.wizard.WizardPage;
+import org.jboss.reddeer.swt.impl.combo.DefaultCombo;
+import org.jboss.reddeer.swt.impl.text.DefaultText;
+
+public class JSFNewProjectFirstPage extends WizardPage{
+	
+	public void setProjectName(String name){
+		new DefaultText(0).setText(name);
+	}
+	
+	public void setJSFVersion(String version){
+		new DefaultCombo(0).setSelection(version);
+	}
+	
+	public void setJSFType(String type){
+		new DefaultCombo(1).setSelection(type);
+	}
+
+}
