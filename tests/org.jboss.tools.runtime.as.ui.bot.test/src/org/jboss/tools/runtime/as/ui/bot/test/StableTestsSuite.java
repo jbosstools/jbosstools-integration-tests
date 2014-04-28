c46d8d495a9774dde51d0e1ed1d6cb3937209a1a
rrabara <rrabara@redhat.com>
2014-04-28 14:23:45 +0200

    Runtime-detection: products url and md5 are no longer in pom.xml
    They are defined in "product-download.properties" file.
    There is also defined "products" profile, which reads the property file
    and reads required urls and md5s.
    
    Updated README to inform about this change.

---
+++ tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/StableTestsSuite.java
@@ -0,0 +1,75 @@
+package org.jboss.tools.runtime.as.ui.bot.test;
+
+import org.jboss.tools.runtime.as.ui.bot.test.detector.seam.seam22.CheckSeam22;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.seam.seam22.DetectSeam22;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.seam.seam23.CheckSeam23;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.seam.seam23.DetectSeam23;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.seam.seam23x.CheckSeam23x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.seam.seam23x.DetectSeam23x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap60x.DetectEAP60x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap60x.OperateEAP60x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap61x.DetectEAP61x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap61x.OperateEAP61x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap62.DetectEAP62;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap62.OperateEAP62;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap62x.DetectEAP62x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap62x.OperateEAP62x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap63.DetectEAP63;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap63.OperateEAP63;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.jboss7.DetectJBoss7;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.jboss7.OperateJBoss7;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.jpp61x.DetectJPP61x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.jpp61x.OperateJPP61x;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.wildfly8.DetectWildFly8;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.wildfly8.OperateWildFly8;
+import org.jboss.tools.runtime.as.ui.bot.test.download.RuntimeDownload;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Stable Tests Suite
+ * 
+ * Contains all mavenized tests
+ * 
+ * @author Radoslav Rabara
+ */
+@RunWith(JBTSuite.class)
+@SuiteClasses({
+		RuntimeDownload.class,
+		
+		DetectWildFly8.class,
+		OperateWildFly8.class,
+		
+		DetectJBoss7.class,
+		OperateJBoss7.class,
+		
+		DetectEAP63.class,
+		OperateEAP63.class,
+		
+		DetectEAP62x.class,
+		OperateEAP62x.class,
+		
+		DetectEAP62.class,
+		OperateEAP62.class,
+		
+		DetectEAP61x.class,
+		OperateEAP61x.class,
+		
+		DetectEAP60x.class,
+		OperateEAP60x.class,
+		
+		DetectJPP61x.class,
+		OperateJPP61x.class,
+		
+		DetectSeam23x.class,
+		CheckSeam23x.class,
+		
+		DetectSeam23.class,
+		CheckSeam23.class,
+		
+		DetectSeam22.class,
+		CheckSeam22.class
+})
+public class StableTestsSuite {
+
+}
