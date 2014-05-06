1ef0222d3f8c0c5336cd80e1f9293e1b6328768b
rawagner <rawagner@redhat.com>
2014-05-06 17:18:33 +0200

    replace PreferencePage for WorkbenchPreferencePage due to RD refactoring

--- plugins/org.jboss.tools.maven.reddeer/src/org/jboss/tools/maven/reddeer/maven/ui/preferences/ConfiguratorPreferencePage.java
+++ plugins/org.jboss.tools.maven.reddeer/src/org/jboss/tools/maven/reddeer/maven/ui/preferences/ConfiguratorPreferencePage.java
@@ -1,10 +1,10 @@
 package org.jboss.tools.maven.reddeer.maven.ui.preferences;
 
-import org.jboss.reddeer.eclipse.jface.preference.PreferencePage;
 import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.workbench.preference.WorkbenchPreferencePage;
 import org.jboss.tools.maven.reddeer.wizards.ConfigureMavenRepositoriesWizard;
 
-public class ConfiguratorPreferencePage extends PreferencePage{
+public class ConfiguratorPreferencePage extends WorkbenchPreferencePage{
 	
 	public ConfiguratorPreferencePage(){
 		super("JBoss Tools","JBoss Maven Integration");
