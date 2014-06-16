4bf4c9efb4df5932a7880d7d6b9336daf8b010cb
Jiri Peterka <peterka.jiri@gmail.com>
2014-06-16 18:21:16 +0200

    Test for mavenized  hibernate project added

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/MavenizedProjectTest.java
@@ -0,0 +1,50 @@
+package org.jboss.tools.hibernate.reddeer.test;
+
+import org.jboss.reddeer.eclipse.ui.problems.ProblemsView;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
+import org.jboss.reddeer.swt.condition.JobIsRunning;
+import org.jboss.reddeer.swt.wait.WaitWhile;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Mavenized project tests that hibernate project can be
+ * imported without errors
+ * @author Jiri Peterka
+ *
+ */
+@RunWith(RedDeerSuite.class)
+@CleanWorkspace
+public class MavenizedProjectTest extends HibernateRedDeerTest {
+
+	@Test
+	public void testHibernateMavenProject35() {
+		importHibernateMavenProject("mvn-hibernate35");
+	}
+
+	@Test
+	public void testHibernateMavenProject36() {
+		importHibernateMavenProject("mvn-hibernate36");
+	}
+
+	@Test
+	public void testHibernateMavenProject40() {
+		importHibernateMavenProject("mvn-hibernate40");
+	}
+
+	@Test
+	public void testHibernateMavenProject43() {
+		importHibernateMavenProject("mvn-hibernate43");
+	}
+	
+	private void importHibernateMavenProject(String projectName) {
+		importProject(projectName);
+		new WaitWhile(new JobIsRunning());
+		ProblemsView problemsView = new ProblemsView();
+		assertTrue("No problems after import are expected", problemsView.getAllErrors().size() == 0);
+	}
+}
