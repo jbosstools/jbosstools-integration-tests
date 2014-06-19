0b3fc0cb4382aace0df32a14f786630639d3c480
Jiri Peterka <peterka.jiri@gmail.com>
2014-06-19 18:28:15 +0200

    Datasource wizard test implemented and some refactoring done

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JBossDatasourceTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JBossDatasourceTest.java
@@ -1,13 +1,22 @@
 package org.jboss.tools.hibernate.reddeer.test;
 
+import static org.junit.Assert.assertFalse;
+
+import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
 import org.jboss.reddeer.requirements.db.DatabaseConfiguration;
 import org.jboss.reddeer.requirements.db.DatabaseRequirement;
 import org.jboss.reddeer.requirements.db.DatabaseRequirement.Database;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
+import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
 import org.jboss.tools.hibernate.factory.ConnectionProfileFactory;
 import org.jboss.tools.hibernate.factory.DriverDefinitionFactory;
+import org.jboss.tools.hibernate.reddeer.wizard.JBossDatasourceWizard;
+import org.jboss.tools.hibernate.reddeer.wizard.NewJBossDatasourceWizardPage;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,17 +28,43 @@ import org.junit.runner.RunWith;
  */
 @RunWith(RedDeerSuite.class)
 @Database(name="testdb")
-public class JBossDatasourceTest {
+@CleanWorkspace
+public class JBossDatasourceTest extends HibernateRedDeerTest {
+	
+	public static final String PRJ = "mvn-hibernate35";
 
     @InjectRequirement
     DatabaseRequirement dbRequirement;
     
-	@Test
-	public void testJBossDatasource() {
+	@Before
+	public void prepare() {
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		DriverDefinitionFactory.createDatabaseDefinition(cfg);
 		ConnectionProfileFactory.createConnectionProfile(cfg);
+		
+		importProject(PRJ);
 	}
+	
+	@Test
+	public void jbossDatasourceTest() {
+		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
+
+		JBossDatasourceWizard wizard = new JBossDatasourceWizard();
+		wizard.open();
+		NewJBossDatasourceWizardPage page = wizard.getFirstPage();
+		page.setConnectionProfile(cfg.getProfileName());
+		page.setParentFolder("/" + PRJ + "/src/main/resources");
+		page.finish();
+
+		ProjectExplorer pe = new ProjectExplorer();
+		pe.open();
+		String dsFileName = cfg.getProfileName() + "-ds.xml";
+		DefaultTreeItem item = new DefaultTreeItem(PRJ,"src","main","resources",dsFileName);
+		item.doubleClick();
+		
+		assertFalse(new DefaultEditor(dsFileName).isDirty());
+	}
+		
 	
 	
 	@After
