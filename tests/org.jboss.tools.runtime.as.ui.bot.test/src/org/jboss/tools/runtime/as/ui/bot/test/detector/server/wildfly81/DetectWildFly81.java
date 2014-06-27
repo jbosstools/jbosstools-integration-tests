fddc5bd7ce78d3febda14c8bfb16933e8b791685
rrabara <rrabara@redhat.com>
2014-06-27 15:13:39 +0200

    Runtime-detection: Tests uses WidlFly 8.1.Final
    Project download test tests download of 8.1 insteadl of 8.0
    
    WildFly 8.1 runtime test uses final version instead of CR1

--- tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/detector/server/wildfly81/DetectWildFly81.java
+++ tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/detector/server/wildfly81/DetectWildFly81.java
@@ -11,7 +11,7 @@ public class DetectWildFly81 extends DetectRuntimeTemplate {
 
 	public static final String SERVER_ID = "wildfly-8.1";
 	
-	public static final String SERVER_NAME = "wildfly-8.1.0.CR1";
+	public static final String SERVER_NAME = "wildfly-8.1.0.Final";
 	
 	@Override
 	protected String getPathID() {
