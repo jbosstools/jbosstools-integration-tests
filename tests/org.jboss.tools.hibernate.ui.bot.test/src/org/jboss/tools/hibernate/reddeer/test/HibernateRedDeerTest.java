421d594c97b086385bd4fd1adfff84d2baf5e82b
Jiri Peterka <peterka.jiri@gmail.com>
2014-07-08 15:39:46 +0200

    Resource file source definitions unified for hibernate UI tests

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/HibernateRedDeerTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/HibernateRedDeerTest.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.hibernate.reddeer.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,20 +11,43 @@ import org.jboss.tools.hibernate.ui.bot.test.Activator;
 
 public class HibernateRedDeerTest {
 	
-	Properties p;
+	Properties p,links;
 	
 	public HibernateRedDeerTest() {
 		super();
 		p = new Properties();
-		String path = ResourceFactory.getResourceAbsolutePath(Activator.PLUGIN_ID, "resources/h2_settings.properties");
+		String path = ResourceFactory.getResourcesLocation(Activator.PLUGIN_ID, "h2_settings.properties");
 		try {
 			p.load(new FileInputStream(path));
 		} catch (IOException e) {
-			new RuntimeException("Cannot read property file");
+			new RuntimeException("Cannot read property file " + e.toString());
 		}
+		
+		links = new Properties();
+		String linksPath = ResourceFactory.getResourcesLocation(Activator.PLUGIN_ID, "links.properties");
+		try {
+			links.load(new FileInputStream(linksPath));
+		} catch (IOException e) {
+			new RuntimeException("Cannot read property file " + e.toString());
+		}
+		
+		assureResources("H2_DRIVER");
+		
 		
 	}
 	
+	private void assureResources(String string) {
+		String sourceVar = string + "_SOURCE";
+		String targetVar = string + "_TARGET";
+				
+		String sourceVal = links.getProperty(sourceVar);
+		String[] separated = sourceVal.split("/");
+		String fileName = separated[separated.length - 1];
+		String targetVal = links.getProperty(targetVar)  + File.separator + fileName;
+		
+		ResourceFactory.assureResource(Activator.PLUGIN_ID, sourceVal,targetVal);
+	}
+
 	public void importProject(String prjName) {
 		ProjectImporter.importProjectWithoutErrors(Activator.PLUGIN_ID, prjName);
 	}
