30ccad6418a64713b5634fdd41188d4e1c4d31a1
Jiri Peterka <peterka.jiri@gmail.com>
2014-07-09 14:53:42 +0200

    Projects updated to avoid Eclipse issue with empty folders

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/test/HibernateAllTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/test/HibernateAllTest.java
@@ -1,11 +1,8 @@
 package org.jboss.tools.hibernate.ui.bot.test;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
-import org.jboss.tools.hibernate.reddeer.test.ConsoleConfigurationFileTest;
-import org.jboss.tools.hibernate.reddeer.test.ConsoleConfigurationTest;
 import org.jboss.tools.hibernate.reddeer.test.EmptyTest;
 import org.jboss.tools.hibernate.reddeer.test.HibernateUIPartsTest;
-import org.jboss.tools.hibernate.reddeer.test.JBossDatasourceTest;
 import org.jboss.tools.hibernate.reddeer.test.MavenizedProjectTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -14,9 +11,9 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
 
 	EmptyTest.class,
-	HibernateUIPartsTest.class /*
-	MavenizedProjectTest.class,	
-	JBossDatasourceTest.class,
+	HibernateUIPartsTest.class, 
+	MavenizedProjectTest.class	
+	/*JBossDatasourceTest.class,
 	ConsoleConfigurationFileTest.class,
 	ConsoleConfigurationTest.class,
 	*/
