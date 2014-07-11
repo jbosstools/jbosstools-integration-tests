34045f3657de583debb7cc24160e4caecf742cb8
rrabara <rrabara@redhat.com>
2014-07-11 11:27:03 +0200

    WebServices: Created ExtendedTextEditor which provides lines operations
    It extends TextEditor with operations like remove line, replace line,
    insert before line or get number of line that match the given matcher.

---
+++ plugins/org.jboss.tools.ws.reddeer/src/org/jboss/tools/ws/reddeer/editor/ExtendedTextEditor.java
@@ -0,0 +1,49 @@
+package org.jboss.tools.ws.reddeer.editor;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.core.StringContains;
+import org.jboss.reddeer.swt.keyboard.Keyboard;
+import org.jboss.reddeer.swt.keyboard.KeyboardFactory;
+import org.jboss.reddeer.workbench.impl.editor.TextEditor;
+
+public class ExtendedTextEditor extends TextEditor {
+
+	private final Keyboard keyboard = KeyboardFactory.getKeyboard();
+	
+	public void removeLine(String contains) {
+		int lineNumber = getLineNum(StringContains.containsString(contains));
+		
+		selectLine(lineNumber);
+		
+		keyboard.type(" ");
+
+		save();
+	}
+	
+	public void replaceLine(String newLine, String contains) {
+		int lineNumber = getLineNum(StringContains.containsString(contains));
+
+		selectLine(lineNumber);
+
+		keyboard.type(newLine + "\n");
+
+		save();
+	}
+
+	public int getLineNum(Matcher<String> matcher) {
+		for (int lineNum = 0; lineNum < getNumberOfLines(); lineNum++) {
+			String lineText = getTextAtLine(lineNum);
+			if (matcher.matches(lineText)) {
+				return lineNum;
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public void insertBeforeLine(String insertText, String afterTextContains) {
+		int lineNumber = getLineNum(StringContains
+				.containsString(afterTextContains));
+		insertLine(lineNumber, insertText);
+		save();
+	}
+}
