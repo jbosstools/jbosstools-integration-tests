e8ff35f4032a9ee88bac8c6872479e648975edda
Jiri Peterka <peterka.jiri@gmail.com>
2014-07-21 13:29:40 +0200

    Connection profiles related fails fixed in Hibernate tests

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/test/HibernateAllTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/test/HibernateAllTest.java
@@ -1,8 +1,12 @@
 package org.jboss.tools.hibernate.ui.bot.test;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.hibernate.reddeer.test.ConnectionProfileTest;
+import org.jboss.tools.hibernate.reddeer.test.ConsoleConfigurationFileTest;
+import org.jboss.tools.hibernate.reddeer.test.ConsoleConfigurationTest;
 import org.jboss.tools.hibernate.reddeer.test.EmptyTest;
 import org.jboss.tools.hibernate.reddeer.test.HibernateUIPartsTest;
+import org.jboss.tools.hibernate.reddeer.test.JBossDatasourceTest;
 import org.jboss.tools.hibernate.reddeer.test.MavenizedProjectTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -12,11 +16,11 @@ import org.junit.runners.Suite;
 
 	EmptyTest.class,
 	HibernateUIPartsTest.class, 
-	MavenizedProjectTest.class	
-	/*JBossDatasourceTest.class,
+	MavenizedProjectTest.class,
+	JBossDatasourceTest.class,
 	ConsoleConfigurationFileTest.class,
 	ConsoleConfigurationTest.class,
-	*/
+	ConnectionProfileTest.class
 	
 })
 public class HibernateAllTest {
