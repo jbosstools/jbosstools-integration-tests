17c7d3e193af6b5552e37c07513d954a311039fc
psrna <psrna@redhat.com>
2014-09-29 17:01:09 +0200

    Forge RedDeer base project and Forge Console view initial implementation

---
+++ plugins/org.jboss.tools.forge.reddeer/src/org/jboss/tools/forge/reddeer/view/ForgeConsoleView.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.forge.reddeer.view;
+
+import org.jboss.reddeer.workbench.impl.view.WorkbenchView;
+/**
+ * Forge Console view RedDeer implementation
+ * @author psrna
+ *
+ */
+public class ForgeConsoleView extends WorkbenchView{
+
+	public ForgeConsoleView() {
+		super("Forge Console");
+	}
+}
