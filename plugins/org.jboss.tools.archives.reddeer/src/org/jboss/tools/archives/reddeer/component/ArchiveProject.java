0d7073d44f111a0aecf1e1e3007a7c3894c6769e
rawagner <rawagner@redhat.com>
2014-12-15 08:31:41 +0100

    Use ViewToolItem instead of DefaultToolItem

--- plugins/org.jboss.tools.archives.reddeer/src/org/jboss/tools/archives/reddeer/component/ArchiveProject.java
+++ plugins/org.jboss.tools.archives.reddeer/src/org/jboss/tools/archives/reddeer/component/ArchiveProject.java
@@ -11,9 +11,11 @@
 package org.jboss.tools.archives.reddeer.component;
 
 import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.condition.JobIsRunning;
 import org.jboss.reddeer.swt.condition.TreeItemHasMinChildren;
 import org.jboss.reddeer.swt.wait.TimePeriod;
 import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.swt.wait.WaitWhile;
 import org.jboss.tools.archives.reddeer.archives.ui.NewJarDialog;
 
 /**
@@ -45,6 +47,7 @@ public class ArchiveProject {
 	public void buildProjectFull() {
 		archiveProject.select();
 		menuAction.buildProjectFull();
+		new WaitWhile(new JobIsRunning());
 	}
 	
 	public Archive getArchive(String archiveName) {
