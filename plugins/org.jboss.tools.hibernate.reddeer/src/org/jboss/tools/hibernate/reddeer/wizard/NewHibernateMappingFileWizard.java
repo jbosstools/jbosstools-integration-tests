bab2fd035c7f7a074d4c3299ae7176b49afb4f42
Jiri Peterka <peterka.jiri@gmail.com>
2015-01-05 17:09:27 +0100

    Hibernate Mapping hbm.xml file test reimplemented

---
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/wizard/NewHibernateMappingFileWizard.java
@@ -0,0 +1,36 @@
+package org.jboss.tools.hibernate.reddeer.wizard;
+
+import org.jboss.reddeer.jface.wizard.NewWizardDialog;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.exception.WaitTimeoutExpiredException;
+import org.jboss.reddeer.swt.impl.button.NoButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+
+/**
+ * New JPA Project Wizard
+ * @author Jiri Peterka
+ *
+ */
+public class NewHibernateMappingFileWizard extends NewWizardDialog {
+
+	
+	public NewHibernateMappingFileWizard() {
+		super("Hibernate", "Hibernate XML Mapping file (hbm.xml)");
+	}
+	
+	@Override
+	public void finish() {	
+		super.finish();
+		 
+		// handle "Open Associated Perspective?" dialog if it occurs
+		String title = "Open Associated Perspective?";
+		try {
+			new WaitUntil(new ShellWithTextIsActive(title));
+			new DefaultShell(title);
+			new NoButton().click();
+		} catch (WaitTimeoutExpiredException e) {
+			// do nothing
+		}
+	}
+}
---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/MappingFileTest.java
@@ -0,0 +1,107 @@
+package org.jboss.tools.hibernate.reddeer.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
+import org.jboss.reddeer.swt.impl.tree.DefaultTree;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
+import org.jboss.reddeer.swt.impl.tree.TreeItemNotFoundException;
+import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
+import org.jboss.tools.hibernate.reddeer.wizard.NewHibernateMappingElementsSelectionPage2;
+import org.jboss.tools.hibernate.reddeer.wizard.NewHibernateMappingFilePage;
+import org.jboss.tools.hibernate.reddeer.wizard.NewHibernateMappingFileWizard;
+import org.jboss.tools.hibernate.reddeer.wizard.NewHibernateMappingPreviewPage;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Hibernate mapping file (hbm.xml) test   
+ * @author Jiri Peterka
+ *
+ */
+@RunWith(RedDeerSuite.class)
+@CleanWorkspace
+public class MappingFileTest extends HibernateRedDeerTest {
+	
+	public static final String PRJ = "java-hb-mapping-prj";
+    
+	@BeforeClass
+	public static void prepare() {
+		importProject(PRJ);
+	}
+	
+	@Test()
+	public void createMappingFileFromPackage() {
+		PackageExplorer pe = new PackageExplorer();
+		DefaultTree tree = new DefaultTree();
+		DefaultTreeItem item = new DefaultTreeItem(PRJ,"src","org.mapping.model.pkg");
+		item.select();
+		
+		NewHibernateMappingFileWizard wizard = new NewHibernateMappingFileWizard();
+		wizard.open();
+		NewHibernateMappingElementsSelectionPage2 selPage = new NewHibernateMappingElementsSelectionPage2();
+		selPage.selectItem("org.mapping.model.pkg");
+		wizard.next();
+		NewHibernateMappingFilePage files = new NewHibernateMappingFilePage();
+		files.selectClasses("Owner");
+		wizard.next();
+		NewHibernateMappingPreviewPage preview = new NewHibernateMappingPreviewPage();
+		assertTrue("Preview text cannot be empty", !preview.getPreviewText().equals(""));
+		wizard.finish();
+		
+		pe.open();
+		
+		try {
+			item = new DefaultTreeItem(PRJ,"src","org.mapping.model.pkg","Dog.hbm.xml");
+		} catch (TreeItemNotFoundException e) {
+			fail("https://issues.jboss.org/browse/JBIDE-18769");
+		}
+		
+		
+		item.doubleClick();
+		new DefaultEditor("Dog.hbm.xml");
+		pe.open();
+		item = new DefaultTreeItem(PRJ,"src","org.mapping.model.pkg","Owner.hbm.xml");
+		item.doubleClick();		
+		new DefaultEditor("Owner.hbm.xml");
+	}
+	
+	@Test
+	public void createMappingFileFromFile() {
+		PackageExplorer pe = new PackageExplorer();
+		DefaultTreeItem item = new DefaultTreeItem(PRJ,"src","org.mapping.model.file","Item.java");
+		item.select();
+		
+		NewHibernateMappingFileWizard wizard = new NewHibernateMappingFileWizard();
+		wizard.open();
+		NewHibernateMappingElementsSelectionPage2 selPage = new NewHibernateMappingElementsSelectionPage2();
+		selPage.selectItem("Item");
+		wizard.next();
+		NewHibernateMappingFilePage files = new NewHibernateMappingFilePage();
+		files.selectClasses("Item");
+		wizard.next();
+		NewHibernateMappingPreviewPage preview = new NewHibernateMappingPreviewPage();
+		assertTrue("Preview text cannot be empty", !preview.getPreviewText().equals(""));
+		wizard.finish();
+		
+		pe.open();
+		try {
+			item = new DefaultTreeItem(PRJ,"src","org.mapping.model.file","Item.hbm.xml");
+		} catch (TreeItemNotFoundException e) {
+			fail("https://issues.jboss.org/browse/JBIDE-18769");
+		}
+
+		item.doubleClick();
+		new DefaultEditor("Dog.hbm.xml");
+	}
+
+		
+	@AfterClass
+	public static void cleanUp() {
+	}
+}
