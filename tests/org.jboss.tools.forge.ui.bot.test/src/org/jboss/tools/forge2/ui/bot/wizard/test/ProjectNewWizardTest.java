c48da8c023461900289c5ff54c644528825ab088
psrna <psrna@redhat.com>
2015-01-07 16:27:22 +0100

    Forge2 refactoring.
    New util methods for getting Forge wizard and project creation.
    Refactoring of existing forge2 tests to use util methods.

--- tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/wizard/test/ProjectNewWizardTest.java
+++ tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/wizard/test/ProjectNewWizardTest.java
@@ -3,64 +3,38 @@ package org.jboss.tools.forge2.ui.bot.wizard.test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
-import org.jboss.reddeer.swt.condition.JobIsRunning;
-import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.jface.wizard.WizardDialog;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
-import org.jboss.reddeer.swt.matcher.RegexMatcher;
-import org.jboss.reddeer.swt.matcher.WithTextMatcher;
-import org.jboss.reddeer.swt.wait.TimePeriod;
-import org.jboss.reddeer.swt.wait.WaitUntil;
-import org.jboss.reddeer.swt.wait.WaitWhile;
 import org.junit.Test;
 
 
 public class ProjectNewWizardTest extends WizardTestBase {
-	
-	private static final String WORKSPACE = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString(); 
+	 
 	private static final String PROJECT_NAME = "testProject";
 	
 	@Test
 	public void testIsFocusedOnStartup(){
-		runForgeCommand("project-new");
-		RegexMatcher rm = new RegexMatcher("(Project: New).*");
-		DefaultShell shell = new DefaultShell(new WithTextMatcher(rm));
-		assertTrue("'Project: New' wizard is not focused on startup", shell.isFocused());
-		shell.close();
+		WizardDialog wd = getWizardDialog("project-new", "(Project: New).*");
+		assertTrue("'Project: New' wizard is not focused on startup", new DefaultShell().isFocused());
+		wd.cancel();
 	}
 	
 	@Test
 	public void testIsProjectCreated(){
-		runForgeCommand("project-new");
-		RegexMatcher rm = new RegexMatcher("(Project: New).*");
-		new WaitUntil(new ShellWithTextIsActive(rm));
-		
-		new LabeledText("Project name:").setText(PROJECT_NAME);
-		new LabeledText("Project location:").setText(WORKSPACE);
-		new PushButton("Finish").click();
-		new WaitWhile(new ShellWithTextIsActive(rm), TimePeriod.VERY_LONG);
-		new WaitWhile(new JobIsRunning());
-		
-		ProjectExplorer pe = new ProjectExplorer();
-		assertTrue(pe.containsProject(PROJECT_NAME));
+		newProject(PROJECT_NAME);
 	}
 	
 	
 	@Test
 	public void testFinishBtnDisabled(){
-		runForgeCommand("project-new");
-		RegexMatcher rm = new RegexMatcher("(Project: New).*");
-		DefaultShell shell = new DefaultShell(new WithTextMatcher(rm));
-		
+		WizardDialog wd = getWizardDialog("project-new", "(Project: New).*");
 		assertTrue(new LabeledText("Project name:").getText().isEmpty());
 		assertFalse(new PushButton("Finish").isEnabled());
-		
 		new LabeledText("Project name:").setText(PROJECT_NAME);
 		assertTrue(new PushButton("Finish").isEnabled());
-		shell.close();
+		wd.cancel();
 	}
 	
 }
--- tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/wizard/test/WizardTestBase.java
+++ tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/wizard/test/WizardTestBase.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.forge2.ui.bot.wizard.test;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 
 import org.eclipse.core.commands.Command;
@@ -9,13 +11,18 @@ import org.eclipse.core.commands.NotEnabledException;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.services.IServiceLocator;
 import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
+import org.jboss.reddeer.jface.wizard.WizardDialog;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
 import org.jboss.reddeer.swt.matcher.RegexMatcher;
 import org.jboss.reddeer.swt.util.Display;
+import org.jboss.reddeer.swt.wait.WaitUntil;
 import org.jboss.tools.forge.reddeer.view.ForgeConsoleView;
 import org.junit.After;
 import org.junit.BeforeClass;
@@ -24,6 +31,8 @@ import org.junit.runner.RunWith;
 @RunWith(RedDeerSuite.class)
 public abstract class WizardTestBase {
 
+	protected static final String WORKSPACE = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
+	
 	@BeforeClass
 	public static void setup(){
 		ForgeConsoleView view = new ForgeConsoleView();
@@ -85,4 +94,25 @@ public abstract class WizardTestBase {
 	    	e.printStackTrace();
 	    }
 	}
+	
+	public WizardDialog getWizardDialog(String command, String title_regex){
+		runForgeCommand(command);
+		RegexMatcher rm = new RegexMatcher(title_regex);
+		new WaitUntil(new ShellWithTextIsActive(rm));
+		return new WizardDialog();
+	}
+	
+	public void newProject(String name, String path){		
+		WizardDialog wd = getWizardDialog("project-new", "(Project: New).*");
+		new LabeledText("Project name:").setText(name);
+		new LabeledText("Project location:").setText(path);
+		wd.finish();
+		ProjectExplorer pe = new ProjectExplorer();
+		assertTrue(pe.containsProject(name));
+	}
+	
+	public void newProject(String name){
+		newProject(name, WORKSPACE);
+	}
+	
 }
