42f3975baf23a3ecce6d4c6ea70546cf7b0b0277
Jiri Peterka <peterka.jiri@gmail.com>
2015-01-14 15:51:15 +0100

    JPA Entity Generation test implemented

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JPAEntityGenerationTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JPAEntityGenerationTest.java
@@ -7,17 +7,23 @@ import org.jboss.reddeer.requirements.db.DatabaseConfiguration;
 import org.jboss.reddeer.requirements.db.DatabaseRequirement;
 import org.jboss.reddeer.requirements.db.DatabaseRequirement.Database;
 import org.jboss.reddeer.swt.condition.ButtonWithTextIsActive;
+import org.jboss.reddeer.swt.condition.JobIsRunning;
 import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
 import org.jboss.reddeer.swt.condition.WidgetIsEnabled;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.swt.impl.button.FinishButton;
 import org.jboss.reddeer.swt.impl.button.OkButton;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.combo.DefaultCombo;
+import org.jboss.reddeer.swt.impl.combo.LabeledCombo;
 import org.jboss.reddeer.swt.impl.group.DefaultGroup;
 import org.jboss.reddeer.swt.impl.link.DefaultLink;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.reddeer.swt.wait.WaitUntil;
 import org.jboss.reddeer.swt.wait.WaitWhile;
+import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
 import org.jboss.tools.hibernate.factory.ConnectionProfileFactory;
 import org.jboss.tools.hibernate.factory.DriverDefinitionFactory;
 import org.junit.After;
@@ -27,12 +33,12 @@ import org.junit.runner.RunWith;
 
 
 /**
- * Set JPA Facets for JPA project 
+ * Test prepares project and generate JPA entities from database 
  * @author Jiri Peterka
  */
 @RunWith(RedDeerSuite.class)
 @Database(name="testdb")
-public class JPAFacetTest extends HibernateRedDeerTest {
+public class JPAEntityGenerationTest extends HibernateRedDeerTest {
 
 	private final String PRJ = "mvn-hibernate43"; 
     @InjectRequirement    
@@ -42,12 +48,13 @@ public class JPAFacetTest extends HibernateRedDeerTest {
 	public void testConnectionProfile() {
     	importProject(PRJ);
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
-		DriverDefinitionFactory.createDatabaseDefinition(cfg);
-		ConnectionProfileFactory.createConnectionProfile(cfg);		
+		DriverDefinitionFactory.createDatabaseDefinition(cfg);		
+		ConnectionProfileFactory.createConnectionProfile(cfg);
+		testSetJPAFacets();
 	}
     
-    @Test
-    public void testSetJPAFacets()
+    
+    private void testSetJPAFacets()
     {
     	DatabaseConfiguration cfg = dbRequirement.getConfiguration();
     	
@@ -79,6 +86,33 @@ public class JPAFacetTest extends HibernateRedDeerTest {
     	OkButton okButton = new OkButton();
     	okButton.click();
     	new WaitWhile(new ShellWithTextIsActive("Properties for " + PRJ));
+    }
+    
+    @Test
+    public void testEntityGeneration() {
+    	DatabaseConfiguration cfg = dbRequirement.getConfiguration();
+    	
+    	ProjectExplorer pe = new ProjectExplorer();
+    	pe.open();
+    	pe.selectProjects(PRJ);
+    	
+    	new ContextMenu("JPA Tools","Generate Entities from Tables...").select();
+    	new WaitUntil(new ShellWithTextIsActive("Generate Entities"));
+    	CheckBox useConsole = new CheckBox("Use Console Configuration");
+    	if (useConsole.isEnabled()) {
+    		useConsole.click();
+    	}
+    	new LabeledText("Package:").setText("org.gen");
+    	new LabeledCombo("Hibernate Version:").setSelection("4.3");
+    	new LabeledCombo("Database Connection").setSelection(cfg.getProfileName());
+    	
+    	new FinishButton().click();
+    	new WaitWhile(new JobIsRunning());
+    	
+    	pe.open();
+    	pe.selectProjects(PRJ);
+    	new DefaultTreeItem(PRJ,"src/main/java","org.gen","Actor.java").doubleClick();
+    	new DefaultEditor("Actor.java");
     }
     
 	@After
