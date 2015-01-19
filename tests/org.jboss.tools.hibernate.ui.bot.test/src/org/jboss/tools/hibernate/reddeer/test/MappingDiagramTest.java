9712297091bb933bc4382123f61634c765e8a110
Jiri Peterka <peterka.jiri@gmail.com>
2015-01-19 18:34:21 +0100

    Mapping diagram test fixed

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/MappingDiagramTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/MappingDiagramTest.java
@@ -5,10 +5,21 @@ import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.requirements.db.DatabaseConfiguration;
 import org.jboss.reddeer.requirements.db.DatabaseRequirement;
 import org.jboss.reddeer.requirements.db.DatabaseRequirement.Database;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.impl.button.OkButton;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.button.RadioButton;
+import org.jboss.reddeer.swt.impl.combo.DefaultCombo;
+import org.jboss.reddeer.swt.impl.combo.LabeledCombo;
+import org.jboss.reddeer.swt.impl.group.DefaultGroup;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.text.DefaultText;
+import org.jboss.reddeer.swt.wait.WaitUntil;
 import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
 import org.jboss.tools.hibernate.factory.ConnectionProfileFactory;
 import org.jboss.tools.hibernate.factory.DriverDefinitionFactory;
+import org.jboss.tools.hibernate.factory.EntityGenerationFactory;
+import org.jboss.tools.hibernate.factory.ProjectConfigurationFactory;
 import org.jboss.tools.hibernate.reddeer.console.KnownConfigurationsView;
 import org.junit.After;
 import org.junit.Before;
@@ -30,26 +41,44 @@ public class MappingDiagramTest extends HibernateRedDeerTest {
     
     @Before
 	public void testConnectionProfile() {
+
     	importProject(PRJ);
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		DriverDefinitionFactory.createDatabaseDefinition(cfg);		
 		ConnectionProfileFactory.createConnectionProfile(cfg);
+		ProjectConfigurationFactory.convertProjectToFacetsForm(PRJ);
+		ProjectConfigurationFactory.setProjectFacetForDB(PRJ, cfg);
+		EntityGenerationFactory.generateJPAEntities(cfg,PRJ,"org.gen","4.3");
 	}
     
     
+
 	@Test
 	public void testMappingDiagram() {
 		KnownConfigurationsView v = new KnownConfigurationsView();
 		v.open();
-		v.triggerAddConfigurationDialog();
+		new ContextMenu("Add Configuration...").select();
+		new WaitUntil(new ShellWithTextIsActive("Edit Configuration"));
+		DefaultGroup prjGroup = new DefaultGroup("Project:");
+		new DefaultText(prjGroup).setText(PRJ);
+		new RadioButton("JPA (jdk 1.5+)").click();
+		DefaultGroup dbConnection = new DefaultGroup("Database connection:");
+		new DefaultCombo(dbConnection,0).setText("[JPA Project Configured Connection]");
+		new LabeledCombo("Hibernate Version:").setSelection("4.3");
+		new PushButton("Apply").click();
+		
+		new OkButton().click();		
+		
+		v.open();
+		v.selectConsole("hibernate");
 
 		ContextMenu mappingMenu = new ContextMenu("Mapping Diagram");
 		mappingMenu.select();
 		
-		new DefaultEditor(PRJ + ":");
+		new DefaultEditor("hibernate: Actor and 15 others");
 	}
 
-    
+   
 	@After
 	public void cleanUp() {
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
