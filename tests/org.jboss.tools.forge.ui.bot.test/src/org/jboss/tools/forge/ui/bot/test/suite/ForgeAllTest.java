950049ea38b1b954b43d0699b1e51b0af8bb4bff
psrna <psrna@redhat.com>
2015-01-20 15:11:29 +0100

    Removes deprecated Forge1 tests and sets Forge2 testsuite as default.

--- tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge/ui/bot/test/suite/ForgeAllTest.java
+++ tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge/ui/bot/test/suite/ForgeAllTest.java
@@ -5,10 +5,8 @@ import org.jboss.tools.forge.ui.bot.console.test.EntityTest;
 import org.jboss.tools.forge.ui.bot.console.test.ForgeViewTest;
 import org.jboss.tools.forge.ui.bot.console.test.InstallPluginTest;
 import org.jboss.tools.forge.ui.bot.console.test.PersistenceTest;
-import org.jboss.tools.forge.ui.bot.console.test.PreferencesTest;
 import org.jboss.tools.forge.ui.bot.console.test.ProjectTest;
 import org.jboss.tools.forge.ui.bot.console.test.ScaffoldingTest;
-import org.jboss.tools.forge.ui.bot.wizard.test.ProjectWizardTest;
 
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
@@ -29,9 +27,6 @@ import org.junit.runners.Suite.SuiteClasses;
 	InstallPluginTest.class,
 	ScaffoldingTest.class,
 	CommandTest.class,
-	PreferencesTest.class,
-	//wizard tests
-	ProjectWizardTest.class
 })
 @RunWith(RequirementAwareSuite.class)
 public class ForgeAllTest {
