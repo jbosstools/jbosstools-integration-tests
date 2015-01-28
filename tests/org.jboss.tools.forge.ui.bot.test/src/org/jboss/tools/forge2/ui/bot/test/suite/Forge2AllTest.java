81e43c9c29fdf6f4c29f7a8fe3ab2e65829bad2b
psrna <psrna@redhat.com>
2015-01-28 18:54:36 +0100

    Forge2 new tests for REST Setup wizard.

--- tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/test/suite/Forge2AllTest.java
+++ tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/test/suite/Forge2AllTest.java
@@ -6,6 +6,7 @@ import org.jboss.tools.forge2.ui.bot.wizard.test.ConstraintSetupWizardTest;
 import org.jboss.tools.forge2.ui.bot.wizard.test.FacesSetupWizardTest;
 import org.jboss.tools.forge2.ui.bot.wizard.test.JPASetupWizardTest;
 import org.jboss.tools.forge2.ui.bot.wizard.test.ProjectNewWizardTest;
+import org.jboss.tools.forge2.ui.bot.wizard.test.RESTSetupWizardTest;
 import org.jboss.tools.forge2.ui.bot.wizard.test.ServletSetupWizardTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -21,7 +22,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	JPASetupWizardTest.class,
 	ServletSetupWizardTest.class,
 	ConstraintSetupWizardTest.class,
-	FacesSetupWizardTest.class
+	FacesSetupWizardTest.class,
+	RESTSetupWizardTest.class
 })
 @RunWith(RedDeerSuite.class)
 public class Forge2AllTest {
---
+++ tests/org.jboss.tools.forge.ui.bot.test/src/org/jboss/tools/forge2/ui/bot/wizard/test/RESTSetupWizardTest.java
@@ -0,0 +1,82 @@
+package org.jboss.tools.forge2.ui.bot.wizard.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
+import org.jboss.reddeer.jface.wizard.WizardDialog;
+import org.jboss.reddeer.swt.impl.combo.LabeledCombo;
+import org.jboss.reddeer.swt.impl.ctab.DefaultCTabItem;
+import org.jboss.reddeer.swt.impl.table.DefaultTable;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.workbench.condition.EditorWithTitleIsActive;
+import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
+import org.jboss.tools.forge.ui.bot.test.util.ResourceUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RESTSetupWizardTest extends WizardTestBase {
+
+	private static String JAX_RS_VERSION = "2.0";
+	private static String APP_PATH = "/rest";
+	private static String CLASS_NAME = "RestApp";
+	private static String DEPENDENCY = "javax.ws.rs-api : " + JAX_RS_VERSION
+			+ " [provided]";
+
+	@Before
+	public void prepare() {
+		newProject(PROJECT_NAME);
+		ProjectExplorer pe = new ProjectExplorer();
+		pe.selectProjects(PROJECT_NAME); // this will set context for forge
+		WizardDialog wd = getWizardDialog("rest-setup", "(REST: Setup).*");
+		new LabeledCombo("JAX-RS Version:").setSelection(JAX_RS_VERSION);
+		new LabeledText("Application Path:").setText(APP_PATH);
+		new LabeledText("Target Package:").setText("org.test");
+		new LabeledText("Class Name:").setText(CLASS_NAME);
+		wd.finish(TimePeriod.getCustom(600));
+	}
+
+	@Test
+	public void testDependenciesAddedToPom() {
+		ProjectExplorer pe = new ProjectExplorer();
+		pe.getProject(PROJECT_NAME).getTreeItem().getItem("pom.xml")
+				.doubleClick();
+		new WaitUntil(new EditorWithTitleIsActive(PROJECT_NAME + "/pom.xml"));
+		new DefaultEditor(PROJECT_NAME + "/pom.xml").activate();
+		new DefaultCTabItem("Dependencies").activate();
+		assertTrue("Dependency: '" + DEPENDENCY + "' not added!",
+				new DefaultTable(1).containsItem(DEPENDENCY));
+	}
+
+	@Test
+	public void testAppJavaFileCreated() {
+		ProjectExplorer pe = new ProjectExplorer();
+		assertTrue(
+				CLASS_NAME + ".java has not been created!",
+				pe.getProject(PROJECT_NAME).containsItem("src", "main", "java",
+						"org", "test", CLASS_NAME + ".java"));
+		File javaFile = new File(WORKSPACE + "/" + PROJECT_NAME
+				+ "/src/main/java/org/test/" + CLASS_NAME + ".java");
+		assertTrue(CLASS_NAME + ".java has not been created!",
+				javaFile.exists());
+	}
+
+	@Test
+	public void testAppPathDefined() {
+		try {
+			String javaContent = ResourceUtils.readFile(WORKSPACE + "/"
+					+ PROJECT_NAME + "/src/main/java/org/test/" + CLASS_NAME
+					+ ".java");
+			assertTrue(javaContent.contains("@ApplicationPath(\"" + APP_PATH
+					+ "\")"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Attempt to read the 'pom.xml' failed!");
+		}
+	}
+}
