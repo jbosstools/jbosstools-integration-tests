61204fd8343dc6b7bd16916123a8b8998591abdf
Jiri Peterka <peterka.jiri@gmail.com>
2015-02-04 17:08:23 +0100

    Project facets setting updated to work with hibernate tests

--- plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/factory/ProjectConfigurationFactory.java
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/factory/ProjectConfigurationFactory.java
@@ -1,10 +1,26 @@
 package org.jboss.tools.hibernate.factory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.requirements.db.DatabaseConfiguration;
+import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.impl.button.OkButton;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.combo.LabeledCombo;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.tree.DefaultTree;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.swt.wait.WaitWhile;
+import org.jboss.reddeer.uiforms.impl.hyperlink.DefaultHyperlink;
 import org.jboss.tools.hibernate.reddeer.dialog.ProjectFacetsPage;
 import org.jboss.tools.hibernate.reddeer.dialog.ProjectPropertyDialog;
+import org.jboss.tools.hibernate.reddeer.entity.FacetDefinition;
+import org.jboss.tools.hibernate.reddeer.entity.Facets;
 import org.jboss.tools.hibernate.reddeer.wizard.JPAFacetWizardPage;
+
 
 /**
  * Project configuration factory provides common routines for setting Hibernate
@@ -37,16 +53,59 @@ public class ProjectConfigurationFactory {
 	 * @param cfg given database configuration
 	 */
 	public static void setProjectFacetForDB(String prj,
+			
 		DatabaseConfiguration cfg) {
 		ProjectExplorer pe = new ProjectExplorer();
 		pe.open();
 		pe.selectProjects(prj);
 		ProjectPropertyDialog prjDlg = new ProjectPropertyDialog(prj);
-		prjDlg.open();    	
+		prjDlg.open();
+		prjDlg.select("Project Facets");
+		
+		List<FacetDefinition> fd = new ArrayList<FacetDefinition>();
+		fd.add(Facets.JPA);		
+		setProjectFacets(fd);
+		prjDlg.ok();
+		
+		prjDlg.open();		
 		prjDlg.select("JPA");
+ 
 		JPAFacetWizardPage jpaPage = new JPAFacetWizardPage();
 		
 		jpaPage.setConnectionProfile(cfg.getProfileName());
 		prjDlg.ok();	
 	}	
+	
+	private static void setProjectFacets(List<FacetDefinition> facets) {
+		List<TreeItem> items = new DefaultTree(1).getAllItems();
+		for (FacetDefinition facet : facets) {
+			for (TreeItem item : items) {
+				if (item.getText().equals(facet.getName())) {
+					item.setChecked(true);
+					changeVersion(facet, item);
+					break;
+				}
+			}		
+		}
+
+		DefaultHyperlink hyperlink = new DefaultHyperlink();
+		hyperlink.activate();	
+		
+		new WaitUntil(new ShellWithTextIsActive("Modify Faceted Project"));
+		new LabeledCombo("Type:").setText("Disable Library Configuration");
+		new OkButton().click();
+		
+		new WaitWhile(new ShellWithTextIsActive("Modify Faceted Project"));
+	}
+	
+	
+	private static void changeVersion(FacetDefinition facet, TreeItem facetTreeItem) {
+		if (facet.getVersion() != null){
+			facetTreeItem.select();
+			new ContextMenu("Change Version...").select();
+			new LabeledCombo("Version:").setSelection(facet.getVersion());
+			new PushButton("OK").click();
+		}
+	}
+
 }
---
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/entity/Facets.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.hibernate.reddeer.entity;
+
+import org.jboss.tools.hibernate.reddeer.entity.FacetDefinition;
+
+/**
+ * Set of facet definitions
+ * @author jpeterka
+ *
+ */
+public class Facets {
+
+	public static final FacetDefinition JAVA_FACET = new FacetDefinition("Java", null, "1.6");
+	
+	public static final FacetDefinition JPA = new FacetDefinition("JPA", null, "2.1");
+}
