5783878a172def09d9def74ab7d6e72bf80bc8df
Jiri Peterka <peterka.jiri@gmail.com>
2015-02-27 08:50:07 +0100

    Test project for hibernate mapping file test added and minor test update done

---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/resources/prj/java-hb-mapping-prj/src/org/mapping/model/pkg/Owner.java
@@ -0,0 +1,21 @@
+package org.mapping.model.pkg;
+
+
+public class Owner {
+	
+	private long id;
+	private String name;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
