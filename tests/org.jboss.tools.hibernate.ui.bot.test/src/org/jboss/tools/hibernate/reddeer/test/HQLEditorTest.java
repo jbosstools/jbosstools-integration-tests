f48436cbd17f3f2e66740a99a42f95b4840d8f5c
Jiri Peterka <peterka.jiri@gmail.com>
2015-03-04 13:48:33 +0100

    Hibernate HQL Editor test epanded to test multiple hibernate versions

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/HQLEditorTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/HQLEditorTest.java
@@ -2,6 +2,7 @@ package org.jboss.tools.hibernate.reddeer.test;
 
 import static org.junit.Assert.assertTrue;
 
+import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.requirements.db.DatabaseConfiguration;
@@ -27,7 +28,6 @@ import org.jboss.tools.hibernate.reddeer.factory.EntityGenerationFactory;
 import org.jboss.tools.hibernate.reddeer.factory.ProjectConfigurationFactory;
 import org.jboss.tools.hibernate.reddeer.view.QueryPageTabView;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -40,44 +40,78 @@ import org.junit.runner.RunWith;
 @Database(name="testdb")
 public class HQLEditorTest extends HibernateRedDeerTest {
 
-	private final String PRJ = "mvn-hibernate43"; 
+	private String prj = "mvn-hibernate43";
+	private String hbVersion = "4.3";
+	private String jpaVersion = "2.1"; 
+	
     @InjectRequirement    
     private DatabaseRequirement dbRequirement;
     
-    @Before
-	public void testConnectionProfile() {
+	private void prepare() {
 
-    	importProject(PRJ);
+    	importProject(prj);
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		DriverDefinitionFactory.createDatabaseDefinition(cfg);		
 		ConnectionProfileFactory.createConnectionProfile(cfg);
-		ProjectConfigurationFactory.convertProjectToFacetsForm(PRJ);
-		ProjectConfigurationFactory.setProjectFacetForDB(PRJ, cfg);
-		EntityGenerationFactory.generateJPAEntities(cfg,PRJ,"org.gen","4.3",true);
+		ProjectConfigurationFactory.convertProjectToFacetsForm(prj);
+		ProjectConfigurationFactory.setProjectFacetForDB(prj, cfg, jpaVersion);
+		EntityGenerationFactory.generateJPAEntities(cfg,prj,"org.gen",hbVersion,true);
 	}
     
     
 
-	@Test
-	public void testMappingDiagram() {
+    @Test
+    public void testHQLEditor35() {
+    	setParams("mvn-hibernate35","3.5","2.0");
+    	testHQLEditor();
+    }
+    
+    @Test
+    public void testHQLEditor36() {
+    	setParams("mvn-hibernate36","3.6","2.0");
+    	testHQLEditor();
+    }
+    
+    @Test
+    public void testHQLEditor40() {
+    	setParams("mvn-hibernate40","4.0","2.0");
+    	testHQLEditor();
+    }
+    
+    @Test
+    public void testHQLEditor43() {
+    	setParams("mvn-hibernate43","4.3","2.1");
+    	testHQLEditor();
+    }
+    
+    private void setParams(String prj, String hbVersion, String jpaVersion) {
+    	this.prj = prj;
+    	this.hbVersion = hbVersion;
+    	this.jpaVersion = jpaVersion;
+    }
+    
+
+	public void testHQLEditor() {
+		prepare();
+		
 		KnownConfigurationsView v = new KnownConfigurationsView();
 		v.open();
 		new ContextMenu("Add Configuration...").select();
 		new WaitUntil(new ShellWithTextIsActive("Edit Configuration"));
 		DefaultGroup prjGroup = new DefaultGroup("Project:");
-		new DefaultText(prjGroup).setText(PRJ);
+		new DefaultText(prjGroup).setText(prj);
 		new RadioButton("JPA (jdk 1.5+)").click();
 		DefaultGroup dbConnection = new DefaultGroup("Database connection:");
 		new DefaultCombo(dbConnection,0).setText("[JPA Project Configured Connection]");
-		new LabeledCombo("Hibernate Version:").setSelection("4.3");
+		new LabeledCombo("Hibernate Version:").setSelection(hbVersion);
 		new PushButton("Apply").click();
 		
 		new OkButton().click();		
 		
 		v.open();
-		v.selectConsole(PRJ);
+		v.selectConsole(prj);
 		new ContextMenu("HQL Editor").select();
-		TextEditor hqlEditor = new TextEditor(PRJ);
+		TextEditor hqlEditor = new TextEditor(prj);
 		hqlEditor.setText("from Actor");
 		
 		new DefaultToolItem("Run HQL").click();
@@ -95,5 +129,8 @@ public class HQLEditorTest extends HibernateRedDeerTest {
 	public void cleanUp() {
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		ConnectionProfileFactory.deleteConnectionProfile(cfg.getProfileName());
+		ProjectExplorer pe = new ProjectExplorer();
+		pe.open();
+		pe.getProject(prj).delete(true);
 	}
 }
