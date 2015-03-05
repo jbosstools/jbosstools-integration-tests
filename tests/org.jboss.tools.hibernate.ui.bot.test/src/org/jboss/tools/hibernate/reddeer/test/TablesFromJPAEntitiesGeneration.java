4fd322c68bb36aed70107a70068a0dc418432c60
Jiri Peterka <peterka.jiri@gmail.com>
2015-03-05 07:24:24 +0100

    DDL generation test extended to support multiple hibernate versions

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/TablesFromJPAEntitiesGeneration.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/TablesFromJPAEntitiesGeneration.java
@@ -17,7 +17,6 @@ import org.jboss.tools.hibernate.reddeer.factory.ProjectConfigurationFactory;
 import org.jboss.tools.hibernate.reddeer.wizard.GenerateDdlWizard;
 import org.jboss.tools.hibernate.reddeer.wizard.GenerateDdlWizardPage;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -30,31 +29,65 @@ import org.junit.runner.RunWith;
 @Database(name = "testdb")
 public class TablesFromJPAEntitiesGeneration extends HibernateRedDeerTest {
 
-	private final String PRJ = "mvn-hibernate43";
+	private String prj = "mvn-hibernate43";
+	private String hbVersion = "4.3";
+	private String jpaVersion = "2.0";
+
 	private final String DDL_FILE = "output.ddl";
 	@InjectRequirement
 	private DatabaseRequirement dbRequirement;
-
-	@Before
-	public void testConnectionProfile() {
-		importProject(PRJ);
+	
+	public void prepare() {
+		importProject(prj);
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		DriverDefinitionFactory.createDatabaseDefinition(cfg);
 		ConnectionProfileFactory.createConnectionProfile(cfg);
 		testSetJPAFacets(cfg);
-		EntityGenerationFactory.generateJPAEntities(cfg, PRJ, "org.gen", "4.3",true);
+		EntityGenerationFactory.generateJPAEntities(cfg, prj, "org.gen", hbVersion, true);
 	}
 
 	private void testSetJPAFacets(DatabaseConfiguration cfg) {
-		ProjectConfigurationFactory.convertProjectToFacetsForm(PRJ);
-		ProjectConfigurationFactory.setProjectFacetForDB(PRJ, cfg);
+		ProjectConfigurationFactory.convertProjectToFacetsForm(prj);
+		ProjectConfigurationFactory.setProjectFacetForDB(prj, cfg, jpaVersion);
 	}
+	
+    @Test
+    public void testDDLGeneration35() {
+    	setParams("mvn-hibernate35","3.5","2.0");
+    	testDDLGeneration();
+    }
+    
+    @Test
+    public void testDDLGeneration36() {
+    	setParams("mvn-hibernate36","3.6","2.0");
+    	testDDLGeneration();
+    }
+    
+    @Test
+    public void testDDLGeneration40() {
+    	setParams("mvn-hibernate40","4.0","2.0");
+    	testDDLGeneration();
+    }
+    
+    @Test
+    public void testDDLGeneration43() {
+    	setParams("mvn-hibernate43","4.3","2.1");
+    	testDDLGeneration();
+    }
+    
 
-	@Test
-	public void testEntityGeneration() {
+    private void setParams(String prj, String hbVersion, String jpaVersion) {
+    	this.prj = prj;
+    	this.hbVersion = hbVersion;
+    	this.jpaVersion = jpaVersion;
+    }
+    
+	private void testDDLGeneration() {
+		prepare();
+		
 		ProjectExplorer pe = new ProjectExplorer();
 		pe.open();
-		pe.selectProjects(PRJ);
+		pe.selectProjects(prj);
 		GenerateDdlWizard w = new GenerateDdlWizard();
 		w.open();
 		GenerateDdlWizardPage p = new GenerateDdlWizardPage();
@@ -62,7 +95,7 @@ public class TablesFromJPAEntitiesGeneration extends HibernateRedDeerTest {
 		w.finish();
 
 		pe.open();
-		new DefaultTreeItem(PRJ, DDL_FILE).doubleClick();
+		new DefaultTreeItem(prj,"src/main/java", DDL_FILE).doubleClick();
 		assertTrue("DDL file cannot be empty", new TextEditor(DDL_FILE)
 				.getText().length() > 0);
 	}
@@ -71,5 +104,8 @@ public class TablesFromJPAEntitiesGeneration extends HibernateRedDeerTest {
 	public void cleanUp() {
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		ConnectionProfileFactory.deleteConnectionProfile(cfg.getProfileName());
+		ProjectExplorer pe = new ProjectExplorer();
+		pe.open();
+		pe.getProject(prj).delete(true);
 	}
 }
