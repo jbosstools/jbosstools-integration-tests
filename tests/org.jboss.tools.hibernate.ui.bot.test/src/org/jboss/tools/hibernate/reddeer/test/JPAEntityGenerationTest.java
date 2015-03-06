3593e23f1869c2fd9b457a2745a34f3a828042bb
Jiri Peterka <peterka.jiri@gmail.com>
2015-03-06 07:30:19 +0100

    New JPA Entities generation tests for additional hibernate versions added

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JPAEntityGenerationTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JPAEntityGenerationTest.java
@@ -16,7 +16,6 @@ import org.jboss.tools.hibernate.reddeer.factory.DriverDefinitionFactory;
 import org.jboss.tools.hibernate.reddeer.factory.EntityGenerationFactory;
 import org.jboss.tools.hibernate.reddeer.factory.ProjectConfigurationFactory;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -29,45 +28,86 @@ import org.junit.runner.RunWith;
 @Database(name="testdb")
 public class JPAEntityGenerationTest extends HibernateRedDeerTest {
 
-	private final String PRJ = "mvn-hibernate43"; 
+	private String prj = "mvn-hibernate43"; 
+	private String jpaVersion = "2.0";
+	 
     @InjectRequirement    
     private DatabaseRequirement dbRequirement;
     
-    @Before
-	public void testConnectionProfile() {
-    	importProject(PRJ);
+    
+	private void prepare() {
+    	importProject(prj);
 		DatabaseConfiguration cfg = dbRequirement.getConfiguration();
 		DriverDefinitionFactory.createDatabaseDefinition(cfg);		
 		ConnectionProfileFactory.createConnectionProfile(cfg);
-		testSetJPAFacets(cfg);
+		ProjectConfigurationFactory.convertProjectToFacetsForm(prj);
+		ProjectConfigurationFactory.setProjectFacetForDB(prj, cfg, jpaVersion);		
 	}
     
-    
-    private void testSetJPAFacets(DatabaseConfiguration cfg)
-    {
-		ProjectConfigurationFactory.convertProjectToFacetsForm(PRJ);
-		ProjectConfigurationFactory.setProjectFacetForDB(PRJ, cfg);		
+    private void setParams(String prj, String hbVersion, String jpaVersion) {
+    	this.prj = prj;
+    	this.jpaVersion = jpaVersion;
     }
     
     @Test
-    public void testEntityGenerationWithConsole() {
+    public void testEntityGenerationWithConsole35() {
+    	setParams("mvn-hibernate35","3.5","2.0");
     	testEntityGeneration(true);
     }
     
     @Test
-    public void testEntityGenerationWithoutConsole() {
+    public void testEntityGenerationWithConsole36() {
+    	setParams("mvn-hibernate36","3.6","2.0");
+    	testEntityGeneration(true);
+    }
+    
+    @Test
+    public void testEntityGenerationWithConsole40() {
+    	setParams("mvn-hibernate40","4.0","2.0");
+    	testEntityGeneration(true);
+    }
+    
+    @Test
+    public void testEntityGenerationWithConsole43() {
+    	setParams("mvn-hibernate43","4.3","2.1");
+    	testEntityGeneration(true);
+    }
+    
+    @Test
+    public void testEntityGenerationWithoutConsole35() {
+    	setParams("mvn-hibernate35","3.5","2.0");
     	testEntityGeneration(false);
     }
     
+    @Test
+    public void testEntityGenerationWithoutConsole36() {
+    	setParams("mvn-hibernate36","3.6","2.0");
+    	testEntityGeneration(false);
+    }
+    
+    @Test
+    public void testEntityGenerationWithoutConsole40() {
+    	setParams("mvn-hibernate40","4.0","2.0");
+    	testEntityGeneration(false);
+    }
+    
+    @Test
+    public void testEntityGenerationWithoutConsole43() {
+    	setParams("mvn-hibernate43","4.3","2.1");
+    	testEntityGeneration(false);
+    }    
+        
     private void testEntityGeneration(boolean useHibernateConsole) {
+    	prepare();
+    	
     	DatabaseConfiguration cfg = dbRequirement.getConfiguration();
-    	EntityGenerationFactory.generateJPAEntities(cfg,PRJ,"org.gen","4.3",useHibernateConsole);
+    	EntityGenerationFactory.generateJPAEntities(cfg,prj,"org.gen","4.3",useHibernateConsole);
     	
     	ProjectExplorer pe = new ProjectExplorer();    
     	pe.open();
-    	pe.selectProjects(PRJ);
+    	pe.selectProjects(prj);
     	try {
-    		new DefaultTreeItem(PRJ,"src/main/java","org.gen","Actor.java").doubleClick();
+    		new DefaultTreeItem(prj,"src/main/java","org.gen","Actor.java").doubleClick();
     	}
     	catch (TreeItemNotFoundException e) {
     		fail("Entities not generated, possible cause https://issues.jboss.org/browse/JBIDE-19175");
@@ -81,6 +121,7 @@ public class JPAEntityGenerationTest extends HibernateRedDeerTest {
 		ConnectionProfileFactory.deleteConnectionProfile(cfg.getProfileName());
 		
 		ProjectExplorer pe = new ProjectExplorer();
-		pe.getProject(PRJ).delete(true);
+		pe.open();
+		pe.getProject(prj).delete(true);
 	}
 }
