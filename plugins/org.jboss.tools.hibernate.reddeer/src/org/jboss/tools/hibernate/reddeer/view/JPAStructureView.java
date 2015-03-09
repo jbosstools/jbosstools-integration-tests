c154710f0cd99df4a6d8056c14570d7ed071ad31
Jiri Peterka <peterka.jiri@gmail.com>
2015-03-09 17:02:53 +0100

    JPA UI parts test added into Hibernate UI tests

---
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/view/JPAStructureView.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.hibernate.reddeer.view;
+
+import org.jboss.reddeer.workbench.impl.view.WorkbenchView;
+
+/**
+ * JPA Structure View RedDeer implementation
+ * @author Jiri Peterka
+ *
+ */
+public class JPAStructureView extends WorkbenchView {
+	
+	/**
+	 * Initialization and lookup for JPA Details view
+	 */
+	public JPAStructureView() {
+		super("JPA Structure");
+	}
+
+}
---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/JPAUIPartsTest.java
@@ -0,0 +1,61 @@
+package org.jboss.tools.hibernate.reddeer.test;
+
+import static org.junit.Assert.*;
+
+import org.jboss.reddeer.eclipse.ui.perspectives.JPAPerspective;
+import org.jboss.reddeer.workbench.impl.view.WorkbenchView;
+import org.jboss.tools.hibernate.reddeer.console.HibernateConfigurationView;
+import org.jboss.tools.hibernate.reddeer.perspective.HibernatePerspective;
+import org.jboss.tools.hibernate.reddeer.view.DynamicSQLPreviewView;
+import org.jboss.tools.hibernate.reddeer.view.JPADetailsView;
+import org.jboss.tools.hibernate.reddeer.view.JPAStructureView;
+import org.jboss.tools.hibernate.reddeer.view.QueryPageTabView;
+import org.jboss.tools.hibernate.reddeer.view.QueryParametersView;
+import org.junit.Test;
+
+/**
+ * Tests JPA UI Parts - perspective and views
+ * @author Jiri Peterka
+ *
+ */
+public class JPAUIPartsTest {
+
+	@Test
+	/**
+	 * Tests JPA perspective
+	 * Tests JPA views
+	 * - JPA Details
+	 * - JPA Structure
+	 */
+	public void testHibernateViews() {
+		checkView(new JPADetailsView());
+		checkView(new JPAStructureView());		
+	}
+	
+	/**
+	 * Tests hibernate perspective
+	 */
+	@Test
+	public void testHibernatePerspective() {
+		JPAPerspective p = new JPAPerspective();
+		p.open();
+		p.reset();
+		
+		assertTrue(p.getPerspectiveLabel().equals("JPA"));
+	}
+	
+	/**
+	 * Check bassic view operation for given view
+	 * @param given view
+	 */
+	private void checkView(WorkbenchView view) {
+		view.open();
+		view.maximize();
+		view.restore();
+		view.minimize();
+		view.restore();
+		view.close();		
+	}
+	
+	
+}
