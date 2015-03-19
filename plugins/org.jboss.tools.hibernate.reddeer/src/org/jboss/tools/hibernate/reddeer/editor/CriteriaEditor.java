79ec2dd3d46a1af1206084508db0baf7b5a4a372
Jiri Peterka <peterka.jiri@gmail.com>
2015-03-19 15:28:28 +0100

    RedDeer implemenatation for Hibernate Criteria editor added

---
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/editor/CriteriaEditor.java
@@ -0,0 +1,44 @@
+package org.jboss.tools.hibernate.reddeer.editor;
+
+import org.jboss.reddeer.common.logging.Logger;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.exception.WaitTimeoutExpiredException;
+import org.jboss.reddeer.swt.impl.button.YesButton;
+import org.jboss.reddeer.swt.impl.toolbar.DefaultToolItem;
+import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.workbench.impl.editor.TextEditor;
+
+/**
+ * Hibernate Criteria Editor RedDeer implementation
+ * @author Jiri Peterka
+ *
+ */
+public class CriteriaEditor extends TextEditor {
+
+	private Logger log = Logger.getLogger(CriteriaEditor.class);
+	
+	/**
+	 * Sets focus to Criteria Editor with given title 
+	 * @param title
+	 */
+	public CriteriaEditor(String title) {
+		super(title);
+	}
+
+	/**
+	 * Executes criteria 
+	 */
+	public void runCriteria() {
+		new DefaultToolItem("Run criteria").click();
+		
+		try {
+			new WaitUntil(new ShellWithTextIsActive("Open Session factory"), TimePeriod.SHORT);
+			new YesButton().click();
+		}
+		catch (WaitTimeoutExpiredException e) {
+			log.warn("Open Session factory question dialog was expected");
+		}		
+	}
+	
+}
