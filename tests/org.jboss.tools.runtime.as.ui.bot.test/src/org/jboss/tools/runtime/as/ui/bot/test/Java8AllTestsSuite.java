1a0452b9083198b6172cdcc74996051727c1638e
rhopp <rhopp@redhat.com>
2015-04-24 12:07:04 +0200

    Runtime detection tests for EAP 6.4 added.

--- tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/Java8AllTestsSuite.java
+++ tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/Java8AllTestsSuite.java
@@ -2,6 +2,8 @@ package org.jboss.tools.runtime.as.ui.bot.test;
 
 import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap63.DetectEAP63;
 import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap63.OperateEAP63;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap64.DetectEAP64;
+import org.jboss.tools.runtime.as.ui.bot.test.detector.server.eap64.OperateEAP64;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -12,8 +14,12 @@ import org.junit.runners.Suite;
  */
 @RunWith(JBTSuite.class)
 @Suite.SuiteClasses({
-		DetectEAP63.class,
-		OperateEAP63.class
+	
+	DetectEAP64.class,
+	OperateEAP64.class,
+	
+	DetectEAP63.class,
+	OperateEAP63.class
 })
 public class Java8AllTestsSuite {
 
