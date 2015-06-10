428fb7af0901a8d8d5400219308075fd2a753804
Jiri Peterka <peterka.jiri@gmail.com>
2015-06-10 16:33:29 +0200

    Criteria editor test fixed for focus changes in Eclipse

--- plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/dialog/ProjectFacetsPage.java
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/dialog/ProjectFacetsPage.java
@@ -3,8 +3,10 @@ package org.jboss.tools.hibernate.reddeer.dialog;
 import org.jboss.reddeer.jface.wizard.WizardPage;
 import org.jboss.reddeer.swt.condition.ButtonWithTextIsActive;
 import org.jboss.reddeer.core.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.core.condition.ShellWithTextIsAvailable;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.link.DefaultLink;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.reddeer.common.wait.WaitUntil;
 
@@ -31,7 +33,8 @@ public class ProjectFacetsPage extends WizardPage {
 	public void convertToFacetedForm() {
     	new DefaultTreeItem("Project Facets").select();
     	new DefaultLink("Convert to faceted form...").click();
-    	new WaitUntil(new ShellWithTextIsActive("Properties for " + projectName));    	
+    	new WaitUntil(new ShellWithTextIsAvailable("Properties for " + projectName));
+    	new DefaultShell("Properties for " + projectName);
     	PushButton apply = new PushButton("Apply");
     	new WaitUntil(new ButtonWithTextIsActive(apply));
     	apply.click();    	
