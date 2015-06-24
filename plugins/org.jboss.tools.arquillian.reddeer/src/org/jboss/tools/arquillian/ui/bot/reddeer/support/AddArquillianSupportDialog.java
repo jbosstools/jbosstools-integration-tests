615f06e9768fbba31c3e65607002afcd9cb98d1f
ljelinkova <ljelinko@redhat.com>
2015-06-24 12:45:56 +0200

    Arquillian: fix problem with updating maven repository

--- plugins/org.jboss.tools.arquillian.reddeer/src/org/jboss/tools/arquillian/ui/bot/reddeer/support/AddArquillianSupportDialog.java
+++ plugins/org.jboss.tools.arquillian.reddeer/src/org/jboss/tools/arquillian/ui/bot/reddeer/support/AddArquillianSupportDialog.java
@@ -1,14 +1,14 @@
 package org.jboss.tools.arquillian.ui.bot.reddeer.support;
 
-import org.jboss.reddeer.eclipse.core.resources.Project;
-import org.jboss.reddeer.swt.api.Button;
+import org.jboss.reddeer.common.wait.TimePeriod;
+import org.jboss.reddeer.common.wait.WaitWhile;
 import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.core.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.eclipse.core.resources.Project;
+import org.jboss.reddeer.swt.api.Button;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
-import org.jboss.reddeer.common.wait.TimePeriod;
-import org.jboss.reddeer.common.wait.WaitWhile;
 
 /**
  * Dialog shown after user selects on Maven project Configure -> Add Arquillian Support 
@@ -32,7 +32,7 @@ public class AddArquillianSupportDialog {
 		button.click();
 
 		new WaitWhile(new ShellWithTextIsActive(shellText), TimePeriod.LONG);
-		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
+		new WaitWhile(new JobIsRunning(), TimePeriod.VERY_LONG);
 	}
 	
 	private void activate(){
