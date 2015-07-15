71d8f7ab76d99a27d9a0900783579878ad77ce19
psrna <psrna@redhat.com>
2015-07-15 12:44:20 +0200

    Initial implementation of FeedHenry integration tests.
    - New test for the import operation

---
+++ tests/org.jboss.tools.aerogear.ui.bot.test/src/org/jboss/tools/aerogear/ui/bot/test/FeedHenryAllBotTests.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.aerogear.ui.bot.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.aerogear.ui.bot.test.rhmap.ImportFHApp;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	ImportFHApp.class
+})
+@RunWith(RedDeerSuite.class)
+public class FeedHenryAllBotTests {
+
+}
