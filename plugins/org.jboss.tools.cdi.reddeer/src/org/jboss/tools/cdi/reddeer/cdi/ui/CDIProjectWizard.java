f2a599298b5847d6155f4fc30f38575a260305f7
rawagner <rawagner@redhat.com>
2015-07-23 09:28:49 +0200

    use Contexts instead of Context

--- plugins/org.jboss.tools.cdi.reddeer/src/org/jboss/tools/cdi/reddeer/cdi/ui/CDIProjectWizard.java
+++ plugins/org.jboss.tools.cdi.reddeer/src/org/jboss/tools/cdi/reddeer/cdi/ui/CDIProjectWizard.java
@@ -3,14 +3,14 @@ package org.jboss.tools.cdi.reddeer.cdi.ui;
 import org.jboss.reddeer.jface.wizard.NewWizardDialog;
 import org.jboss.reddeer.eclipse.jst.servlet.ui.WebProjectFirstPage;
 import org.jboss.tools.cdi.reddeer.cdi.ui.wizard.facet.CDIInstallWizardPage;
+import org.jboss.tools.cdi.reddeer.CDIConstants;
 
 public class CDIProjectWizard extends NewWizardDialog{
 
-	public static final String CATEGORY="CDI (Context and Dependency Injection)";
 	public static final String NAME="CDI Web Project";
 	
 	public CDIProjectWizard(){
-		super(CATEGORY,NAME);
+		super(CDIConstants.CDI_GROUP,NAME);
 		addWizardPage(new WebProjectFirstPage(), 0);
 		addWizardPage(new CDIInstallWizardPage(), 3);
 	}
