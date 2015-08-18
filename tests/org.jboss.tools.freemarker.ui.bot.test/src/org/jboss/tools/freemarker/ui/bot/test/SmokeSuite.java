4234949b58defe47d20592fb151b8f58658075f2
Jiri Peterka <peterka.jiri@gmail.com>
2015-08-18 18:42:26 +0200

    Smoke tests updated to work without central plugin and Hibernate and Freemarker suites added

---
+++ tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/SmokeSuite.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.freemarker.ui.bot.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(RedDeerSuite.class)
+@Suite.SuiteClasses({
+
+	FreemarkerPreferencePageTest.class,
+	FreeMarkerEditorTest.class
+})
+public class SmokeSuite {
+
+}
---
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/ui/bot/test/SmokeSuite.java
@@ -0,0 +1,28 @@
+package org.jboss.tools.hibernate.ui.bot.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.hibernate.reddeer.test.CreateSingleJPAProjectTest;
+import org.jboss.tools.hibernate.reddeer.test.EmptyTest;
+import org.jboss.tools.hibernate.reddeer.test.HibernateUIPartsTest;
+import org.jboss.tools.hibernate.reddeer.test.JPAUIPartsTest;
+import org.jboss.tools.hibernate.reddeer.test.MappingFileTest;
+import org.jboss.tools.hibernate.reddeer.test.MavenizedProjectTest;
+import org.jboss.tools.hibernate.reddeer.test.PersistenceXMLFileTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(RedDeerSuite.class)
+@Suite.SuiteClasses({
+
+	// Hibernate smoke tests
+	EmptyTest.class,
+	CreateSingleJPAProjectTest.class,
+	HibernateUIPartsTest.class,
+	JPAUIPartsTest.class,
+	MappingFileTest.class,
+	MavenizedProjectTest.class,
+	PersistenceXMLFileTest.class,
+})
+public class SmokeSuite {
+
+}
