c0ef446b9d7e713e45eaccf73ff611a2e319df0c
psrna <psrna@redhat.com>
2015-09-01 15:55:37 +0200

    JST eclipse test plugin. Initial commit.
    This plugin should contain tests for:
    * bower
    * angularjs
    * tern

---
+++ tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/jst/ui/bot/test/AllJSTTests.java
@@ -0,0 +1,33 @@
+/******************************************************************************* 
+ * Copyright (c) 2015 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.jst.ui.bot.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.jst.ui.bot.test.bower.BowerInitTest;
+import org.jboss.tools.jst.ui.bot.test.bower.BowerUpdateTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(RedDeerSuite.class)
+@Suite.SuiteClasses({
+	// Bower Tests
+	BowerInitTest.class,
+	BowerUpdateTest.class
+	
+	// AngularJS Tests
+	
+	// ...
+	
+})
+public class AllJSTTests {
+
+}
