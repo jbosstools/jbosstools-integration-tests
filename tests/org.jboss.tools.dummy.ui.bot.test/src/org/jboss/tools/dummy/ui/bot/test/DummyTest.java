e287ffdbf19781cba11eaae595949bcece7e10a3
Jiri Peterka <peterka.jiri@gmail.com>
2015-09-16 09:28:31 +0200

    Added check for blank screenshot in dummy test

--- tests/org.jboss.tools.dummy.ui.bot.test/src/org/jboss/tools/dummy/ui/bot/test/DummyTest.java
+++ tests/org.jboss.tools.dummy.ui.bot.test/src/org/jboss/tools/dummy/ui/bot/test/DummyTest.java
@@ -1,9 +1,12 @@
 package org.jboss.tools.dummy.ui.bot.test;
 
 
+import static org.junit.Assert.*;
+
 import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.jface.preference.PreferencePage;
 import org.jboss.reddeer.junit.screenshot.CaptureScreenshotException;
+import org.jboss.reddeer.junit.screenshot.ImageTool;
 import org.jboss.reddeer.junit.screenshot.ScreenshotCapturer;
 import org.jboss.reddeer.workbench.impl.shell.WorkbenchShell;
 import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
@@ -18,7 +21,8 @@ import org.junit.Test;
  */
 public class DummyTest {
 	
-	Logger log = Logger.getLogger(DummyTest.class);
+	private Logger log = Logger.getLogger(DummyTest.class);
+	private boolean screenshotTaken = false;
 	
 	@BeforeClass
 	public static void before() {		
@@ -32,11 +36,19 @@ public class DummyTest {
 		preferenceDialog.select(preferencePage);
 		
 		// Take a screenshot
-		ScreenshotCapturer sc = ScreenshotCapturer.getInstance();
-		try {
-			sc.captureScreenshot("dummyOK.png");
-		} catch (CaptureScreenshotException e) {
-			log.error("Cannot capture screenshot:" + e.getMessage(), e);
+		if (!screenshotTaken) {
+			ScreenshotCapturer sc = ScreenshotCapturer.getInstance();
+			try {
+				screenshotTaken = true;
+				String path = sc.captureScreenshot("dummyOK.png");
+				ImageTool it = ImageTool.getInstance();
+				boolean isScreenshotBlank = it.isImageBlank(path);
+				if (isScreenshotBlank) log.error("Screenshot " + path + " is blank");
+				else log.info("Screenshot " + path + " has visible content");
+				assertTrue("Screenshot " + path + " should not be blank", !isScreenshotBlank );
+			} catch (CaptureScreenshotException e) {
+				log.error("Cannot capture screenshot:" + e.getMessage(), e);
+			}
 		}
 		
 		preferenceDialog.cancel();
@@ -45,7 +57,7 @@ public class DummyTest {
 	}
 	
 	@Test
-	public void hundredTimes() {
+	public void hundredTimesDummyTest() {
 		final int cycles = 100;
 		for (int i = 0 ; i < cycles ; i++)
 		{		
