a508bd6070ec1f6a72cc118dcb91da4cfcb5387f
Jiri Peterka <peterka.jiri@gmail.com>
2015-09-18 21:26:01 +0200

    Project Properties shell pages updated to make hibernate tests stable

--- plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/dialog/ProjectFacetsPage.java
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/dialog/ProjectFacetsPage.java
@@ -2,12 +2,22 @@ package org.jboss.tools.hibernate.reddeer.dialog;
 
 import org.jboss.reddeer.jface.wizard.WizardPage;
 import org.jboss.reddeer.swt.condition.ButtonWithTextIsEnabled;
+import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.core.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.core.condition.WidgetIsFound;
+import org.jboss.reddeer.core.matcher.ClassMatcher;
+import org.jboss.reddeer.core.matcher.WithMnemonicTextMatcher;
+import org.jboss.reddeer.core.matcher.WithStyleMatcher;
+import org.jboss.reddeer.core.matcher.WithTextMatcher;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.link.DefaultLink;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
+import org.jboss.reddeer.common.wait.WaitWhile;
 
 /**
  * Project facets page
@@ -32,8 +42,12 @@ public class ProjectFacetsPage extends WizardPage {
 	public void convertToFacetedForm() {
     	new DefaultTreeItem("Project Facets").select();
     	new DefaultLink("Convert to faceted form...").click();
+    	new WaitWhile(new JobIsRunning());
     	new WaitUntil(new ShellWithTextIsAvailable("Properties for " + projectName));
     	new DefaultShell("Properties for " + projectName);
+    	new WaitWhile(new JobIsRunning());
+    	new DefaultShell("Properties for " + projectName);
+    	new WaitUntil(new WidgetIsFound<Button>(new ClassMatcher(Button.class),new WithStyleMatcher(SWT.PUSH), new WithMnemonicTextMatcher("Apply")), TimePeriod.LONG);
     	PushButton apply = new PushButton("Apply");
     	new WaitUntil(new ButtonWithTextIsEnabled(apply));
     	apply.click();    	
--- plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/dialog/ProjectPropertyDialog.java
+++ plugins/org.jboss.tools.hibernate.reddeer/src/org/jboss/tools/hibernate/reddeer/dialog/ProjectPropertyDialog.java
@@ -1,11 +1,16 @@
 package org.jboss.tools.hibernate.reddeer.dialog;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Shell;
 import org.jboss.reddeer.swt.api.TreeItem;
 import org.jboss.reddeer.swt.condition.ButtonWithTextIsEnabled;
 import org.jboss.reddeer.swt.condition.CLabelWithTextIsAvailable;
+import org.jboss.reddeer.swt.condition.WidgetIsEnabled;
+import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.core.condition.ShellWithTextIsActive;
 import org.jboss.reddeer.core.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.core.condition.WidgetIsFound;
 import org.jboss.reddeer.core.handler.ShellHandler;
 import org.jboss.reddeer.core.handler.WidgetHandler;
 import org.jboss.reddeer.swt.impl.button.CancelButton;
@@ -15,6 +20,10 @@ import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.reddeer.core.lookup.ShellLookup;
+import org.jboss.reddeer.core.matcher.ClassMatcher;
+import org.jboss.reddeer.core.matcher.WithMnemonicTextMatcher;
+import org.jboss.reddeer.core.matcher.WithStyleMatcher;
+import org.jboss.reddeer.core.matcher.WithTextMatcher;
 import org.jboss.reddeer.jface.preference.PreferencePage;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
@@ -85,8 +94,11 @@ public class ProjectPropertyDialog {
 	 * Presses Ok button on Preference Dialog. 
 	 */
 	public void ok() {
-		new WaitUntil(new ShellWithTextIsAvailable(DIALOG_TITLE + projectName));
+		new WaitUntil(new ShellWithTextIsAvailable(DIALOG_TITLE + projectName), TimePeriod.LONG);
+		new WaitWhile(new JobIsRunning());
 		new DefaultShell(DIALOG_TITLE + projectName);
+		new WaitWhile(new JobIsRunning());
+		new WaitUntil(new WidgetIsFound<Button>(new ClassMatcher(Button.class),new WithMnemonicTextMatcher("OK"),new WithStyleMatcher(SWT.PUSH)),TimePeriod.LONG);
 		PushButton ok = new PushButton("OK");
 		new WaitUntil(new ButtonWithTextIsEnabled(ok));
 		ok.click();
