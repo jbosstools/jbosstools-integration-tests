49100af39c519eea6717b4ae9a9374c97aceaa33
Jiri Peterka <peterka.jiri@gmail.com>
2015-10-29 13:34:42 +0100

    Assign directive freemarker test added

--- tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/SmokeSuite.java
+++ tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/SmokeSuite.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
 
 	FreemarkerPreferencePageTest.class,
-	FreeMarkerEditorTest.class
+	FreeMarkerEditorTest.class,
+	FreemarkerDirectiveTest.class
 })
 public class SmokeSuite {
 
