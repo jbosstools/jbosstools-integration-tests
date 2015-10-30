0f78fa65c9e0db67ac808ed696503bc4aad7a816
Jiri Peterka <peterka.jiri@gmail.com>
2015-10-30 15:55:51 +0100

    Freemarker test added and other general updates done

---
+++ tests/org.jboss.tools.freemarker.ui.bot.test/resources/prj/org.jboss.tools.freemarker.testprj/ftl/attempt-directive.ftl
@@ -0,0 +1,14 @@
+<#--
+<#attempt>
+  Optional content: ${thisMayFails}
+<#recover>
+  Ops! The optional content is not available.
+</#attempt>
+Primary content continued
+-->
+<#attempt>
+  Optional content: ${thisMayFails}
+<#recover>
+  Ops! The optional content is not available.
+</#attempt>
+Primary content continued
--- tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/FreeMarkerEditorTest.java
+++ tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/FreeMarkerEditorTest.java
@@ -1,19 +1,12 @@
 package org.jboss.tools.freemarker.ui.bot.test;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Platform;
 import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.common.matcher.RegexMatcher;
 import org.jboss.reddeer.common.wait.WaitWhile;
@@ -22,21 +15,12 @@ import org.jboss.reddeer.core.condition.ShellWithTextIsActive;
 import org.jboss.reddeer.core.matcher.WithTextMatcher;
 import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.eclipse.ui.console.ConsoleView;
-import org.jboss.reddeer.eclipse.ui.perspectives.JavaPerspective;
 import org.jboss.reddeer.eclipse.ui.views.contentoutline.OutlineView;
-import org.jboss.reddeer.eclipse.ui.views.log.LogMessage;
-import org.jboss.reddeer.eclipse.ui.views.log.LogView;
-import org.jboss.reddeer.eclipse.ui.wizards.datatransfer.ExternalProjectImportWizardDialog;
-import org.jboss.reddeer.eclipse.ui.wizards.datatransfer.WizardProjectsImportPage;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.swt.api.TreeItem;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
-import org.jboss.reddeer.workbench.handler.EditorHandler;
 import org.jboss.reddeer.workbench.impl.editor.TextEditor;
-import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -52,27 +36,6 @@ public class FreeMarkerEditorTest extends FreemarkerTest {
 	private static final Logger log = Logger.getLogger(FreeMarkerEditorTest.class);
 	private String prj = "org.jboss.tools.freemarker.testprj";
 	
-	@BeforeClass
-	public static void beforeClass() {
-		JavaPerspective p = new JavaPerspective();
-		p.open();
-		EditorHandler.getInstance().closeAll(false);
-		new WaitWhile(new JobIsRunning());		
-
-		JavaPerspective jp = new JavaPerspective();
-		jp.open();
-		
-		WorkbenchPreferenceDialog dlg = new WorkbenchPreferenceDialog();
-		dlg.open();
-		dlg.select("FreeMarker");
-		
-		log.step("Set Freemarker outline level to full level on freemarker preference page");
-		FreemarkerPreferencePage page = new FreemarkerPreferencePage();
-		page.setOutlineLevelOfDetail(OutlineLevelOfDetail.FULL);
-		
-		dlg.ok();
-	}
-
 	@Test
 	public void emptyTest() {
 		assertTrue(true);
@@ -80,7 +43,6 @@ public class FreeMarkerEditorTest extends FreemarkerTest {
 
 	@Test
 	public void freeMarkerTest() {
-		emptyErrorLog();
 		log.step("Import test project for freemarker test");
 		importTestProject();
 		log.step("Open ftl file in freemarker editor");
@@ -90,29 +52,6 @@ public class FreeMarkerEditorTest extends FreemarkerTest {
 		checkErrorLog();
 	}	
 
-	private void importTestProject() {
-		
-		ExternalProjectImportWizardDialog wizard = new ExternalProjectImportWizardDialog();		
-		wizard.open();
-
-		String rpath = getResourceAbsolutePath(
-				Activator.PLUGIN_ID, "resources/prj");
-		String wpath = getWorkspaceAbsolutePath();
-		File rfile = new File(rpath);
-		File wfile = new File(wpath);
-		try {
-			copyFilesBinaryRecursively(rfile, wfile, null);
-		} catch (IOException e) {
-			fail("Unable to copy freemarker test project");
-		}
-		
-		WizardProjectsImportPage firstPage = new WizardProjectsImportPage();
-		
-		firstPage.setRootDirectory(wpath);
-		firstPage.selectAllProjects();
-		firstPage.copyProjectsIntoWorkspace(false);
-		wizard.finish();		
-	}
 
 	private void openFTLFileInEditor() {
 		
@@ -142,21 +81,6 @@ public class FreeMarkerEditorTest extends FreemarkerTest {
 		//assertTrue(list.contains("latestProduct.url"));		
 	}
 
-	private void emptyErrorLog() {
-		
-		LogView elv = new LogView();
-		elv.open();
-		//new ContextMenu("Delete Log").select();
-		new WaitWhile(new JobIsRunning());
-	}
-
-	private void checkErrorLog() {
-		LogView el = new LogView();
-		List<LogMessage> errorMessages = el.getErrorMessages();
-		for (LogMessage lm : errorMessages) { 
-			log.info(lm.getMessage());
-		}
-	}
 
 	@SuppressWarnings({ "unused", "unchecked" })
 	private void checkFreemMarkerOutput() {
@@ -190,68 +114,4 @@ public class FreeMarkerEditorTest extends FreemarkerTest {
 		assertTrue("Output equal check",consoleText.equals(outputExpected));
 	}
 
-	private String readTextFileToString(String filePath) throws IOException {
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(filePath));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-
-			while (line != null) {
-				sb.append(line);				
-				line = br.readLine();
-				if (line != null)
-					sb.append("\n");
-			}
-			String everything = sb.toString();
-			return everything;
-		} finally {
-			br.close();
-		}
-	}
-
-	@AfterClass
-	public static void aterClass() {
-		// wait for all jobs
-		new WaitWhile(new JobIsRunning());
-	}
-			
-	/**
-	 * Provide bundle resource absolute path
-	 * @param pluginId - plugin id
-	 * @param path - resource relative path
-	 * @return resource absolute path
-	 */
-	public static String getResourceAbsolutePath(String pluginId, String... path) {
-
-		// Construct path
-		StringBuilder builder = new StringBuilder();
-		for (String fragment : path) {
-			builder.append("/" + fragment);
-		}
-
-		String filePath = "";
-		try {
-			filePath = FileLocator.toFileURL(
-					Platform.getBundle(pluginId).getEntry("/")).getFile()
-					+ "resources" + builder.toString();
-			File file = new File(filePath);
-			if (!file.isFile()) {
-				filePath = FileLocator.toFileURL(
-						Platform.getBundle(pluginId).getEntry("/")).getFile()
-						+ builder.toString();
-			}
-		} catch (IOException ex) {
-			String message = filePath + " resource file not found";
-			//log.error(message);
-			fail(message);
-		}
-
-		return filePath;
-	}
 }
