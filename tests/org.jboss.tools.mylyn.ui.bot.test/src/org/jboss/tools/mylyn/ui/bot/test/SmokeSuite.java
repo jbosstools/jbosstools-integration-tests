73bf69f07ba4ff0d3e8e5653448b49be141264bb
Leonard Dimaggio <ldimaggi@ldimaggi.boston.csb>
2015-10-29 22:45:26 -0400

    Added SmokeSuite for org.jboss.tools.mylyn.ui.bot.test

---
+++ tests/org.jboss.tools.mylyn.ui.bot.test/src/org/jboss/tools/mylyn/ui/bot/test/SmokeSuite.java
@@ -0,0 +1,24 @@
+package org.jboss.tools.mylyn.ui.bot.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Mylyn Test Suite
+ * @author ldimaggi
+ *
+ */
+
+@RunWith(RedDeerSuite.class)
+
+@SuiteClasses({
+	MylynTestJenkins.class,
+	MylynTestValidate.class,
+	MylynTestLocalRepo.class,
+	MylynTestBzQuery.class
+	})
+
+public class SmokeSuite {
+	
+}
