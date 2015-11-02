9510d8bb8b785b0a8c60edb80dbb96071418bdc9
Rastislav Wagner <rawagner@redhat.com>
2015-11-02 14:17:52 +0100

    Test CDI wizards in smoke suite

---
+++ tests/org.jboss.tools.cdi.bot.test/src/org/jboss/tools/cdi/bot/test/wizard/cdi11/AllWizardsTestCDI11.java
@@ -0,0 +1,16 @@
+package org.jboss.tools.cdi.bot.test.wizard.cdi11;
+
+import org.jboss.ide.eclipse.as.reddeer.server.requirement.ServerReqType;
+import org.jboss.ide.eclipse.as.reddeer.server.requirement.ServerRequirement.JBossServer;
+import org.jboss.reddeer.eclipse.ui.perspectives.JavaEEPerspective;
+import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
+import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
+import org.jboss.reddeer.requirements.server.ServerReqState;
+import org.jboss.tools.cdi.bot.test.wizard.template.WizardTemplate;
+
+@CleanWorkspace
+@OpenPerspective(JavaEEPerspective.class)
+@JBossServer(state=ServerReqState.PRESENT, type=ServerReqType.WILDFLY8x)
+public class AllWizardsTestCDI11 extends WizardTemplate {
+
+}
