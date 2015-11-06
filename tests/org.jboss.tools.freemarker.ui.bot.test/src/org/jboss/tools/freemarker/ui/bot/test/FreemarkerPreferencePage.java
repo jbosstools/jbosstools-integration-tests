e8e3c768d5f03876c8ccf007a63d821873d49810
Jiri Peterka <peterka.jiri@gmail.com>
2015-11-06 16:16:45 +0100

    Import directive test and other refactoring done in freemarker tests

--- tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/FreemarkerPreferencePage.java
+++ tests/org.jboss.tools.freemarker.ui.bot.test/src/org/jboss/tools/freemarker/ui/bot/test/FreemarkerPreferencePage.java
@@ -6,23 +6,42 @@ import org.jboss.reddeer.swt.impl.button.RadioButton;
 import org.jboss.reddeer.swt.impl.group.DefaultGroup;
 
 
+/**
+ * Freemarker preference page RedDeer implementation
+ * @author Jiri Peterka
+ *
+ */
 public class FreemarkerPreferencePage extends PreferencePage {
 	
 
+	/**
+	 * Freemarker preference page
+	 */
 	public FreemarkerPreferencePage() {
 		super(new String[] {"FreeMarker"});
 	}
 
+	/**
+	 * Sets Freemarker highlight related directives to given value
+	 */
 	public void setHighLightRelatedDirectives(boolean value) {
 		CheckBox cb = new CheckBox();
 		cb.toggle(value);
 	}
 
+	/**
+	 * Gets Freemarker highlight related directives to given value
+	 * @return Freemarker highlight related directives 
+	 */
 	public boolean getHighLightRelatedDirectives() {
 		CheckBox b = new CheckBox();
 		return b.isChecked();		
 	}
 	
+	/**
+	 * Sets Freemarker outline level of details
+	 * @param olod given details level
+	 */
 	public void setOutlineLevelOfDetail(OutlineLevelOfDetail olod) {		
 		DefaultGroup group = new DefaultGroup("Outline Level of Detail:");
 		switch (olod) {
