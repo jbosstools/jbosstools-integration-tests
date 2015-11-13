964109c11a176414acb0015ded5bcea40f636c4f
ljelinkova <ljelinko@redhat.com>
2015-11-13 12:08:42 +0100

    AS, arquillian and batch smoke tests added.

---
+++ tests/org.jboss.tools.arquillian.ui.bot.test/src/org/jboss/tools/arquillian/ui/bot/test/SmokeSuite.java
@@ -0,0 +1,18 @@
+package org.jboss.tools.arquillian.ui.bot.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.arquillian.ui.bot.test.project.AddArquillianProfile;
+import org.jboss.tools.arquillian.ui.bot.test.project.CreateArquillianProject;
+import org.jboss.tools.arquillian.ui.bot.test.testcase.CreateArquillianTestCase;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(RedDeerSuite.class)
+@SuiteClasses({
+	CreateArquillianProject.class,
+	AddArquillianProfile.class,
+	CreateArquillianTestCase.class,
+	})
+public class SmokeSuite {
+	
+}
