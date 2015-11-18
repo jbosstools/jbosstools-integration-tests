0daaa64441653315582e944fca6f73cb11e16f83
Jiri Peterka <peterka.jiri@gmail.com>
2015-11-18 13:41:52 +0100

    Sakila support API added into hibernate ui tests

--- tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/HibernateRedDeerTest.java
+++ tests/org.jboss.tools.hibernate.ui.bot.test/src/org/jboss/tools/hibernate/reddeer/test/HibernateRedDeerTest.java
@@ -3,15 +3,29 @@ package org.jboss.tools.hibernate.reddeer.test;
 import org.jboss.reddeer.core.interceptor.SyncInterceptorManager;
 import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.workbench.handler.EditorHandler;
+import org.jboss.tools.hibernate.reddeer.database.DatabaseUtils;
 import org.jboss.tools.hibernate.reddeer.importer.ProjectImporter;
 import org.jboss.tools.hibernate.reddeer.interceptor.ErrorLogInterceptor;
 import org.jboss.tools.hibernate.ui.bot.test.Activator;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 public class HibernateRedDeerTest {
 	
 	private SyncInterceptorManager sim = SyncInterceptorManager.getInstance();
 	private final String LOG_INTERCEPTOR = "error-log-interceptor";
+	private static String dbFolder = System.getProperty("database.path");
 	
+	@BeforeClass
+	public static void beforeClass() {
+		DatabaseUtils.runSakilaDB(dbFolder);
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		DatabaseUtils.stopSakilaDB();
+	}
+
 	public HibernateRedDeerTest() {
 		super();
 	
