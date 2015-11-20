3b55ee385754e90bf6865ca1e1b16c85b6ea9e90
mlabuda <mlabuda@redhat.com>
2015-11-20 14:16:04 +0100

    Added OpenShift oc binary test and requirement, making smoke suite stable

--- tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/SmokeSuite.java
+++ tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/SmokeSuite.java
@@ -2,23 +2,13 @@ package org.jboss.tools.openshift.ui.bot.test;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.tools.openshift.ui.bot.test.connection.ID101OpenOpenShiftExplorerTest;
-import org.jboss.tools.openshift.ui.bot.test.connection.ID102OpenNewConnectionShellTest;
-import org.jboss.tools.openshift.ui.bot.test.connection.ID103CreateNewConnectionTest;
-import org.jboss.tools.openshift.ui.bot.test.connection.ID106RemoveConnectionTest;
-import org.jboss.tools.openshift.ui.bot.test.connection.ID109EditConnectionTest;
-import org.jboss.tools.openshift.ui.bot.test.connection.ID113ConnectionPropertiesTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(RedDeerSuite.class)
-@SuiteClasses({
+@SuiteClasses({	
 	// OpenShift v2 smoke tests
 	ID101OpenOpenShiftExplorerTest.class,
-	ID102OpenNewConnectionShellTest.class,
-	ID103CreateNewConnectionTest.class,
-	ID106RemoveConnectionTest.class,
-	ID109EditConnectionTest.class,
-	ID113ConnectionPropertiesTest.class,	
 })
 public class SmokeSuite {
 
