f063da62c97245f1c5e2db0a4071d6c8e3375b0b
Jiri Peterka <peterka.jiri@gmail.com>
2015-11-27 14:37:17 +0100

    Eclipse Checkstyle UI parts test addded

---
+++ tests/org.jboss.tools.eclipsecs.ui.test/src/org/jboss/tools/eclipsecs/ui/test/view/MarkerStatsView.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.eclipsecs.ui.test.view;
+
+import org.jboss.reddeer.workbench.impl.view.WorkbenchView;
+
+/**
+ * Checkstyle violations view
+ * @author Jiri Peterka
+ *
+ */
+public class MarkerStatsView extends WorkbenchView {
+
+	/**
+	 * Initialization and lookup for Checkstyle violations view
+	 */
+	public MarkerStatsView() {
+		super("Checkstyle violations");
+	}
+
+}
