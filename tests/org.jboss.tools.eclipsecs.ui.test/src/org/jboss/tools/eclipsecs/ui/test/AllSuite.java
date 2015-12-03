bce8d839f2aca75be856c60938ca13214d79e52d
Jiri Peterka <peterka.jiri@gmail.com>
2015-12-03 14:08:12 +0100

    AllSuite added into EclipseCS UI tests

---
+++ tests/org.jboss.tools.eclipsecs.ui.test/src/org/jboss/tools/eclipsecs/ui/test/AllSuite.java
@@ -0,0 +1,20 @@
+package org.jboss.tools.eclipsecs.ui.test;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Suite for all EclipseCS UI tests
+ * @author Jiri Peterka
+ *
+ */
+@RunWith(RedDeerSuite.class)
+@Suite.SuiteClasses({
+
+	CheckstyleTest.class,
+	CheckStyleUIPartsTest.class
+})
+public class AllSuite {
+
+}
