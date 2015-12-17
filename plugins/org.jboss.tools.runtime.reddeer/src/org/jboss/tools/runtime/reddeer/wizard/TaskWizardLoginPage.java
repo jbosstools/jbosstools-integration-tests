23cf2589eb434fc2c63d6b2aeb6c2dcb3d1ea722
rhopp <rhopp@redhat.com>
2015-12-17 15:54:17 +0100

    Changes in runtime tests reflecting new credentials framework.

--- plugins/org.jboss.tools.runtime.reddeer/src/org/jboss/tools/runtime/reddeer/wizard/TaskWizardLoginPage.java
+++ plugins/org.jboss.tools.runtime.reddeer/src/org/jboss/tools/runtime/reddeer/wizard/TaskWizardLoginPage.java
@@ -1,16 +1,37 @@
 package org.jboss.tools.runtime.reddeer.wizard;
 
 import org.jboss.reddeer.jface.wizard.WizardPage;
+import org.jboss.reddeer.swt.impl.button.CancelButton;
+import org.jboss.reddeer.swt.impl.button.OkButton;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.combo.LabeledCombo;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
 
 public class TaskWizardLoginPage extends WizardPage{
 	
 	public void setUsername(String username){
-		new LabeledText("Username: ").setText(username);
+		new LabeledCombo("Username: ").setSelection(username);
 	}
 	
-	public void setPassword(String password){
+	public String getDomain(){
+		return new LabeledCombo("Domain: ").getText();
+	}
+	
+	public void addCredentials(String username, String password){
+		new PushButton("Add...").click();
+		new DefaultShell("Add a Credential");
+		new LabeledText("Username: ").setText(username);
 		new LabeledText("Password: ").setText(password);
+		new OkButton().click();
+		
+		//Do not store this to secure storage
+		new DefaultShell("Secure Storage Password");
+		new CancelButton().click();
 	}
 
+	public boolean containsUsername(String username){
+		return new LabeledCombo("Username: ").getItems().contains(username);
+	}
+	
 }
