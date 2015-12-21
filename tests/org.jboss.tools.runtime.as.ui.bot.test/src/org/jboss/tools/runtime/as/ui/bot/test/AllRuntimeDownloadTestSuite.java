54231eb10049acdf1e587b4bc731e05155f49590
rhopp <rhopp@redhat.com>
2015-12-21 13:43:31 +0100

    Runtime detection of CDK added.
    FileUtils class created in common plugin.
    
    Removed JBTSuite (was replaced by RedDeerSuite)

--- tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/AllRuntimeDownloadTestSuite.java
+++ tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/AllRuntimeDownloadTestSuite.java
@@ -1,10 +1,11 @@
 package org.jboss.tools.runtime.as.ui.bot.test;
 
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.tools.runtime.as.ui.bot.test.download.AllRuntimeDownload;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(JBTSuite.class)
+@RunWith(RedDeerSuite.class)
 @Suite.SuiteClasses({
 	AllRuntimeDownload.class
 })
