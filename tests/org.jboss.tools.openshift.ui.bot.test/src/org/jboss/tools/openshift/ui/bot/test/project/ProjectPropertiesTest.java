94e8dcb5f727aa38cf9219684b9670686201c8fd
mlabuda <mlabuda@redhat.com>
2016-01-07 10:12:31 +0100

    Added license info to classes headers for OpenShift plugin and tests

--- tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/project/ProjectPropertiesTest.java
+++ tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/project/ProjectPropertiesTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.openshift.ui.bot.test.project;
 
 import static org.junit.Assert.*;
