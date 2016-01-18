eb8c730c92cace06deeb408208a77efb27b903bd
rhopp <rhopp@redhat.com>
2016-01-18 15:44:04 +0100

    Runtime detection product tests stability improvements.

--- tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/template/OperateServerTemplate.java
+++ tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/template/OperateServerTemplate.java
@@ -6,15 +6,13 @@ import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.fail;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.jboss.ide.eclipse.as.reddeer.matcher.ServerConsoleContainsNoExceptionMatcher;
 import org.jboss.ide.eclipse.as.reddeer.server.editor.JBossServerEditor;
 import org.jboss.ide.eclipse.as.reddeer.server.view.JBossServer;
 import org.jboss.ide.eclipse.as.reddeer.server.view.JBossServerView;
 import org.jboss.ide.eclipse.as.reddeer.server.wizard.page.JBossRuntimeWizardPage;
 import org.jboss.reddeer.common.condition.AbstractWaitCondition;
+import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
 import org.jboss.reddeer.common.wait.WaitWhile;
@@ -28,6 +26,10 @@ import org.jboss.reddeer.eclipse.wst.server.ui.view.ServersView;
 import org.jboss.reddeer.jface.wizard.WizardDialog;
 import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
 import org.jboss.reddeer.requirements.jre.JRERequirement;
+import org.jboss.reddeer.swt.condition.ShellIsActive;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
 import org.junit.After;
 import org.junit.Before;
@@ -42,7 +44,7 @@ import org.junit.Test;
  */
 public abstract class OperateServerTemplate {
 
-	private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
+	private final Logger LOGGER = Logger.getLogger(this.getClass());
 	
 	private ServersView serversView = new ServersView();
 	private ConsoleView consoleView = new ConsoleView();
@@ -57,9 +59,13 @@ public abstract class OperateServerTemplate {
 		serverIsPresentInServersView();
 		new WaitWhile(new JobIsRunning());
 		new WaitUntil(new ServerHasState("Stopped"));
+		LOGGER.step("Starting server");
 		startServer();
+		LOGGER.step("Restarting server");
 		restartServer();
+		LOGGER.step("Stopping server");
 		stopServer();
+		LOGGER.step("Deleting server");
 		deleteServer();
 	}
 	
@@ -70,7 +76,7 @@ public abstract class OperateServerTemplate {
 			sw.getServer(getServerName());
 		} catch(EclipseLayerException e) {
 			String failMessage = "Server \"" + getServerName() + "\" not found in Servers View.";
-			LOGGER.log(Level.SEVERE, failMessage, e);
+			LOGGER.error(failMessage, e);
 			fail(failMessage);
 		}
 	}
@@ -101,6 +107,14 @@ public abstract class OperateServerTemplate {
 
 	@After
 	public void cleanServerAndConsoleView() {
+		try{
+			LOGGER.step("Trying to close shell \"Warning: server process not terminated\"");
+			new DefaultShell("Warning: server process not terminated");
+			new PushButton("Yes").click();
+			LOGGER.step("Warning shell is closed.");
+		}catch(Exception e){
+			//nothing
+		}
 		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
 		serversView.open();
 		for (Server server : serversView.getServers()) {
@@ -124,6 +138,7 @@ public abstract class OperateServerTemplate {
 
 	public void restartServer() {
 		serversView.getServer(getServerName()).restart();
+		tryServerProcessNotTerminated();
 		final String state = "Started";
 		new WaitUntil(new ConsoleHasNoChange(TimePeriod.getCustom(5)), TimePeriod.LONG);
 		new WaitUntil(new ServerHasState(state));
@@ -136,12 +151,29 @@ public abstract class OperateServerTemplate {
 
 	public void stopServer() {
 		serversView.getServer(getServerName()).stop();
+		tryServerProcessNotTerminated();
 		final String state = "Stopped";
 		new WaitUntil(new ConsoleHasNoChange(TimePeriod.getCustom(5)), TimePeriod.LONG);
 		new WaitUntil(new ServerHasState(state));
 
 		assertNoException("Stopping server");
 		assertServerState("Stopping server", state);
+	}
+
+	/**
+	 * Closes "Server process not terminated" shell in case it pops up.
+	 */
+	private void tryServerProcessNotTerminated() {
+		try{
+			LOGGER.step("Trying to close shell \"Warning: server process not terminated\"");
+			DefaultShell warningShell = new DefaultShell("Warning: server process not terminated");
+			new PushButton("Yes");
+			new WaitWhile(new ShellIsActive(warningShell));
+			LOGGER.step("Warning shell is closed.");
+		}catch (SWTLayerException e) {
+			//Shell did not pop up -> do nothing
+		}
+		
 	}
 
 	public void deleteServer() {
