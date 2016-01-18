b2a103de82ead82f404aae9f527f79d222145022
rhopp <rhopp@redhat.com>
2016-01-18 15:44:04 +0100

    Runtime detection product tests stability improvements.

--- plugins/org.jboss.tools.runtime.reddeer/src/org/jboss/tools/runtime/reddeer/wizard/TaskWizardSecondPage.java
+++ plugins/org.jboss.tools.runtime.reddeer/src/org/jboss/tools/runtime/reddeer/wizard/TaskWizardSecondPage.java
@@ -1,7 +1,16 @@
 package org.jboss.tools.runtime.reddeer.wizard;
 
-import org.jboss.reddeer.common.exception.WaitTimeoutExpiredException;
-import org.jboss.reddeer.core.exception.CoreLayerException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.hamcrest.Matcher;
+import org.jboss.reddeer.common.wait.WaitUntil;
+import org.jboss.reddeer.core.condition.WidgetIsFound;
+import org.jboss.reddeer.core.matcher.ClassMatcher;
+import org.jboss.reddeer.core.matcher.WithMnemonicTextMatcher;
+import org.jboss.reddeer.core.matcher.WithStyleMatcher;
 import org.jboss.reddeer.jface.wizard.WizardPage;
 import org.jboss.reddeer.swt.impl.button.RadioButton;
 
@@ -9,16 +18,19 @@ public class TaskWizardSecondPage extends WizardPage{
 
 	public void acceptLicense(boolean accept){
 		if(accept){
-			
-			try {
-				new RadioButton("I accept the terms of the license agreement").click();
-			} catch (CoreLayerException e) {
-				// This can sometimes fail. Try again
-				new RadioButton("I accept the terms of the license agreement").click();
-			}
+			new WaitUntil(new WidgetIsFound<Button>(getRadioButtonMatchers("I accept the terms of the license agreement")));
+			new RadioButton("I accept the terms of the license agreement").click();
 		}else {
 			new RadioButton("I do not accept the terms of the license agreement").click();
 		}
+	}
+	
+	private Matcher<?>[] getRadioButtonMatchers(String text){
+		List<Matcher<?>> list= new ArrayList<Matcher<?>>();
+		list.add(new WithStyleMatcher(SWT.RADIO));
+		list.add(new WithMnemonicTextMatcher(text));
+		list.add(new ClassMatcher(Button.class));
+		return list.toArray(new Matcher[list.size()]);
 	}
 	
 }
