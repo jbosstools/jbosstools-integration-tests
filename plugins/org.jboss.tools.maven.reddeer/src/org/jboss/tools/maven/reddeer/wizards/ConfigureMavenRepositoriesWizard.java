cfd2262ab69e2b5a2639ee387e57c93d3f752635
jkopriva <jkopriva@redhat.com>
2016-11-16 14:10:39 +0100

    Maven repository Windows specific fix

--- plugins/org.jboss.tools.maven.reddeer/src/org/jboss/tools/maven/reddeer/wizards/ConfigureMavenRepositoriesWizard.java
+++ plugins/org.jboss.tools.maven.reddeer/src/org/jboss/tools/maven/reddeer/wizards/ConfigureMavenRepositoriesWizard.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.maven.reddeer.wizards;
 
+import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.core.condition.ShellWithTextIsActive;
 
 import java.util.List;
@@ -15,24 +16,24 @@ import org.jboss.reddeer.swt.impl.text.LabeledText;
 import org.jboss.reddeer.uiforms.impl.expandablecomposite.DefaultExpandableComposite;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
+import org.jboss.reddeer.common.wait.WaitWhile;
 
+public class ConfigureMavenRepositoriesWizard {
 
-public class ConfigureMavenRepositoriesWizard{
-	
-	public void open(){
+	public void open() {
 		new PushButton("Configure Maven Repositories...").click();
 		new DefaultShell("Maven Repositories");
 	}
-	
-	public String chooseRepositoryFromList(String repo, boolean activeByDefault, boolean snapshots){
+
+	public String chooseRepositoryFromList(String repo, boolean activeByDefault, boolean snapshots) {
 		new PushButton(" Add Repository...").click();
 		new DefaultShell("Add Maven Repository");
 		new DefaultCombo(0).setSelection(repo);
 		new CheckBox("Active by default").toggle(activeByDefault);
-		String a =new LabeledText("Name:").getText();
+		String a = new LabeledText("Name:").getText();
 		String b = new LabeledText("URL:").getText();
-		String nameWithUrl = a+"-"+b;
-		if(snapshots){
+		String nameWithUrl = a + "-" + b;
+		if (snapshots) {
 			new DefaultExpandableComposite("Advanced").setExpanded(true);
 			new CheckBox("Enable snapshots").toggle(true);
 		}
@@ -40,25 +41,25 @@ public class ConfigureMavenRepositoriesWizard{
 		new DefaultShell("Maven Repositories");
 		return nameWithUrl;
 	}
-	
-	public List<String> getRepositoriesList(){
+
+	public List<String> getRepositoriesList() {
 		new PushButton(" Add Repository...").click();
 		new DefaultShell("Add Maven Repository");
 		return new DefaultCombo(0).getItems();
 	}
-	
-	public String addRepository(String repoID, String repoURL, boolean activeByDefault, boolean snapshots){
+
+	public String addRepository(String repoID, String repoURL, boolean activeByDefault, boolean snapshots) {
 		new PushButton(" Add Repository...").click();
 		new DefaultShell("Add Maven Repository");
 		new DefaultCombo(0).setText(repoID);
 		new CheckBox("Active by default").toggle(activeByDefault);
 		new LabeledText("ID:").setText(repoID);
 		new LabeledText("Name:").setText(repoID);
-		String a =new LabeledText("Name:").getText();
+		String a = new LabeledText("Name:").getText();
 		new LabeledText("URL:").setText(repoURL);
 		String b = new LabeledText("URL:").getText();
-		String nameWithUrl = a+"-"+b;
-		if(snapshots){
+		String nameWithUrl = a + "-" + b;
+		if (snapshots) {
 			new DefaultExpandableComposite("Advanced").setExpanded(true);
 			new CheckBox("Enable snapshots").toggle(true);
 		}
@@ -66,18 +67,22 @@ public class ConfigureMavenRepositoriesWizard{
 		new DefaultShell("Maven Repositories");
 		return nameWithUrl;
 	}
-	
+
 	public void removeRepo(String repo) {
 		new DefaultTable(new DefaultGroup("Repositories")).select(repo);
+		if (!new PushButton(" Remove ").isEnabled()) {
+			//This can help when repo is not selected on windows
+			new DefaultShell("Maven Repositories");
+			new DefaultTable(new DefaultGroup("Repositories")).select(repo);
+		}
 		new PushButton(" Remove ").click();
 		new DefaultShell("Question?");
 		new PushButton("Yes").click();
 		new DefaultShell("Maven Repositories");
-		
 	}
-	
+
 	public boolean removeAllRepos() {
-		if(new DefaultTable(new DefaultGroup("Repositories")).rowCount() > 0){
+		if (new DefaultTable(new DefaultGroup("Repositories")).rowCount() > 0) {
 			new PushButton(" Remove All ").click();
 			new DefaultShell("Question?");
 			new PushButton("Yes").click();
@@ -85,40 +90,40 @@ public class ConfigureMavenRepositoriesWizard{
 			return true;
 		}
 		return false;
-		
+
 	}
-	
-	public void editRepo(String repo,boolean activeByDefault,String id, String name,String url){
+
+	public void editRepo(String repo, boolean activeByDefault, String id, String name, String url) {
 		new DefaultTable(new DefaultGroup("Repositories")).select(repo);
 		new PushButton(" Edit Repository...").click();
 		new DefaultShell("Edit Maven Repository");
 		new CheckBox("Active by default").toggle(activeByDefault);
-		if(id != null){
+		if (id != null) {
 			new LabeledText("ID:").setText(id);
 		}
-		if(name != null){
+		if (name != null) {
 			new LabeledText("Name:").setText(name);
 		}
-		if(url!=null){
+		if (url != null) {
 			new LabeledText("URL:").setText(url);
 		}
 		new PushButton("OK").click();
 		new DefaultShell("Maven Repositories");
 	}
-	
-	public void confirm(){
+
+	public void confirm() {
 		new DefaultShell("Maven Repositories");
 		new PushButton("Finish").click();
-		try{
-			new WaitUntil(new ShellWithTextIsActive("Confirm File Update"),TimePeriod.NORMAL);
+		try {
+			new WaitUntil(new ShellWithTextIsActive("Confirm File Update"), TimePeriod.NORMAL);
 			new DefaultShell("Confirm File Update");
 			new PushButton("Yes").click();
-		} catch(WaitTimeoutExpiredException ex){
-			
+		} catch (WaitTimeoutExpiredException ex) {
+
 		}
 	}
-	
-	public void cancel(){
+
+	public void cancel() {
 		new DefaultShell("Maven Repositories");
 		new PushButton("Cancel").click();
 	}
