5e4523e984f05aad37d5d2ce76580233293bca8f
Andre Dietisheim <adietish@redhat.com>
2016-11-22 14:29:09 +0100

    [JBIDE-23509] made sure ProjectNameValidationTest has a conn

--- tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/project/ProjectNameValidationTest.java
+++ tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/project/ProjectNameValidationTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2016 Red Hat, Inc.
+ * Copyright (c) 2016 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v 1.0 which accompanies this distribution,
@@ -10,9 +10,6 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.ui.bot.test.project;
 
-import static org.junit.Assert.fail;
-
-import org.jboss.reddeer.common.exception.RedDeerException;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitWhile;
 import org.jboss.reddeer.core.condition.ShellWithTextIsAvailable;
@@ -21,11 +18,19 @@ import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.text.DefaultText;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftConnectionRequirement.RequiredBasicConnection;
 import org.jboss.tools.openshift.reddeer.utils.OpenShiftLabel;
 import org.jboss.tools.openshift.reddeer.view.OpenShiftExplorerView;
 import org.jboss.tools.openshift.reddeer.view.resources.OpenShift3Connection;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author mlabuda@redhat.com
+ * @author adietish@redhat.com
+ */
+@RequiredBasicConnection
 public class ProjectNameValidationTest {
 	
 	public static final String PROJECT_NAME_FORMAT_ERROR = 
@@ -38,61 +43,51 @@ public class ProjectNameValidationTest {
 	public static final String PROJECT_NAME_MAX_LENGTH_ERROR = 
 			" Maximum length allowed is 63 characters for project name";
 	
+	@Before
+	public void setUp() {
+		openNewProjectShell();
+	}
+	
+	@After
+	public void cleanUp() {
+		closeNewProjectShell();
+	}
+	
 	@Test
 	public void testShortProjectName() {
-		openNewProjectShell();
 		new LabeledText(OpenShiftLabel.TextLabels.PROJECT_NAME).setText("s");
-		
-		try {
-			new DefaultText(PROJECT_NAME_SHORT_ERROR);
-		} catch (RedDeerException ex) {
-			fail("There is no validation message warning about small length of a project name.");
-		}
-		
-		closeNewProjectShell();
+		new DefaultText(PROJECT_NAME_SHORT_ERROR);
 	}
 	
 	@Test
 	public void testInvalidProjectNameFormat() {
-		openNewProjectShell();
 		new LabeledText(OpenShiftLabel.TextLabels.PROJECT_NAME).setText("--");
-		
-		try {
-			new DefaultText(PROJECT_NAME_FORMAT_ERROR);
-		} catch (RedDeerException ex) {
-			fail("There is no validation message warning about inappropriate project name.");
-		}
-		
-		closeNewProjectShell();
+		new DefaultText(PROJECT_NAME_FORMAT_ERROR);
 	}
 	
 	@Test
 	public void testForbiddenCharactersInProjectName() {
-		openNewProjectShell();
 		new LabeledText(OpenShiftLabel.TextLabels.PROJECT_NAME).setText("AAA");
-		
-		try {
-			new DefaultText(PROJECT_NAME_FORMAT_ERROR);
-		} catch (RedDeerException ex) {
-			fail("There is no validation message warning about inappropriate project name.");
-		}
-		
-		closeNewProjectShell();
+		new DefaultText(PROJECT_NAME_FORMAT_ERROR);
 	}
 	
 	@Test
 	public void testLongProjectName() {
-		openNewProjectShell();
-		new LabeledText(OpenShiftLabel.TextLabels.PROJECT_NAME).setText("012345678901234567890" +
-				"12345678901234567890123456789012345678901234567890123456789012345678901234567890" +
-				"123456789012345678901234");
-		try {
-			new DefaultText(PROJECT_NAME_MAX_LENGTH_ERROR);
-		} catch (RedDeerException ex) {
-			fail("There is no validation message warning about small length of a project name.");
-		}
-		
-		closeNewProjectShell();
+		new LabeledText(OpenShiftLabel.TextLabels.PROJECT_NAME).setText(
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"0123456789" +
+				"01234");
+		new DefaultText(PROJECT_NAME_MAX_LENGTH_ERROR);
 	}
 	
 	private void openNewProjectShell() {
