a506bc4ffcaa08fd8936245f9c28e0818f70713d
Len DiMaggio <ldimaggi@redhat.com>
2016-11-26 16:45:32 -0500

    Integration tests are failing dur to plugin lifecycle errors - set this error to IGNORE before running tests.

--- tests/org.jboss.tools.arquillian.ui.bot.test/src/org/jboss/tools/arquillian/ui/bot/test/project/CreateArquillianProject.java
+++ tests/org.jboss.tools.arquillian.ui.bot.test/src/org/jboss/tools/arquillian/ui/bot/test/project/CreateArquillianProject.java
@@ -2,9 +2,12 @@ package org.jboss.tools.arquillian.ui.bot.test.project;
 
 import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.eclipse.core.resources.Project;
+import org.jboss.reddeer.eclipse.m2e.core.ui.preferences.MavenErrorPreferencePage;
+import org.jboss.reddeer.eclipse.m2e.core.ui.preferences.MavenErrorPreferencePage.MavenErrorSeverity;
 import org.jboss.reddeer.eclipse.ui.perspectives.JavaPerspective;
 import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
 import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
 import org.jboss.tools.arquillian.ui.bot.reddeer.support.AddArquillianSupportDialog;
 import org.jboss.tools.arquillian.ui.bot.test.AbstractArquillianTestCase;
 import org.jboss.tools.maven.reddeer.wizards.MavenProjectWizard;
@@ -19,16 +22,28 @@ import org.junit.Test;
  * <ul>
  * 	<li> there are no errors or warnings in Problems view
  * </ul>
- * @author Lucia Jelinkova
+ * @author Lucia Jelinkova, Len DiMaggio
  *
  */
 @OpenPerspective(JavaPerspective.class)
 public class CreateArquillianProject extends AbstractArquillianTestCase{
+	
+	private WorkbenchPreferenceDialog preferencesDialog = new WorkbenchPreferenceDialog();
+	private MavenErrorPreferencePage theMavenPrefPage = new MavenErrorPreferencePage();
 
 	private static final Logger log = Logger.getLogger(CreateArquillianProject.class);
 	
 	@Before
 	public void setupProject(){
+
+		/* Set the Maven->Error preference to ignore plugin lifecyle errors */
+		log.info("Set the Maven->Error preference to ignore plugin lifecyle errors");
+		preferencesDialog.open();
+		preferencesDialog.select(theMavenPrefPage);		
+		MavenErrorPreferencePage theMavenPrefPage = new MavenErrorPreferencePage();
+		theMavenPrefPage.setPluginExecution(MavenErrorSeverity.IGNORE);
+		preferencesDialog.ok();
+		
 		log.step("Create maven project with name " + PROJECT_NAME);
 		MavenProjectWizard wizard = new MavenProjectWizard();
 		wizard.open();
