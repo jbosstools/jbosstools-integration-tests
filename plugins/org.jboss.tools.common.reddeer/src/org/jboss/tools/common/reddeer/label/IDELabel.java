84f105573ba7213ca270bc453e9439912ae041c5
jkopriva <jkopriva@redhat.com>
2016-12-06 15:51:05 +0100

    Added stackTraceToString method + added missing copyright

--- plugins/org.jboss.tools.common.reddeer/src/org/jboss/tools/common/reddeer/label/IDELabel.java
+++ plugins/org.jboss.tools.common.reddeer/src/org/jboss/tools/common/reddeer/label/IDELabel.java
@@ -1,4 +1,14 @@
- package org.jboss.tools.common.reddeer.label;
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.common.reddeer.label;
 
 /**
  * Base label constants for all widgets. Naming convention is (except buttons
