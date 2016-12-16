d56f3b38ef5993a8325cb2b1acc6a2d26d6d067b
André Dietisheim <adietish@redhat.com>
2016-12-16 15:34:15 +0100

    [JBIDE-23648] fixed ImageTagTest#addUpperCaseTagToImage (#1708)

--- tests/org.jboss.tools.docker.ui.bot.test/src/org/jboss/tools/docker/ui/bot/test/image/ImageTagTest.java
+++ tests/org.jboss.tools.docker.ui.bot.test/src/org/jboss/tools/docker/ui/bot/test/image/ImageTagTest.java
@@ -14,11 +14,13 @@ package org.jboss.tools.docker.ui.bot.test.image;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitWhile;
 import org.jboss.reddeer.core.condition.JobIsRunning;
-import org.jboss.reddeer.core.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.eclipse.ui.views.properties.PropertiesView;
 import org.jboss.reddeer.swt.impl.button.OkButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.tools.docker.reddeer.ui.DockerImagesTab;
 import org.jboss.tools.docker.ui.bot.test.AbstractDockerBotTest;
 import org.junit.After;
@@ -31,10 +33,14 @@ import org.junit.Test;
  */
 
 public class ImageTagTest extends AbstractDockerBotTest {
-	private static String imageName = "busybox";
-	private static String imageNameToPull = "busybox:latest";
-	private static String imageTag = "testtag";
-	private static String imageTagUppercase = "UPPERCASETAG";
+	private static final String IMAGE_NAME = "busybox";
+	private static final String IMAGE_NAME_TO_PULL = "busybox:latest";
+	private static final String IMAGE_TAG = "testtag";
+	private static String IMAGE_TAG_UPPERCASE = "UPPERCASETAG";
+
+	// docker daemon 1.11 errors with uppercase tags, priors dont
+	private static final int DAEMON_MAJOR_VERSION = 1; 
+	private static final int DAEMON_MINOR_VERSION = 11;
 
 	@Test
 	public void testAddRemoveTagToImage() {
@@ -42,42 +48,85 @@ public class ImageTagTest extends AbstractDockerBotTest {
 		imageTab.activate();
 		imageTab.refresh();
 		new WaitWhile(new JobIsRunning(), TimePeriod.NORMAL);
-		pullImage(imageNameToPull);
+		pullImage(IMAGE_NAME_TO_PULL);
 		new WaitWhile(new JobIsRunning());
-		assertTrue("Image has not been deployed!", imageIsDeployed(imageName));
+		assertTrue("Image has not been deployed!", imageIsDeployed(IMAGE_NAME));
 
 		imageTab.activate();
-		imageTab.addTagToImage(imageName, imageTag);
+		imageTab.addTagToImage(IMAGE_NAME, IMAGE_TAG);
 		new WaitWhile(new JobIsRunning());
-		assertTrue("Image tag has not been added", imageTab.getImageTags(imageName).contains(imageTag));
+		assertTrue("Image tag has not been added", imageTab.getImageTags(IMAGE_NAME).contains(IMAGE_TAG));
 
 		imageTab.activate();
-		imageTab.removeTagFromImage(imageName, imageTag);
+		imageTab.removeTagFromImage(IMAGE_NAME, IMAGE_TAG);
 		new WaitWhile(new JobIsRunning());
-		assertTrue("ImageTaghasNotBeenRemoved", !imageTab.getImageTags(imageName).contains(imageTag));
+		assertTrue("ImageTaghasNotBeenRemoved", !imageTab.getImageTags(IMAGE_NAME).contains(IMAGE_TAG));
 	}
 	
+	/**
+	 * Tries to add an uppercase tag to an image. This errors in docker daemon
+	 * >= 1.11 while it succeeds in older versions.
+	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=509223
+	 */
 	@Test
-	public void testAddRemoveUpperCaseTagToImage() {
+	public void testAddUpperCaseTagToImage() {
 		DockerImagesTab imageTab = new DockerImagesTab();
 		imageTab.activate();
 		imageTab.refresh();
 		new WaitWhile(new JobIsRunning(), TimePeriod.NORMAL);
-		pullImage(imageNameToPull);
+		pullImage(IMAGE_NAME_TO_PULL);
 		new WaitWhile(new JobIsRunning());
-		assertTrue("Image has not been deployed!", imageIsDeployed(imageName));
+		assertTrue("Image has not been deployed!", imageIsDeployed(IMAGE_NAME));
 
 		imageTab.activate();
-		imageTab.addTagToImage(imageName, imageTagUppercase);
-		new ShellWithTextIsAvailable("Error tagging image to <" + imageTagUppercase + ">");
-		new OkButton().click();
-		assertFalse("Image tag has been added!", imageTab.getImageTags(imageName).contains(imageTag));
+		imageTab.addTagToImage(IMAGE_NAME, IMAGE_TAG_UPPERCASE);
+		
+		if (isDockerDaemon(DAEMON_MAJOR_VERSION, DAEMON_MINOR_VERSION)) {
+			// docker daemon >= 1.11
+			new DefaultShell("Error tagging image to <" + IMAGE_TAG_UPPERCASE + ">");
+			new OkButton().click();
+			assertFalse("Image tag has been added!", imageTab.getImageTags(IMAGE_NAME).contains(IMAGE_TAG));
+		} else {
+			assertTrue("Image tag has not been added!", imageTab.getImageTags(IMAGE_NAME).contains(IMAGE_TAG_UPPERCASE));
+		}
+	}
+
+	/**
+	 * Returns {@code true} if the running docker daemon matches at least the
+	 * given major and minor version. Returns {@code false} otherwise.
+	 * 
+	 * @param majorVersion
+	 * @param minorVersion
+	 * @return
+	 */
+	private boolean isDockerDaemon(int majorVersion, int minorVersion) {
+		getConnection().select();
+		PropertiesView infoTab = openPropertiesTab("Info");
+		String daemonVersion = infoTab.getProperty("Version").getPropertyValue();
+		assertTrue("Could not retrieve docker daemon version.", !StringUtils.isBlank(daemonVersion));
+		String[] versionComponents = daemonVersion.split("\\.");
+		assertTrue("Could not evaluate docker daemon version " + daemonVersion, 
+				versionComponents == null || versionComponents.length >= 2); 
+		int actualMajorVersion = Integer.parseInt(versionComponents[0]); 			
+		if (actualMajorVersion > majorVersion) {
+			return true;
+		}
+		int actualMinorVersion = Integer.parseInt(versionComponents[1]);
+		return actualMinorVersion >= minorVersion;
 	}
 
 	@After
 	public void after() {
-		deleteImageContainerAfter(imageName);
+		deleteImageContainerAfter(IMAGE_NAME);
 		cleanUpWorkspace();
 	}
+	
+	protected PropertiesView openPropertiesTab(String tabName) {
+		PropertiesView propertiesView = new PropertiesView();
+		propertiesView.open();
+		propertiesView.selectTab(tabName);
+		return propertiesView;
+	}
+
 
 }
