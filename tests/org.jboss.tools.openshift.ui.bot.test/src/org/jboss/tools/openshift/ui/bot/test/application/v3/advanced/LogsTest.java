407abe0e7f28dd3fe471b8225fb25556628e8531
André Dietisheim <adietish@redhat.com>
2017-01-13 13:39:36 +0100

    [JBIDE-23659] fixed pod logs test (#1724)

--- tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/application/v3/advanced/LogsTest.java
+++ tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/application/v3/advanced/LogsTest.java
@@ -10,99 +10,154 @@
  ******************************************************************************/
 package org.jboss.tools.openshift.ui.bot.test.application.v3.advanced;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.reddeer.common.exception.WaitTimeoutExpiredException;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
 import org.jboss.reddeer.eclipse.condition.ConsoleHasNoChange;
+import org.jboss.reddeer.eclipse.condition.ConsoleHasText;
 import org.jboss.reddeer.eclipse.ui.console.ConsoleView;
+import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
-import org.jboss.tools.openshift.reddeer.condition.AmountOfResourcesExists;
-import org.jboss.tools.openshift.reddeer.condition.ApplicationPodIsRunning;
+import org.jboss.tools.common.reddeer.perspectives.JBossPerspective;
 import org.jboss.tools.openshift.reddeer.condition.ConsoleHasSomeText;
-import org.jboss.tools.openshift.reddeer.condition.OpenShiftResourceExists;
 import org.jboss.tools.openshift.reddeer.enums.Resource;
-import org.jboss.tools.openshift.reddeer.enums.ResourceState;
-import org.jboss.tools.openshift.reddeer.utils.DatastoreOS3;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftCommandLineToolsRequirement.OCBinary;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftConnectionRequirement;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftConnectionRequirement.RequiredBasicConnection;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftProjectRequirement;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftProjectRequirement.RequiredProject;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftResources;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftServiceRequirement;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftServiceRequirement.RequiredService;
 import org.jboss.tools.openshift.reddeer.utils.OpenShiftLabel;
-import org.jboss.tools.openshift.reddeer.utils.TestUtils;
 import org.jboss.tools.openshift.reddeer.view.OpenShiftExplorerView;
+import org.jboss.tools.openshift.reddeer.view.resources.OpenShift3Connection;
+import org.jboss.tools.openshift.reddeer.view.resources.OpenShiftProject;
 import org.jboss.tools.openshift.reddeer.view.resources.OpenShiftResource;
-import org.jboss.tools.openshift.ui.bot.test.application.v3.create.AbstractCreateApplicationTest;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class LogsTest extends AbstractCreateApplicationTest {
+@RunWith(RedDeerSuite.class)
+@OpenPerspective(JBossPerspective.class)
+@OCBinary
+@RequiredBasicConnection()
+@RequiredProject()
+@RequiredService(service=OpenShiftResources.EAP_SERVICE, template=OpenShiftResources.EAP_TEMPLATE)
+public class LogsTest {
+
+	private static final String  PODS_TAB_BUILD_POD_POSTFIX = "build";
+	private static final int WAIT_CONSOLE_NO_CHANGE = 7;
+	private static final int WAIT_CONSOLE_PUSH_SUCCESS = 600;
+
+	@InjectRequirement
+	private OpenShiftProjectRequirement requiredProject;
+	@InjectRequirement
+	private OpenShiftConnectionRequirement requiredConnection;
+	@InjectRequirement
+	private OpenShiftServiceRequirement requiredService;
+
+	private OpenShiftExplorerView explorer;
+	private ConsoleView consoleView;
+	private String serviceName;
 	
-	@BeforeClass
-	public static void waitForApplication() {
-		new WaitUntil(new OpenShiftResourceExists(Resource.BUILD, "eap-app-1", ResourceState.COMPLETE), 
-				TimePeriod.getCustom(600), true, TimePeriod.getCustom(8));
+	@Before
+	public void setUp() {
+		assertThat(requiredService.getService(), notNullValue());
+		this.serviceName = requiredService.getService().getName();
+
+		this.explorer = new OpenShiftExplorerView();
+		explorer.open();
+		explorer.activate();
 		
-		new WaitUntil(new AmountOfResourcesExists(Resource.POD, 2), TimePeriod.VERY_LONG, true,
-				TimePeriod.getCustom(5));
+		this.consoleView = new ConsoleView();
+		this.consoleView.open();
+		
 	}
-	
+
 	@Test
-	public void testPodLogOfApplicationPod() {
-		TestUtils.setUpOcBinary();
-		
-		ApplicationPodIsRunning applicationPodIsRunning = new ApplicationPodIsRunning();
-		new WaitUntil(applicationPodIsRunning, TimePeriod.LONG);
-		
-		OpenShiftResource pod  = new OpenShiftExplorerView().getOpenShift3Connection().getProject().
-			getOpenShiftResource(Resource.POD, applicationPodIsRunning.getApplicationPodName());
-		pod.select();
+	public void shouldShowLogFromApplicationPodContextMenu() {
+		OpenShiftResource pod  = getApplicationPod(serviceName, getOpenShiftProject(requiredProject.getProjectName(), explorer));
 		String podName = pod.getName();
-		
+		pod.select();
 		new ContextMenu(OpenShiftLabel.ContextMenu.POD_LOG).select();
-		
-		ConsoleView consoleView = new ConsoleView();
-		
-		new WaitUntil(new ConsoleHasSomeText() {
-			@Override public String errorMessage() {
-				return "Console of pod log is not opened or does not have text";
-			}}, TimePeriod.NORMAL);
-		new WaitUntil(new ConsoleHasNoChange(TimePeriod.getCustom(7)), TimePeriod.VERY_LONG);
-		
-		assertTrue("Console label is incorrect, it should contains project name and pod name.\n"
-						+ "but label is: " + consoleView.getConsoleLabel(), consoleView.getConsoleLabel().contains(
-						DatastoreOS3.PROJECT1 + "\\" + podName));
+
+		new WaitUntil(new ConsoleHasSomeText(), TimePeriod.NORMAL);
+		new WaitUntil(new ConsoleHasNoChange(TimePeriod.getCustom(WAIT_CONSOLE_NO_CHANGE)), TimePeriod.VERY_LONG);
+
+		assertTrue("Console label is incorrect, it should contains project name and pod name.\n" + "but label is: " + consoleView.getConsoleLabel(),
+				consoleView.getConsoleLabel().contains(requiredProject.getProjectName() + "\\" + podName));
 		assertTrue("Console text should contain output from EAP runtime",
 				consoleView.getConsoleText().contains("Admin console listening on"));
 	}
-	
-	@Test
-	public void testPodLogOfCompletedBuildPod() {
-		TestUtils.setUpOcBinary();
-		
-		new WaitUntil(new OpenShiftResourceExists(Resource.BUILD, "eap-app-1"), 
-				TimePeriod.getCustom(600), true, TimePeriod.getCustom(8));
-		OpenShiftExplorerView explorer = new OpenShiftExplorerView();
-		
-		explorer.activate();
-		explorer.getOpenShift3Connection().
-				getProject().getOpenShiftResources(Resource.BUILD).get(0).select();
-		new ContextMenu(OpenShiftLabel.ContextMenu.BUILD_LOG).select();
-		
-		new WaitUntil(new ConsoleHasSomeText() {
-			@Override public String errorMessage() {
-				return "Console is not opened or does not have text. Could be failing because of JBIDE-23622.";
-			}}, TimePeriod.NORMAL);
 
-		ConsoleView consoleView = new ConsoleView();
-		assertTrue("Console label is incorrect, it should contains project name and name of build pod.\n"
-				+ "but label is: " + consoleView.getConsoleLabel(), consoleView.getConsoleLabel().contains(
-				DatastoreOS3.PROJECT1 + "\\eap-app-1-build\n Could be failing because of JBIDE-23622."));
-		
+	@Test
+	public void shouldShowLogFromBuildPodContextMenu() {
+		OpenShiftResource pod = getBuildPod(serviceName, getOpenShiftProject(requiredProject.getProjectName(), explorer));
+		String podName = pod.getName();
+		pod.select();
+		new ContextMenu(OpenShiftLabel.ContextMenu.BUILD_LOG).select();
+
+		new WaitUntil(new ConsoleHasSomeText(), TimePeriod.NORMAL);
+		new WaitUntil(new ConsoleHasNoChange(TimePeriod.getCustom(WAIT_CONSOLE_NO_CHANGE)), TimePeriod.VERY_LONG);
+
+		assertTrue("Console label is incorrect, it should contain project name and name of build pod.\n"
+				+ "but label is: " + consoleView.getConsoleLabel(), 
+				consoleView.getConsoleLabel().contains(requiredProject.getProjectName() + "\\" + podName));
 		try {
-			new WaitUntil(new org.jboss.reddeer.eclipse.condition.ConsoleHasText("Push successful"),
-				TimePeriod.getCustom(600));
+			new WaitUntil(new ConsoleHasText("Push successful"), 
+					TimePeriod.getCustom(WAIT_CONSOLE_PUSH_SUCCESS));
 		} catch (WaitTimeoutExpiredException ex) {
 			fail("There should be output of succesful build in console log, but there is not.\n"
 					+ "Check whether output has not changed. Assumed output in the end of log is 'Push successful'");
 		}
 	}
+
+	private OpenShiftProject getOpenShiftProject(String projectName, OpenShiftExplorerView explorer) {
+		OpenShift3Connection connection = explorer.getOpenShift3Connection(requiredConnection.getConnection());
+		OpenShiftProject project = connection.getProject(projectName);
+		assertThat("Could not find project " + projectName + " in OpenShift Explorer",  project, notNullValue());
+		project.expand();
+		return project;
+	}
+
+	private OpenShiftResource getBuildPod(String name, OpenShiftProject project) {
+		assertTrue(!StringUtils.isBlank(name));
+
+		for (OpenShiftResource pod : project.getOpenShiftResources(Resource.BUILD)) {
+			String podName = pod.getName();
+			if (!StringUtils.isBlank(podName)) {
+				if (podName.startsWith(name)) {
+					return pod;
+				}
+			}
+		}
+		fail("Build pod for service " + serviceName + " was not found");
+		return null;
+	}
+
+	private OpenShiftResource getApplicationPod(String name, OpenShiftProject project) {
+		assertTrue(!StringUtils.isBlank(name));
+
+		for (OpenShiftResource pod : project.getOpenShiftResources(Resource.POD)) {
+			String podName = pod.getName();
+			if (!StringUtils.isBlank(podName)) {
+				if (podName.startsWith(name)
+						&& !podName.endsWith(PODS_TAB_BUILD_POD_POSTFIX)) {
+					return pod;
+				}
+			}
+		}
+		fail("Pod for service " + serviceName + " was not found");
+		return null;
+	}
+
 }
