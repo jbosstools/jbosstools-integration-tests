9b27ec14f2ce2bfa02de44031476d65cdc7865d7
Jan Richter <jrichter@redhat.com>
2017-02-06 10:25:59 +0100

    Isolate OS3 RemoveConnectionTest class (#1745)

--- tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/connection/v3/RemoveConnectionTest.java
+++ tests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/connection/v3/RemoveConnectionTest.java
@@ -19,17 +19,15 @@ import org.jboss.reddeer.core.condition.ShellWithTextIsAvailable;
 import org.jboss.reddeer.swt.impl.button.OkButton;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.tools.openshift.reddeer.requirement.OpenShiftConnectionRequirement.RequiredBasicConnection;
 import org.jboss.tools.openshift.reddeer.utils.DatastoreOS3;
 import org.jboss.tools.openshift.reddeer.utils.OpenShiftLabel;
 import org.jboss.tools.openshift.reddeer.view.OpenShiftExplorerView;
-import org.jboss.tools.openshift.reddeer.view.OpenShiftExplorerView.AuthenticationMethod;
-import org.jboss.tools.openshift.reddeer.view.OpenShiftExplorerView.ServerType;
-import org.junit.After;
 import org.junit.Test;
 
+@RequiredBasicConnection
 public class RemoveConnectionTest {
-
-	private boolean connectionRemoved = false;
+	
 	private OpenShiftExplorerView explorer = new OpenShiftExplorerView();
 	
 	@Test
@@ -42,28 +40,11 @@ public class RemoveConnectionTest {
 		new DefaultShell(OpenShiftLabel.Shell.REMOVE_CONNECTION);
 		new OkButton().click();
 		
-		connectionRemoved = true;
-		
 		new WaitWhile(new ShellWithTextIsAvailable(OpenShiftLabel.Shell.REMOVE_CONNECTION),
 				TimePeriod.LONG);
 		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
 		
 		assertFalse("Connection is still presented in OpenShift explorer",
 				explorer.connectionExists(DatastoreOS3.USERNAME, DatastoreOS3.SERVER));
-	}
-	
-	@After
-	public void testRecreateConnection() {
-		explorer.open();
-		if (connectionRemoved) {
-			explorer.openConnectionShellViaToolItem();
-			if (DatastoreOS3.AUTH_METHOD.equals(AuthenticationMethod.BASIC)) {
-				explorer.connectToOpenShift(DatastoreOS3.SERVER, DatastoreOS3.USERNAME, DatastoreOS3.PASSWORD, 
-						false, false, ServerType.OPENSHIFT_3, AuthenticationMethod.BASIC, false);
-			} else if (DatastoreOS3.AUTH_METHOD.equals(AuthenticationMethod.OAUTH)) {
-				explorer.connectToOpenShift(DatastoreOS3.SERVER, DatastoreOS3.USERNAME, DatastoreOS3.TOKEN, false,
-						false, ServerType.OPENSHIFT_3, AuthenticationMethod.OAUTH, false);
-			}
-		}
 	}
 }
