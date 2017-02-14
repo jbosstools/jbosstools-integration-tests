a44dca7fb9562484141b3100e0be6a1e1de6da4f
Ondrej Dockal <odockal@redhat.com>
2017-02-14 16:36:06 +0100

    Add expected exception to stepListenerTest unless resolved

---
+++ tests/org.jboss.tools.batch.ui.bot.test/src/org/jboss/tools/batch/ui/bot/test/editor/jobxml/CustomAssertionErrorJBIDE23656.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.batch.ui.bot.test.editor.jobxml;
+
+public class CustomAssertionErrorJBIDE23656 extends AssertionError {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5675052313339397472L;
+}
