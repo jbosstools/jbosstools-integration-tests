81e053440f619f6a633a9cb2e92f6a4a6ee5336c
Pavol Srna <psrna@redhat.com>
2017-02-16 11:04:44 +0100

    Enhance NodeJS Integration Tests - wait for nodejs process to be terminated (#1753)

--- tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/jst/ui/bot/test/nodejs/NodeJSLauncherTest.java
+++ tests/org.jboss.tools.jst.ui.bot.test/src/org/jboss/tools/jst/ui/bot/test/nodejs/NodeJSLauncherTest.java
@@ -20,7 +20,7 @@ import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.eclipse.ui.console.ConsoleView;
 import org.jboss.reddeer.eclipse.ui.perspectives.DebugPerspective;
 import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
-import org.jboss.tools.jst.ui.bot.test.JSTTestBase;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -29,74 +29,105 @@ import org.junit.Assert;
 
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchManager;
+import org.jboss.tools.jst.ui.bot.test.JSTTestBase;
+
 /**
  * Tests for NodeJS Launchers
- * 
+ *
  * @author Pavol Srna
  *
  */
 @OpenPerspective(DebugPerspective.class)
 public class NodeJSLauncherTest extends JSTTestBase {
-
-	private static String TEST_APP_NAME = "jsdt-node-test-project";
-	private static String IMPORT_PATH = "resources/" + TEST_APP_NAME;
-
-	@BeforeClass
-	public static void prepare() {
-		/* PE is closed in Debug perspective, open it */
-		new ProjectExplorer().open();
-		importExistingProject(IMPORT_PATH);
-		assertTrue(TEST_APP_NAME + " has not been imported!", new ProjectExplorer().containsProject(TEST_APP_NAME));
-		npmInstall(TEST_APP_NAME);
-	}
-
-	@AfterClass
-	public static void cleanup() {
-		ShellHandler.getInstance().closeAllNonWorbenchShells();
-		new ProjectExplorer().deleteAllProjects();
-	}
-
-	@Test
-	public void testNodeJSRunAsLauncherAvailable() {
-		ExplorerItem indexJS = new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js");
-		indexJS.select();
-		assertTrue("'Run As -> Node.js Application' not available!", runAsNodeJSAppMenu().isEnabled());
-	}
-
-	@Test
-	public void testNodeJSAppIsRunning() {
-		ExplorerItem indexJS = new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js");
-		indexJS.select();
-		runAsNodeJSAppMenu().select();
-
-		ConsoleView console = new ConsoleView();
-		console.activate();
-		try {
-			new WaitUntil(new ConsoleHasText("Listening on port 3000"));
-		} catch (WaitTimeoutExpiredException e) {
-			Assert.fail("Node.js App is not running!");
-		}
-		console.terminateConsole();
-	}
-
-	@Test
-	public void testNodeJSDebugAsLauncherAvailable() {
-		assertTrue("'Debug As -> Node.js Application' not available!",
-				debugAsNodeJSAppMenu(new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js"))
-						.isEnabled());
-	}
-
-	@Test
-	public void testNodeJSAppIsDebugging() {
-		debugAsNodeJSAppMenu(new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js")).select();
-
-		ConsoleView console = new ConsoleView();
-		console.activate();
-		new WaitUntil(new ConsoleHasText("Debugger listening"), TimePeriod.LONG);
-
-		assertTrue("Node.js App is not debugging!", console.getConsoleText().contains("Debugger listening"));
-
-		console.terminateConsole();
-	}
-
+    
+    private static String TEST_APP_NAME = "jsdt-node-test-project";
+    private static String IMPORT_PATH = "resources/" + TEST_APP_NAME;
+    
+    private static int APP_PORT = 3000;
+    private static int DEBUG_PORT = 5858;
+    
+    private NodeJSLaunchListener launchListener = null;
+    
+    @BeforeClass
+    public static void prepare() {
+        /* PE is closed in Debug perspective, open it */
+        new ProjectExplorer().open();
+        importExistingProject(IMPORT_PATH);
+        assertTrue(TEST_APP_NAME + " has not been imported!", new ProjectExplorer().containsProject(TEST_APP_NAME));
+        npmInstall(TEST_APP_NAME);
+    }
+    
+    @AfterClass
+    public static void cleanup() {
+        ShellHandler.getInstance().closeAllNonWorbenchShells();
+        new ProjectExplorer().deleteAllProjects();
+    }
+    
+    @Test
+    public void testNodeJSRunAsLauncherAvailable() {
+        ExplorerItem indexJS = new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js");
+        indexJS.select();
+        assertTrue("'Run As -> Node.js Application' not available!", runAsNodeJSAppMenu().isEnabled());
+    }
+    
+    @Test
+    public void testNodeJSAppIsRunning() {
+        assertTrue(portAvailable(APP_PORT));
+    	
+        launchListener = new NodeJSLaunchListener();
+        ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
+        manager.addLaunchListener(launchListener);
+        
+        ExplorerItem indexJS = new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js");
+        indexJS.select();
+        runAsNodeJSAppMenu().select();
+        
+        ConsoleView console = new ConsoleView();
+        console.activate();
+        try {
+            new WaitUntil(new ConsoleHasText("Listening on port 3000"));
+        } catch (WaitTimeoutExpiredException e) {
+            Assert.fail("Node.js App is not running!");
+        }
+    }
+    
+    @Test
+    public void testNodeJSDebugAsLauncherAvailable() {
+        assertTrue("'Debug As -> Node.js Application' not available!",
+                   debugAsNodeJSAppMenu(new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js"))
+                   .isEnabled());
+    }
+    
+    @Test
+    public void testNodeJSAppIsDebugging() {
+    	assertTrue(portAvailable(APP_PORT));
+    	assertTrue(portAvailable(DEBUG_PORT));
+    	
+        launchListener = new NodeJSLaunchListener();
+        ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
+        manager.addLaunchListener(launchListener);
+        
+        debugAsNodeJSAppMenu(new ProjectExplorer().getProject(TEST_APP_NAME).getProjectItem("index.js")).select();
+        
+        ConsoleView console = new ConsoleView();
+        console.activate();
+        new WaitUntil(new ConsoleHasText("Debugger listening"), TimePeriod.LONG);
+        
+        assertTrue("Node.js App is not debugging!", console.getConsoleText().contains("Debugger listening"));
+    }
+    
+    @After
+    public void terminate(){
+    	if(launchListener != null){
+    		ILaunch launch = launchListener.getNodeJSLaunch();
+    		if(launch != null){
+    			terminatePrcs(launch.getProcesses());
+    		}
+            ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
+            manager.removeLaunchListener(launchListener);
+    	}
+    }
 }
