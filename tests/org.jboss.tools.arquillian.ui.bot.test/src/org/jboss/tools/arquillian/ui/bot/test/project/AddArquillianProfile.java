d8028f2136192454fc58f2b962e474d128a61069
jkopriva <jkopriva@redhat.com>
2017-03-17 15:46:18 +0100

    JBIDE-23866 - Arquillian tests refactoring and fixes, adding copyright

--- tests/org.jboss.tools.arquillian.ui.bot.test/src/org/jboss/tools/arquillian/ui/bot/test/project/AddArquillianProfile.java
+++ tests/org.jboss.tools.arquillian.ui.bot.test/src/org/jboss/tools/arquillian/ui/bot/test/project/AddArquillianProfile.java
@@ -1,53 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation    
+ ******************************************************************************/
 package org.jboss.tools.arquillian.ui.bot.test.project;
-import org.jboss.reddeer.common.logging.Logger;
-import org.jboss.reddeer.common.wait.TimePeriod;
-import org.jboss.reddeer.common.wait.WaitWhile;
-import org.jboss.reddeer.core.condition.JobIsRunning;
-import org.jboss.reddeer.eclipse.core.resources.Project;
-import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
-import org.jboss.reddeer.swt.impl.button.PushButton;
-import org.jboss.tools.arquillian.ui.bot.reddeer.profile.AddArquillianProfilesDialog;
+
 import org.jboss.tools.arquillian.ui.bot.test.AbstractArquillianTestCase;
-import org.jboss.tools.maven.reddeer.profiles.SelectProfilesDialog;
+import org.junit.Before;
 import org.junit.Test;
 
-
 /**
- * Add remote Wildfly Arquillian profile using right-click on project. 
+ * Add remote Wildfly Arquillian profile using right-click on project.
  * 
  * @author Lucia Jelinkova
  *
  */
 public class AddArquillianProfile extends AbstractArquillianTestCase {
 
-	private static final Logger log = Logger.getLogger(AddArquillianProfile.class);
-	
+	@Before
+	public void before() {
+		prepareProject();
+	}
+
 	@Test
-	public void testAddingProfile(){
-		addProfile();
+	public void testAddingProfile() {
+		addArquillianProfile();
 		selectMavenProfile();
 		forceMavenRepositoryUpdate();
 		checkProblems();
 	}
-	
-	private void addProfile(){
-		log.step("Add Arquillian profile");
-		PackageExplorer explorer = new PackageExplorer();
-		explorer.open();
-		Project project = explorer.getProject(PROJECT_NAME);
-		
-		AddArquillianProfilesDialog dialog = new AddArquillianProfilesDialog();
-		dialog.open(project);
-		dialog.selectProfile(PROFILE_NAME);
-		dialog.ok();
-	}
-	
-	private void selectMavenProfile() {
-		log.step("Select maven profile");
-		SelectProfilesDialog dialog = new SelectProfilesDialog(PROJECT_NAME);
-		dialog.open();
-		dialog.activateProfile(PROFILE_NAME);
-		new PushButton("OK").click();
-		new WaitWhile(new JobIsRunning(),TimePeriod.VERY_LONG);
-	}
+
 }
