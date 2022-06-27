231b108edea5cae4f373785b50d19e794f0afb02
Oleksii Korniienko <olkornii@redhat.com>
2022-06-27 11:25:09 +0200

    Reworked testNG project
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- tests/org.jboss.tools.testng.ui.bot.test/src/org/jboss/tools/testng/ui/bot/test/AbstractTestNGTest.java
+++ tests/org.jboss.tools.testng.ui.bot.test/src/org/jboss/tools/testng/ui/bot/test/AbstractTestNGTest.java
@@ -12,29 +12,31 @@ package org.jboss.tools.testng.ui.bot.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.util.List;
 
 import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.common.wait.WaitWhile;
-import org.eclipse.reddeer.eclipse.m2e.core.ui.wizard.MavenProjectWizard;
 import org.eclipse.reddeer.eclipse.ui.navigator.resources.ProjectExplorer;
 import org.eclipse.reddeer.eclipse.ui.problems.Problem;
 import org.eclipse.reddeer.eclipse.ui.views.markers.ProblemsView;
 import org.eclipse.reddeer.eclipse.ui.views.markers.ProblemsView.ProblemType;
+import org.eclipse.reddeer.swt.condition.ControlIsEnabled;
 import org.eclipse.reddeer.swt.impl.button.FinishButton;
 import org.eclipse.reddeer.swt.impl.button.NextButton;
+import org.eclipse.reddeer.swt.impl.button.OkButton;
 import org.eclipse.reddeer.swt.impl.button.PushButton;
-import org.eclipse.reddeer.swt.impl.combo.LabeledCombo;
 import org.eclipse.reddeer.swt.impl.list.DefaultList;
 import org.eclipse.reddeer.swt.impl.menu.ContextMenuItem;
 import org.eclipse.reddeer.swt.impl.tab.DefaultTabItem;
-import org.eclipse.reddeer.swt.impl.table.DefaultTable;
-import org.eclipse.reddeer.swt.impl.text.LabeledText;
 import org.eclipse.reddeer.swt.impl.tree.DefaultTree;
 import org.eclipse.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
 import org.eclipse.reddeer.workbench.handler.WorkbenchShellHandler;
 import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
+import org.jboss.tools.easymport.reddeer.wizard.SmartImportRootWizardPage;
+import org.jboss.tools.easymport.reddeer.wizard.SmartImportWizard;
 import org.junit.After;
 
 /**
@@ -51,41 +53,48 @@ public abstract class AbstractTestNGTest {
 		assertEquals("There should be no errors in imported project", 0, problems.size());
 
 	}
-
+	
 	public static void createProject(String projectName) {
-		openProjectWizard();
-
-		new LabeledCombo("Group Id:").setText(projectName);
-		new LabeledCombo("Artifact Id:").setText(projectName);
-
-		new PushButton("Finish").click();
+		SmartImportWizard easymportWizard = new SmartImportWizard();
+		easymportWizard.open();
+		SmartImportRootWizardPage selectImportRootWizardPage = new SmartImportRootWizardPage(easymportWizard);
+		File projectFile = new File("resources/" + projectName);
+		String path = projectFile.getAbsolutePath();
+		selectImportRootWizardPage.selectDirectory(path);
+		new WaitUntil(new ControlIsEnabled(new FinishButton(easymportWizard)), TimePeriod.LONG);
+		easymportWizard.finish();
 		new WaitWhile(new JobIsRunning(), TimePeriod.getCustom(600));
 	}
 
-	public static void openProjectWizard() {
-		MavenProjectWizard MPW = new MavenProjectWizard();
-		MPW.open();
-		MPW.next();
-		new WaitWhile(new JobIsRunning(), TimePeriod.VERY_LONG);
-		new LabeledText("Filter:").setText("quarkus-amazon-lambda-archetype");
-		new DefaultTable(0).select(0);
-		MPW.next();
-	}
-
-	public void addTestNGLibrary(String projectName) {
+	public void addLibraries(String projectName) {
 		new WorkbenchShell().setFocus();
 		new ProjectExplorer().selectProjects(projectName);
 
 		new ContextMenuItem("Properties").select();
 		new DefaultTreeItem("Java Build Path").select();
 		new DefaultTabItem("Libraries").activate();
-		new DefaultTree(1).getItem("Modulepath").select();
-		new PushButton("Add Library...").click();
-		new DefaultList().select("TestNG");
-		new NextButton().click();
-		new FinishButton().click();
+		
+		addLibrary("TestNG");
+		addLibrary("JUnit");
+		addJquery(projectName);
+		
 		new PushButton("Apply and Close").click();
 		new WaitWhile(new JobIsRunning(), TimePeriod.VERY_LONG);
+	}
+	
+	private void addLibrary(String libName) {
+		new DefaultTree(1).getItem("Classpath").select();
+		new PushButton("Add Library...").click();
+		new DefaultList().select(libName);
+		new NextButton().click();
+		new FinishButton().click();
+	}
+	
+	private void addJquery(String projectName) {
+		new DefaultTree(1).getItem("Classpath").select();
+		new PushButton("Add JARs...").click();
+		new DefaultTreeItem(projectName, "jquery.jar").select();
+		new OkButton().click();
 	}
 
 	@After
