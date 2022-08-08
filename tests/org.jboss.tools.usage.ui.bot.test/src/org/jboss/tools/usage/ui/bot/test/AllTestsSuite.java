157ce0cd102615c391d1dccd3891082949a6412e
Tamara Babalova <tbabalov@redhat.com>
2022-08-08 11:55:36 +0200

    Usage Project Added
    Signed-off-by: Tamara Babalova <tbabalov@redhat.com>

---
+++ tests/org.jboss.tools.usage.ui.bot.test/src/org/jboss/tools/usage/ui/bot/test/AllTestsSuite.java
@@ -0,0 +1,11 @@
+package org.jboss.tools.usage.ui.bot.test;
+
+import org.eclipse.reddeer.junit.runner.RedDeerSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith (RedDeerSuite.class)
+@SuiteClasses ({DummyTest.class})
+public class AllTestsSuite {
+
+}
