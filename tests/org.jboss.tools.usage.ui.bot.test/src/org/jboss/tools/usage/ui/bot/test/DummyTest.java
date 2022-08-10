410ac6b6720e2e7fc65a3b20a6530742bc8c87ce
Tamara Babalova <tbabalov@redhat.com>
2022-08-10 10:59:15 +0200

    Added Activator and simple test
    Signed-off-by: Tamara Babalova <tbabalov@redhat.com>

--- tests/org.jboss.tools.usage.ui.bot.test/src/org/jboss/tools/usage/ui/bot/test/DummyTest.java
+++ tests/org.jboss.tools.usage.ui.bot.test/src/org/jboss/tools/usage/ui/bot/test/DummyTest.java
@@ -1,11 +1,17 @@
 package org.jboss.tools.usage.ui.bot.test;
 
+import org.eclipse.reddeer.swt.impl.button.PushButton;
+import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
 import org.junit.Test;
 
 public class DummyTest {
 	
 	@Test
 	public void test() {
-		//content
+		DefaultShell dialog = new DefaultShell("JBoss Tools Usage");
+		dialog.setFocus();
+		assert(dialog.isVisible());
+		new PushButton("No").click();
+		assert(dialog.isDisposed());
 	}
 }
