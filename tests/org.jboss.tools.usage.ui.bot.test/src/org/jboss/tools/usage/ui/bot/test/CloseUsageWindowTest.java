c7cc673d843f1f04112871111423605e558ac093
Tamara Babalova <tbabalov@redhat.com>
2022-08-15 10:49:59 +0200

    PR change request addressed
    Signed-off-by: Tamara Babalova <tbabalov@redhat.com>

--- tests/org.jboss.tools.usage.ui.bot.test/src/org/jboss/tools/usage/ui/bot/test/CloseUsageWindowTest.java
+++ tests/org.jboss.tools.usage.ui.bot.test/src/org/jboss/tools/usage/ui/bot/test/CloseUsageWindowTest.java
@@ -1,8 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
 package org.jboss.tools.usage.ui.bot.test;
 
 import org.eclipse.reddeer.swt.impl.button.PushButton;
 import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
 import org.junit.Test;
+
+/** Testing closing usage dialog on load.
+ * @author Tamara Babalova */
 
 public class CloseUsageWindowTest {
 	
