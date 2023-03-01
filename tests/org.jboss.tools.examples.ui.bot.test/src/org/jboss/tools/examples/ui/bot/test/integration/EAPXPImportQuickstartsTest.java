0c6c077051b291bd91290c6a2e3cc03232b1307f
Oleksii Korniienko <olkornii@redhat.com>
2023-03-01 12:49:02 +0100

    Fixes for eap-xp-examples-itests
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- tests/org.jboss.tools.examples.ui.bot.test/src/org/jboss/tools/examples/ui/bot/test/integration/EAPXPImportQuickstartsTest.java
+++ tests/org.jboss.tools.examples.ui.bot.test/src/org/jboss/tools/examples/ui/bot/test/integration/EAPXPImportQuickstartsTest.java
@@ -12,27 +12,48 @@
 package org.jboss.tools.examples.ui.bot.test.integration;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.reddeer.common.condition.AbstractWaitCondition;
 import org.eclipse.reddeer.common.exception.WaitTimeoutExpiredException;
 import org.eclipse.reddeer.common.wait.TimePeriod;
 import org.eclipse.reddeer.common.wait.WaitUntil;
+import org.eclipse.reddeer.common.wait.WaitWhile;
 import org.eclipse.reddeer.eclipse.condition.ConsoleHasText;
+import org.eclipse.reddeer.eclipse.condition.ServerHasState;
 import org.eclipse.reddeer.eclipse.ui.browser.WebBrowserView;
 import org.eclipse.reddeer.eclipse.ui.console.ConsoleView;
+import org.eclipse.reddeer.eclipse.wst.server.ui.Runtime;
+import org.eclipse.reddeer.eclipse.wst.server.ui.RuntimePreferencePage;
+import org.eclipse.reddeer.eclipse.wst.server.ui.cnf.Server;
+import org.eclipse.reddeer.eclipse.wst.server.ui.cnf.ServersView2;
+import org.eclipse.reddeer.eclipse.wst.server.ui.cnf.ServersViewEnums.ServerState;
+import org.eclipse.reddeer.jface.preference.PreferenceDialog;
 import org.eclipse.reddeer.junit.internal.runner.ParameterizedRequirementsRunnerFactory;
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
+import org.eclipse.reddeer.requirements.browser.InternalBrowserRequirement.UseInternalBrowser;
 import org.eclipse.reddeer.requirements.server.ServerRequirementState;
+import org.eclipse.reddeer.swt.impl.button.FinishButton;
+import org.eclipse.reddeer.swt.impl.button.PushButton;
+import org.eclipse.reddeer.swt.impl.text.LabeledText;
+import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
+import org.eclipse.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
+import org.jboss.tools.maven.reddeer.requirement.NewRepositoryRequirement.DefineMavenRepository;
+import org.jboss.tools.maven.reddeer.requirement.NewRepositoryRequirement.MavenRepository;
 import org.jboss.ide.eclipse.as.reddeer.server.requirement.ServerRequirement.JBossServer;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized.Parameter;
@@ -50,22 +71,38 @@ import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 @RunWith(RedDeerSuite.class)
 @UseParametersRunnerFactory(ParameterizedRequirementsRunnerFactory.class)
+@DefineMavenRepository(newRepositories = {@MavenRepository(url="https://maven.repository.redhat.com/ga/",ID="ga",snapshots=true)})
 @JBossServer(state = ServerRequirementState.RUNNING)
+@UseInternalBrowser
 public class EAPXPImportQuickstartsTest extends AbstractImportQuickstartsTest {
 	public static final String SERVER_NAME = "Enterprise Application Platform";
 	public static final String BLACKLIST_FILE = "resources/servers/eap-blacklist";
 	public static final String BLACKLIST_ERRORS_REGEXES_FILE = "resources/servers/eap-xp-blacklist-test-errors-regexes.json";
 	public static final String URLS_FILE = "resources/servers/eap-xp-quickstarts-urls.json";
+	public static final String STANDALONE = "standalone-microprofile-ha.xml";
 	protected JSONObject urlsFileContents;
 
 	@Parameters(name = "{0}")
 	public static Collection<Quickstart> data() {
-		return createQuickstartsList();
+		Collection<Quickstart> quickList = createQuickstartsList();
+
+		List<String> newQuickstarts = Arrays.asList("country-client", "country-server");
+		quickList = replaceQuickstart(quickList, "microprofile-rest-client", newQuickstarts); // microprofile-rest-client containts 2 quickstarts, replace microprofile-rest-client with them
+		return quickList;
 	}
 
 	@Parameter
 	public Quickstart qstart;
 
+	/*
+	 * EAP-XP requires standalone-microprofile-ha.xml configuration instead of default one: https://issues.redhat.com/browse/JBIDE-28825
+	 */
+	@BeforeClass
+	public static void setup() {
+		setupLog();
+		setupStandalone(STANDALONE);
+	}
+	
 	/*
 	 * Main tests. Imports quickstart as maven project, performs checks and deploy
 	 * on server.
@@ -73,6 +110,59 @@ public class EAPXPImportQuickstartsTest extends AbstractImportQuickstartsTest {
 	@Test
 	public void quickstartTest() {
 		runQuickstarts(qstart, SERVER_NAME, BLACKLIST_FILE, BLACKLIST_ERRORS_REGEXES_FILE);
+	}
+	
+	private static Collection<Quickstart> replaceQuickstart(Collection<Quickstart> quickList, String quickstartForReplace, List<String> newQuickstarts) {
+		Collection<Quickstart> newQuickList = new ArrayList<Quickstart>();
+
+		for (Quickstart actualQuickstart : quickList) {
+			if (quickstartForReplace.equals(actualQuickstart.getName())) {
+				String actualPath = actualQuickstart.getPath().toString();
+				for (String newQuickstartName : newQuickstarts) {
+					Quickstart newQuickstart = new Quickstart(newQuickstartName, actualPath + "/" + newQuickstartName);
+					newQuickList.add(newQuickstart);
+				}
+			} else {
+				newQuickList.add(actualQuickstart);
+			}
+		}
+		return newQuickList;
+	}
+	
+	private static void setupStandalone(String standalone) {
+		ServersView2 serversView = new ServersView2();
+		serversView.open();
+		String serverFullName = getServerFullName(serversView.getServers(), SERVER_NAME);
+		String runtimeFullName = serverFullName.replace("Server", "Runtime");
+		Server server = serversView.getServer(serverFullName);
+		if (server.getLabel().getState() == ServerState.STARTED) {
+			server.stop();
+		}
+
+		PreferenceDialog dialog = new WorkbenchPreferenceDialog();
+		dialog.open();
+		dialog.select("Server","Runtime Environments");
+
+		RuntimePreferencePage runtimePreferencePage = new RuntimePreferencePage(dialog);
+		List<Runtime> runtimes = runtimePreferencePage.getServerRuntimes();
+		runtimePreferencePage.editRuntime(runtimeFullName);
+		new LabeledText("Configuration file: ").setText("standalone-microprofile-ha.xml");
+		new FinishButton().click();
+		new WaitWhile(new JobIsRunning(), TimePeriod.DEFAULT);
+
+		new PushButton(runtimePreferencePage, "Apply and Close").click();;
+		new WaitWhile(new JobIsRunning(), TimePeriod.DEFAULT);
+
+		server.start();
+	}
+	
+	private static String getServerFullName(List<Server> servers, String partOfName) {
+		for (Server srv : servers) {
+			if (srv.getLabel().getName().contains(partOfName)) {
+				return srv.getLabel().getName();
+			}
+		}
+		return null;
 	}
 
 	/**
