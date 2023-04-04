ac7f97c04c06ea21c28292cba1f0f000b02b790f
Oleksii Korniienko <olkornii@redhat.com>
2023-04-04 15:32:13 +0200

    TestNg tests folder removed from integration tests
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- tests/org.jboss.tools.testng.ui.bot.test/resources/testng_project/src/testng_project/HelloWorldClass.java
+++
@@ -1,8 +0,0 @@
-package testng_project;
-
-public class HelloWorldClass {
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-}
